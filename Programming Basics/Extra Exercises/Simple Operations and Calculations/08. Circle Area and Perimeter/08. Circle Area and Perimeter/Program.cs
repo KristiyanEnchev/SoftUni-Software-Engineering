0acbaea1a60fe0f0622057e4aba7704c6332d206
Kristiyan Enchev <kristiqnenchevv@gmail.com>
2020-12-02 23:09:09 +0200

    New commitment

---
+++ Programming Basics/Extra Exercises/Simple Operations and Calculations/08. Circle Area and Perimeter/08. Circle Area and Perimeter/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace ConsoleApp35
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double r = double.Parse(Console.ReadLine());
+
+            double area = Math.PI * r * r;
+            double perimeter = 2 * Math.PI * r;
+
+            Console.WriteLine($"{area:f2}");
+            Console.WriteLine($"{perimeter:f2}");
+        }
+    }
+}
