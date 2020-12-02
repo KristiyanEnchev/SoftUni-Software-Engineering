4b5c621c7edf8a4af45688cda80b39eefae8cfe5
Kristiyan Enchev <kristiqnenchevv@gmail.com>
2020-12-02 23:11:29 +0200

    New commitment

---
+++ Programming Basics/Extra Exercises/Simple Operations and Calculations/10. Weather Forecast - Part 2/10. Weather Forecast - Part 2/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace ConsoleApp35
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double degree = double.Parse(Console.ReadLine());
+            if (degree >= 5 && degree <= 11.9)
+            {
+                Console.WriteLine("Cold");
+            }
+            else if (degree >= 12 && degree <= 14.9)
+            {
+                Console.WriteLine("Cool");
+            }
+            else if (degree >= 15 && degree <= 20)
+            {
+                Console.WriteLine("Mild");
+            }
+            else if (degree > 20 && degree <= 25.9)
+            {
+                Console.WriteLine("Warm");
+            }
+            else if (degree >= 26 && degree <= 35)
+            {
+                Console.WriteLine("Hot");
+            }
+            else
+            {
+                Console.WriteLine("unknown");
+            }
+        }
+    }
+}
