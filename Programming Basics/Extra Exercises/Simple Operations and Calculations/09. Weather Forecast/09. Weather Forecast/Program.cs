d3b9f620db0da38c305c0fe8c912b8b8bb5d211e
Kristiyan Enchev <kristiqnenchevv@gmail.com>
2020-12-02 23:10:18 +0200

    New commitment

---
+++ Programming Basics/Extra Exercises/Simple Operations and Calculations/09. Weather Forecast/09. Weather Forecast/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace ConsoleApp35
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string comand = Console.ReadLine();
+            if (comand == "sunny")
+            {
+                Console.WriteLine("It's warm outside!");
+            }
+            else
+            {
+                Console.WriteLine("It's cold outside!");
+            }
+        }
+    }
+}
