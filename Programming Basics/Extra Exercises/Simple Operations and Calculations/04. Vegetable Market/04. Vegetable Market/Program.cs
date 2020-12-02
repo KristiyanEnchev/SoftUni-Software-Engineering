a1c7f5af9ba8bf70308f829768809933d4cdef71
Kristiyan Enchev <kristiqnenchevv@gmail.com>
2020-12-02 23:04:33 +0200

    New commitment

--- Programming Basics/Extra Exercises/Simple Operations and Calculations/04. Vegetable Market/04. Vegetable Market/Program.cs
+++ Programming Basics/Extra Exercises/Simple Operations and Calculations/04. Vegetable Market/04. Vegetable Market/Program.cs
@@ -1,12 +1,18 @@
 ﻿using System;
 
-namespace _04._Vegetable_Market
+namespace ConsoleApp35
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            double vegiPrice = double.Parse(Console.ReadLine());
+            double fruitPRice = double.Parse(Console.ReadLine());
+            double vegiCount = double.Parse(Console.ReadLine());
+            double fruitCount = double.Parse(Console.ReadLine());
+            double total = vegiPrice * vegiCount + fruitPRice * fruitCount;
+            double inEuro = total / 1.94;
+            Console.WriteLine($"{inEuro:f2}");
         }
     }
 }
