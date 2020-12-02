1e2ff558b13692d66c6cacc5d057be5c546abf70
Kristiyan Enchev <kristiqnenchevv@gmail.com>
2020-12-02 23:05:20 +0200

    New commitment

---
+++ Programming Basics/Extra Exercises/Simple Operations and Calculations/05. Training Lab/05. Training Lab/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace ConsoleApp35
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double w = double.Parse(Console.ReadLine());
+            double h = double.Parse(Console.ReadLine());
+            if (3 <= h || h <= w || w <= 100)
+            {
+                double wSm = w * 100;
+                double hSm = h * 100;
+                double rows = Math.Floor((hSm - 100) / 70);
+                double colums = Math.Floor(wSm / 120);
+                double total = (rows * colums) - 3;
+                Console.WriteLine($"{Math.Floor(total)}");
+            }
+            else
+            {
+                w = double.Parse(Console.ReadLine());
+                h = double.Parse(Console.ReadLine());
+            }
+        }
+    }
+}
