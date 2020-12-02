3afed93d94b41d1ca9719d9a4c281e3b5eed2837
Kristiyan Enchev <kristiqnenchevv@gmail.com>
2020-12-02 23:02:22 +0200

    New commitment

---
+++ Programming Basics/Extra Exercises/Simple Operations and Calculations/03. Celsius to Fahrenheit/03. Celsius to Fahrenheit/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace ConsoleApp35
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double c = double.Parse(Console.ReadLine());
+            double convert = c * 1.80 + 32.00;
+            Console.WriteLine($"{convert:f2}");
+        }
+    }
+}
