05e11911f2ac676a4235081e125e49f0df8c3444
Kristiyan Enchev <kristiqnenchevv@gmail.com>
2020-12-02 23:00:38 +0200

    New commitment

---
+++ Programming Basics/Extra Exercises/Simple Operations and Calculations/02. Triangle Area/02. Triangle Area/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace ConsoleApp35
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double b1 = double.Parse(Console.ReadLine());
+            double h = double.Parse(Console.ReadLine());
+
+            double area = b1 * h / 2;
+            Console.WriteLine($"{area:f2}");
+        }
+    }
+}
