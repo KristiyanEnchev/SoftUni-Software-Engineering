0f182586697c55a05f74a5513ca58895ce88632f
Kristiyan Enchev <kristiqnenchevv@gmail.com>
2020-12-02 23:25:02 +0200

    New commitment

---
+++ Programming Basics/Exercises/02.Conditional Statements - Exercise/01. Sum Seconds/01. Sum Seconds/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace ConsoleApp12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int first = int.Parse(Console.ReadLine());
+            int second = int.Parse(Console.ReadLine());
+            int thurd = int.Parse(Console.ReadLine());
+
+            int totalseconds = first + second + thurd;
+            int totalMin = totalseconds / 60;
+            int totalSec = totalseconds % 60;
+
+            Console.WriteLine($"{totalMin}:{totalSec:d2}");
+        }
+    }
+}
