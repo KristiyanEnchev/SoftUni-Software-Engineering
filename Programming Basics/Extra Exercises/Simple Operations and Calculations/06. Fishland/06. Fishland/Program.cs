97946c94e6443018e513dd15fe1c399eba325b44
Kristiyan Enchev <kristiqnenchevv@gmail.com>
2020-12-02 23:06:37 +0200

    New commitment

---
+++ Programming Basics/Extra Exercises/Simple Operations and Calculations/06. Fishland/06. Fishland/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace ConsoleApp35
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double skPrice = double.Parse(Console.ReadLine());
+            double ccPrice = double.Parse(Console.ReadLine());
+            double palCount = double.Parse(Console.ReadLine());
+            double safritCount = double.Parse(Console.ReadLine());
+            double oysterCount = double.Parse(Console.ReadLine());
+
+            double palPrice = skPrice * 1.60;
+            double safritPrice = ccPrice * 1.80;
+            double oysterPrice = 7.50;
+            double total = palCount * palPrice + safritCount * safritPrice + oysterCount * oysterPrice;
+
+            Console.WriteLine($"{total:f2}");
+        }
+    }
+}
