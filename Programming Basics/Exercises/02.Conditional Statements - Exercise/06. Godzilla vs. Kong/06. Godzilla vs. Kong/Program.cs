c4a1e61fb166affd0ea3597640f955729f72f765
Kristiyan Enchev <kristiqnenchevv@gmail.com>
2020-12-03 16:54:31 +0200

    Add new commit

---
+++ Programming Basics/Exercises/02.Conditional Statements - Exercise/06. Godzilla vs. Kong/06. Godzilla vs. Kong/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace ConsoleApp12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double budget = double.Parse(Console.ReadLine());
+            int person = int.Parse(Console.ReadLine());
+            double cloth = double.Parse(Console.ReadLine());
+
+            double decor = budget * 0.10;
+            double clothCost = person * cloth;
+
+            if (person > 150)
+            {
+                clothCost *= 0.90;
+            }
+
+            double cost = decor + clothCost;
+
+            if (budget >= cost)
+            {
+                double extra = budget - cost;
+                Console.WriteLine("Action!");
+                Console.WriteLine($"Wingard starts filming with {extra:f2} leva left.");
+            }
+            else
+            {
+                double need = cost - budget;
+                Console.WriteLine("Not enough money!");
+                Console.WriteLine($"Wingard needs {need:f2} leva more.");
+            }
+        }
+    }
+}
