c11fd8a17b2293b32f0cb4cfc9074176a2392b10
Kristiyan Enchev <kristiqnenchevv@gmail.com>
2020-12-03 16:55:34 +0200

    Add new commit

---
+++ Programming Basics/Exercises/02.Conditional Statements - Exercise/07. World Swimming Record/07. World Swimming Record/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace ConsoleApp12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double recordSeconds = double.Parse(Console.ReadLine());
+            double totalMeters = double.Parse(Console.ReadLine());
+            double timeForOneMeter = double.Parse(Console.ReadLine());
+
+            double totalSecondsForRecord = totalMeters * timeForOneMeter;
+            double deley = Math.Floor(totalMeters / 15) * 12.5;
+
+            double totalTime = totalSecondsForRecord + deley;
+            if (totalTime < recordSeconds)
+            {
+                Console.WriteLine($" Yes, he succeeded! The new world record is {totalTime:f2} seconds.");
+            }
+            else
+            {
+                double need = totalTime - recordSeconds;
+                Console.WriteLine($"No, he failed! He was {need:f2} seconds slower.");
+            }
+        }
+    }
+}
