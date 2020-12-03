e72c306e071d4e75815d5f5fb6a124a259d7b0fe
Kristiyan Enchev <kristiqnenchevv@gmail.com>
2020-12-03 16:57:09 +0200

    Add new commit

---
+++ Programming Basics/Exercises/02.Conditional Statements - Exercise/08. Scholarship/08. Scholarship/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double income = double.Parse(Console.ReadLine());
+            double grade = double.Parse(Console.ReadLine());
+            double minimalWage = double.Parse(Console.ReadLine());
+
+            double social = 0;
+            double excelent = 0; ;
+
+            if (income < minimalWage)
+            {
+                if (grade > 4.50)
+                {
+                    social = minimalWage * 0.35;
+                }
+            }
+            if (grade >= 5.50)
+            {
+                excelent = grade * 25;
+            }
+            if (social > excelent)
+            {
+                if (social == 0)
+                {
+                    Console.WriteLine("You cannot get a scholarship!");
+                }
+                else
+                {
+                    Console.WriteLine($"You get a Social scholarship {Math.Floor(social)} BGN");
+                }
+            }
+            else if (excelent > social)
+            {
+                if (excelent == 0)
+                {
+                    Console.WriteLine("You cannot get a scholarship!");
+                }
+                else if (excelent == social)
+                {
+                    Console.WriteLine($"You get a scholarship for excellent results {Math.Floor(excelent)} BGN");
+                }
+                else
+                {
+                    Console.WriteLine($"You get a scholarship for excellent results {Math.Floor(excelent)} BGN");
+                }
+            }
+            else
+            {
+                Console.WriteLine("You cannot get a scholarship!");
+            }
+        }
+    }
+}
