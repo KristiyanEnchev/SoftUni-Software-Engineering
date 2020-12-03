3d0c7a674bf4e3e9b5cf7961d7bd0e8f3b7be41f
Kristiyan Enchev <kristiqnenchevv@gmail.com>
2020-12-03 16:53:41 +0200

    Add new commit

---
+++ Programming Basics/Exercises/02.Conditional Statements - Exercise/05. Time + 15 Minutes/05. Time + 15 Minutes/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace ConsoleApp12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int h = int.Parse(Console.ReadLine());
+            int min = int.Parse(Console.ReadLine());
+            min += 15;
+            if (min >= 60)
+            {
+                min %= 60;
+                h += 1;
+            }
+            if (h >= 24)
+            {
+                h = 0;
+            }
+            Console.WriteLine($"{h}:{min:d2}");
+        }
+    }
+}
