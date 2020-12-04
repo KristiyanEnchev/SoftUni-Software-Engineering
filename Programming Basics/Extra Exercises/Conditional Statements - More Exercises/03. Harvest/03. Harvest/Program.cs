5939519a291d372c806be80ee50b476fa91e29a0
Kristiyan Enchev <kristiqnenchevv@gmail.com>
2020-12-04 14:44:11 +0200

    New Add exercises

---
+++ Programming Basics/Extra Exercises/Conditional Statements - More Exercises/03. Harvest/03. Harvest/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace ConsoleApp37
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double loze = double.Parse(Console.ReadLine());
+            double grozde = double.Parse(Console.ReadLine());
+            double wineNeeded = double.Parse(Console.ReadLine());
+            double peopleCount = double.Parse(Console.ReadLine());
+            double allGrozde = grozde * loze;
+            double grozdeForWine = allGrozde * 0.40;
+            double wineProduced = grozdeForWine / 2.5;
+            if (wineProduced < wineNeeded)
+            {
+                Console.WriteLine($"It will be a tough winter! More {Math.Floor(wineNeeded - wineProduced)} liters wine needed.");
+            }
+            else
+            {
+                Console.WriteLine($"Good harvest this year! Total wine: {Math.Floor(wineProduced)} liters.");
+                Console.WriteLine($"{Math.Ceiling(wineProduced - wineNeeded)} liters left -> {Math.Ceiling(Math.Ceiling(wineProduced - wineNeeded) / peopleCount)} liters per person.");
+            }
+        }
+    }
+}
