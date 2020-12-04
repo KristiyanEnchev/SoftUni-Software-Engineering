5bbbc4f5f8c3a1639d6c1dcb12200a0da653b5c7
Kristiyan Enchev <kristiqnenchevv@gmail.com>
2020-12-04 14:48:54 +0200

    New Add Exercises

---
+++ Programming Basics/Extra Exercises/Conditional Statements - More Exercises/08. Fuel Tank - Part 2/08. Fuel Tank - Part 2/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace ConsoleApp37
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string gasType = Console.ReadLine();
+            double gasCount = double.Parse(Console.ReadLine());
+            string card = Console.ReadLine();
+            double price = 0;
+            if (gasType == "Gas")
+            {
+                price = 0.93;
+                if (card == "Yes")
+                {
+                    price -= 0.08;
+                }
+            }
+            else if (gasType == "Gasoline")
+            {
+                price = 2.22;
+                if (card == "Yes")
+                {
+                    price -= 0.18;
+                }
+            }
+            else if (gasType == "Diesel")
+            {
+                price = 2.33;
+                if (card == "Yes")
+                {
+                    price -= 0.12;
+                }
+            }
+            if (gasCount >= 20 && gasCount <= 25)
+            {
+                price *= 0.92;
+            }
+            else if (gasCount > 25)
+            {
+                price *= 0.90;
+            }
+            Console.WriteLine($"{price * gasCount:f2} lv.");
+        }
+    }
+}
