ddd65f63086cf2b087f1d14a71153d8e46789387
Kristiyan Enchev <kristiqnenchevv@gmail.com>
2020-12-04 14:47:44 +0200

    New Add Exercises

---
+++ Programming Basics/Extra Exercises/Conditional Statements - More Exercises/08. Fuel Tank/08. Fuel Tank/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Xml.Linq;
+
+namespace ConsoleApp37
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string gasType = Console.ReadLine();
+            double gasCount = double.Parse(Console.ReadLine());
+            if (gasType == "Diesel" || gasType == "Gasoline" || gasType == "Gas")
+            {
+                if (gasCount >= 25)
+                {
+                    Console.WriteLine($"You have enough {gasType.ToLower()}.");
+                }
+                else if (gasCount < 25)
+                {
+                    Console.WriteLine($"Fill your tank with {gasType.ToLower()}!");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Invalid fuel!");
+            }
+        }
+    }
+}
