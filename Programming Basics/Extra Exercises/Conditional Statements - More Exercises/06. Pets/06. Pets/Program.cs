5101e944a2e90fbfa2f9f9373cafc3afde1cbb60
Kristiyan Enchev <kristiqnenchevv@gmail.com>
2020-12-04 14:46:17 +0200

    New Add Exercises

---
+++ Programming Basics/Extra Exercises/Conditional Statements - More Exercises/06. Pets/06. Pets/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Xml.Linq;
+
+namespace ConsoleApp37
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int dayCount = int.Parse(Console.ReadLine());
+            double foodCount = double.Parse(Console.ReadLine());
+            double dogFood = double.Parse(Console.ReadLine());
+            double catFood = double.Parse(Console.ReadLine());
+            double turtleFood = double.Parse(Console.ReadLine());
+            double turtleFoodinKilo = turtleFood / 1000;
+            double totalFood = (dogFood + catFood + turtleFoodinKilo) * dayCount;
+            if (foodCount >= totalFood)
+            {
+                Console.WriteLine($"{Math.Floor(foodCount - totalFood)} kilos of food left.");
+            }
+            else
+            {
+                Console.WriteLine($"{Math.Ceiling(totalFood - foodCount)} more kilos of food are needed.");
+            }
+        }
+    }
+}
