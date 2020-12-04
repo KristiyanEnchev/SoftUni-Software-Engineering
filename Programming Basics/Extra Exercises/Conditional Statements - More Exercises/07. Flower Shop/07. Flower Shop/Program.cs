ece92a49d4cd6962b6c012a37d147afb5bd6f3ac
Kristiyan Enchev <kristiqnenchevv@gmail.com>
2020-12-04 14:46:59 +0200

    New Add Exercises

---
+++ Programming Basics/Extra Exercises/Conditional Statements - More Exercises/07. Flower Shop/07. Flower Shop/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Xml.Linq;
+
+namespace ConsoleApp37
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double magnoCount = double.Parse(Console.ReadLine());
+            double zimbCount = double.Parse(Console.ReadLine());
+            double rosesCount = double.Parse(Console.ReadLine());
+            double kaktusCount = double.Parse(Console.ReadLine());
+            double presentPrice = double.Parse(Console.ReadLine());
+            double total = magnoCount * 3.25 + zimbCount * 4.00 + rosesCount * 3.50 + kaktusCount * 8.00;
+            double moneyLeft = total * 0.95;
+            if (presentPrice <= moneyLeft)
+            {
+                Console.WriteLine($"She is left with {Math.Floor(moneyLeft - presentPrice)} leva.");
+            }
+            else
+            {
+                Console.WriteLine($"She will have to borrow {Math.Ceiling(presentPrice - moneyLeft)} leva.");
+            }
+        }
+    }
+}
