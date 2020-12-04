48e22731fa75d66178736738634a6338d3e7f9c0
Kristiyan Enchev <kristiqnenchevv@gmail.com>
2020-12-04 14:45:39 +0200

    New Add Exercises

---
+++ Programming Basics/Extra Exercises/Conditional Statements - More Exercises/05. Firm/05. Firm/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Transactions;
+using System.Xml.Linq;
+
+namespace ConsoleApp37
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double hoursNeeded = double.Parse(Console.ReadLine());
+            double daysNeeded = double.Parse(Console.ReadLine());
+            double peopleWorkingExtraCount = double.Parse(Console.ReadLine());
+            double daysLeft = daysNeeded * 0.90;
+            double totalH = daysLeft * 8;
+            double extraH = peopleWorkingExtraCount * (2 * daysNeeded);
+            double totalWorking = Math.Floor(totalH + extraH);
+            if (hoursNeeded <= totalWorking)
+            {
+                Console.WriteLine($"Yes!{totalWorking - hoursNeeded} hours left.");
+            }
+            else
+            {
+                Console.WriteLine($"Not enough time!{hoursNeeded - totalWorking} hours needed.");
+            }
+
+        }
+    }
+}
