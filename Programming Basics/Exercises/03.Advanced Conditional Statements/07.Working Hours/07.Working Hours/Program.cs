14a2ed5a551904455a6ea214ffdccb08f26e4a7f
Kristiyan Enchev <kristiqnenchevv@gmail.com>
2020-12-05 16:56:02 +0200

    New Add

---
+++ Programming Basics/Exercises/03.Advanced Conditional Statements/07.Working Hours/07.Working Hours/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double time = double.Parse(Console.ReadLine());
+            string day = Console.ReadLine();
+            if (day == "Monday" || day == "Tuesday" || day == "Wednesday" || day == "Thursday" || day == "Friday" || day == "Saturday")
+            {
+                if (time >= 10 && time <= 18)
+                {
+                    Console.WriteLine("open");
+                }
+                else
+                {
+                    Console.WriteLine("closed");
+                }
+            }
+            else if (day == "Sunday")
+            {
+                Console.WriteLine("closed");
+            }
+        }
+    }
+}
