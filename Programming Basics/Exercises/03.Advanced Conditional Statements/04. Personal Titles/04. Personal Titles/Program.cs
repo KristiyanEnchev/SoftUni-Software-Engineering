8a1658570fdb8ab2b4e944e14076798fc02cb5be
Kristiyan Enchev <kristiqnenchevv@gmail.com>
2020-12-05 16:53:28 +0200

    Add new files

---
+++ Programming Basics/Exercises/03.Advanced Conditional Statements/04. Personal Titles/04. Personal Titles/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double age = double.Parse(Console.ReadLine());
+            string gender = Console.ReadLine();
+            if (gender == "m")
+            {
+                if (age >= 16)
+                {
+                    Console.WriteLine("Mr.");
+                }
+                else
+                {
+                    Console.WriteLine("Master");
+                }
+            }
+            else if (gender == "f")
+            {
+                if (age >= 16)
+                {
+                    Console.WriteLine("Ms.");
+                }
+                else
+                {
+                    Console.WriteLine("Miss");
+                }
+            }
+        }
+    }
+}
