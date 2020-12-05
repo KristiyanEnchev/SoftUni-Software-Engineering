e8e30ed89b50bf72b49c55e3e0281871fe12c0ad
Kristiyan Enchev <kristiqnenchevv@gmail.com>
2020-12-05 17:02:04 +0200

    New Exercises Add

---
+++ Programming Basics/Exercises/03.Advanced Conditional Statements/10. Invalid Number/10. Invalid Number/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace ConsoleApp5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double num = double.Parse(Console.ReadLine());
+
+            if (num > 99 && num < 201 || num == 0)
+            {
+
+            }
+            else
+            {
+                Console.WriteLine("invalid");
+            }
+        }
+    }
+}
+
