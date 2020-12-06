6d66de2359c6cc19d73137706fd0be78cc2e273f
Kristiyan Enchev <kristiqnenchevv@gmail.com>
2020-12-06 19:20:45 +0200

    New Add

---
+++ Programming Basics/Exercises/03.Conditional Statements Advanced - Exercise/01. Cinema/01. Cinema/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace ConsoleApp15
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string type = Console.ReadLine();
+            double line = double.Parse(Console.ReadLine());
+            double colum = double.Parse(Console.ReadLine());
+            double price = 0;
+            if (type == "Premiere")
+            {
+                price = 12.00;
+            }
+            else if (type == "Normal")
+            {
+                price = 7.50;
+            }
+            else if (type == "Discount")
+            {
+                price = 5.00;
+            }
+            double income = (line * colum) * price;
+            Console.WriteLine($"{income:f2} leva");
+        }
+    }
+}
