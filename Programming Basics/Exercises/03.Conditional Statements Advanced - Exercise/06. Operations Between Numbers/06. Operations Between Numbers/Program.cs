cf581871733be116175e4aed16ce63cf47ef6d4b
Kristiyan Enchev <kristiqnenchevv@gmail.com>
2020-12-06 19:26:07 +0200

    New Add

---
+++ Programming Basics/Exercises/03.Conditional Statements Advanced - Exercise/06. Operations Between Numbers/06. Operations Between Numbers/Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+class Program
+{
+    static void Main(string[] args)
+    {
+        double N1 = double.Parse(Console.ReadLine());
+        double N2 = double.Parse(Console.ReadLine());
+        string operation = Console.ReadLine();
+        double result = 0;
+        string type = "";
+        if (operation == "+")
+        {
+            result = N1 + N2;
+            if (result % 2 == 0)
+            {
+                type = "even";
+            }
+            else
+            {
+                type = "odd";
+            }
+            Console.WriteLine($"{N1} {operation} {N2} = {result} - {type}");
+        }
+        else if (operation == "-")
+        {
+            result = N1 - N2;
+            if (result % 2 == 0)
+            {
+                type = "even";
+            }
+            else
+            {
+                type = "odd";
+            }
+            Console.WriteLine($"{N1} {operation} {N2} = {result} - {type}");
+        }
+        else if (operation == "*")
+        {
+            result = N1 * N2;
+            if (result % 2 == 0)
+            {
+                type = "even";
+            }
+            else
+            {
+                type = "odd";
+            }
+            Console.WriteLine($"{N1} {operation} {N2} = {result} - {type}");
+        }
+        else if (operation == "/")
+        {
+            if (N1 == 0)
+            {
+                Console.WriteLine($"Cannot divide {N2} by zero");
+            }
+            else if (N2 == 0)
+            {
+                Console.WriteLine($"Cannot divide {N1} by zero");
+            }
+            else if (N1 != 0 && N2 != 0)
+            {
+                result = N1 / N2;
+                Console.WriteLine($"{N1} {operation} {N2} = {result:f2}");
+            }
+        }
+        else if (operation == "%")
+        {
+            if (N1 == 0)
+            {
+                Console.WriteLine($"Cannot divide {N2} by zero");
+            }
+            else if (N2 == 0)
+            {
+                Console.WriteLine($"Cannot divide {N1} by zero");
+            }
+            else
+            {
+                result = N1 % N2;
+                Console.WriteLine($"{N1} {operation} {N2} = {result}");
+            }
+        }
+    }
+}
