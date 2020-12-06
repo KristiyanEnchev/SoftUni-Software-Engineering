ca7ac5ef72d7aa4e83581e6a3f05fb79433cb0bd
Kristiyan Enchev <kristiqnenchevv@gmail.com>
2020-12-06 19:22:11 +0200

    New Add

---
+++ Programming Basics/Exercises/03.Conditional Statements Advanced - Exercise/02. Summer Outfit/02. Summer Outfit/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+
+namespace ConsoleApp15
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double degree = double.Parse(Console.ReadLine());
+            string timeOfDay = Console.ReadLine();
+            string outfit = "";
+            string shoes = "";
+            if (degree >= 10 && degree <= 18)
+            {
+                switch (timeOfDay)
+                {
+                    case "Morning":
+                        outfit = "Sweatshirt";
+                        shoes = "Sneakers";
+                        break;
+                    case "Afternoon":
+                    case "Evening":
+                        outfit = "Shirt";
+                        shoes = "Moccasins";
+                        break;
+                }
+            }
+            else if (degree > 18 && degree <= 24)
+            {
+                switch (timeOfDay)
+                {
+                    case "Morning":
+                        outfit = "Shirt";
+                        shoes = "Moccasins";
+                        break;
+                    case "Afternoon":
+                        outfit = "T-Shirt";
+                        shoes = "Sandals";
+                        break;
+                    case "Evening":
+                        outfit = "Shirt";
+                        shoes = "Moccasins";
+                        break;
+                }
+            }
+            else if (degree > 24)
+            {
+                switch (timeOfDay)
+                {
+                    case "Morning":
+                        outfit = "T-Shirt";
+                        shoes = "Sandals";
+                        break;
+                    case "Afternoon":
+                        outfit = "Swim Suit";
+                        shoes = "Barefoot";
+                        break;
+                    case "Evening":
+                        outfit = "Shirt";
+                        shoes = "Moccasins";
+                        break;
+                }
+            }
+            Console.WriteLine($"It's {degree} degrees, get your {outfit} and {shoes}.");
+        }
+    }
+}
