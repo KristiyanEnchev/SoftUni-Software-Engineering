1f85001f492fb3093b48eac89965ea2a0ba00b00
Kristiyan Enchev <kristiqnenchevv@gmail.com>
2020-12-06 19:28:30 +0200

    New Commit

---
+++ Programming Basics/Exercises/03.Conditional Statements Advanced - Exercise/9. Volleyball/9. Volleyball/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+class Program
+{
+    static void Main(string[] args)
+    {
+        string year = Console.ReadLine();
+        double b = double.Parse(Console.ReadLine());
+        double h = double.Parse(Console.ReadLine());
+        double WLSF = ((48 - h) / 4) * 3;
+        double ganmeB = (b / 3) * 2;
+        double total = h + WLSF + ganmeB;
+        if (year == "normal")
+        {
+            Console.WriteLine($"{Math.Floor(total)}");
+        }
+        else if (year == "leap")
+        {
+            double extra = total * 1.15;
+            Console.WriteLine($"{Math.Floor(extra)}");
+        }
+    }
+}
