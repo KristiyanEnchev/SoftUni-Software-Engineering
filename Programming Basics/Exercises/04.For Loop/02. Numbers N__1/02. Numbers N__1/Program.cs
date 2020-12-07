37ddbb01cfe5a8c3fca0458e2b0f7e4440084465
Kristiyan Enchev <kristiqnenchevv@gmail.com>
2020-12-07 16:03:46 +0200

    New Add

---
+++ Programming Basics/Exercises/04.For Loop/02. Numbers N__1/02. Numbers N__1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+class Program
+{
+    static void Main(string[] args)
+    {
+        int n = int.Parse(Console.ReadLine());
+        for (int i = n; i >= 1; i -= 1)
+        {
+            Console.WriteLine(i);
+        }
+    }
+}
