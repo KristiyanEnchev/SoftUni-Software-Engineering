778c2ec81d5f1129cc9af9ba8ff63464cdce8f3c
Kristiyan Enchev <kristiqnenchevv@gmail.com>
2020-12-07 16:10:10 +0200

    New Add

---
+++ Programming Basics/Exercises/04.For Loop/09. Left and Right Sum/09. Left and Right Sum/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int n = int.Parse(Console.ReadLine());
+        int left = 0;
+        int right = 0;
+        for (int i = 0; i < n; i++)
+        {
+            int num = int.Parse(Console.ReadLine());
+            left += num;
+        }
+        for (int i = 0; i < n; i++)
+        {
+            int num1 = int.Parse(Console.ReadLine());
+            right += num1;
+        }
+        if (left == right)
+        {
+            Console.WriteLine($"Yes, sum = {left}");
+        }
+        else
+        {
+            double dif = left - right;
+            Console.WriteLine($"No, diff = {Math.Abs(dif)}");
+        }
+    }
+}
