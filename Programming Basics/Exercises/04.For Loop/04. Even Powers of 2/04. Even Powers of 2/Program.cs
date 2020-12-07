354377778a2913dcda364bc3d92a329199685d8b
Kristiyan Enchev <kristiqnenchevv@gmail.com>
2020-12-07 16:05:32 +0200

    New Add

---
+++ Programming Basics/Exercises/04.For Loop/04. Even Powers of 2/04. Even Powers of 2/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+class Program
+{
+    static void Main(string[] args)
+    {
+        int n = int.Parse(Console.ReadLine());
+        for (int i = 0; i <= n; i += 2)
+        {
+            double result = Math.Pow(2, i);
+            Console.WriteLine(result);
+        }
+    }
+}
