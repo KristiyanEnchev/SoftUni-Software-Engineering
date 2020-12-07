7480c43f46174090ac25b183d428cbb89e836c9e
Kristiyan Enchev <kristiqnenchevv@gmail.com>
2020-12-07 16:10:57 +0200

    New Add

---
+++ Programming Basics/Exercises/04.For Loop/10. Odd Even Sum/10. Odd Even Sum/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int n = int.Parse(Console.ReadLine());
+        int oddSum = 0;
+        int evenSum = 0;
+        for (int i = 1; i <= n; i++)
+        {
+            int element = int.Parse(Console.ReadLine());
+            if (i % 2 == 0)
+            {
+                evenSum += element;
+            }
+            else
+            {
+                oddSum += element;
+            }
+        }
+        if (oddSum == evenSum)
+        {
+            Console.WriteLine("Yes");
+            Console.WriteLine($"Sum = {evenSum}");
+        }
+        else
+        {
+            int sum = Math.Abs(oddSum - evenSum);
+            Console.WriteLine("No");
+            Console.WriteLine($"Diff = {sum}");
+        }
+    }
+}
