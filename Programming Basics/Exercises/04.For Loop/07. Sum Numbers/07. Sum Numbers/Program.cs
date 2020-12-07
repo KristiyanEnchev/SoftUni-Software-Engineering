76cd0bc3151e1b039aca5b6d38190d8cab000a55
Kristiyan Enchev <kristiqnenchevv@gmail.com>
2020-12-07 16:08:13 +0200

    New Add

---
+++ Programming Basics/Exercises/04.For Loop/07. Sum Numbers/07. Sum Numbers/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int n = int.Parse(Console.ReadLine());
+        int sum = 0;
+        for (int i = 0; i < n; i++)
+        {
+            int n1 = int.Parse(Console.ReadLine());
+            sum += n1;
+        }
+        Console.WriteLine(sum);
+    }
+}
