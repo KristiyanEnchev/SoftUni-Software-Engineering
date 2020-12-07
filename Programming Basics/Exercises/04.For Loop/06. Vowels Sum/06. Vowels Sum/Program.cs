3661b230bd91f2fed6edc3443cc02c8320f2d9b3
Kristiyan Enchev <kristiqnenchevv@gmail.com>
2020-12-07 16:07:15 +0200

    New Add

---
+++ Programming Basics/Exercises/04.For Loop/06. Vowels Sum/06. Vowels Sum/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+class Program
+{
+    static void Main(string[] args)
+    {
+        string text = Console.ReadLine();
+        int sum = 0;
+        for (int i = 0; i < text.Length; i++)
+        {
+            switch (text[i])
+            {
+                case 'a':
+                    sum += 1;
+                    break;
+                case 'e':
+                    sum += 2;
+                    break;
+                case 'i':
+                    sum += 3;
+                    break;
+                case 'o':
+                    sum += 4;
+                    break;
+                case 'u':
+                    sum += 5;
+                    break;
+            }
+        }
+        Console.WriteLine(sum);
+    }
+}
