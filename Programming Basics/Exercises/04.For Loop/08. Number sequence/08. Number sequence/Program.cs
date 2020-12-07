0a725714b8e8eee229d7111ef25b19f5ec90d281
Kristiyan Enchev <kristiqnenchevv@gmail.com>
2020-12-07 16:09:19 +0200

    New Add

---
+++ Programming Basics/Exercises/04.For Loop/08. Number sequence/08. Number sequence/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int n = int.Parse(Console.ReadLine());
+        int max = int.MinValue;
+        int min = int.MaxValue;
+        for (int i = 0; i < n; i++)
+        {
+            int num = int.Parse(Console.ReadLine());
+
+            if (num < min)
+            {
+                min = num;
+            }
+            if (num > max)
+            {
+                max = num;
+            }
+        }
+        Console.WriteLine($"Max number: {max}");
+        Console.WriteLine($"Min number: {min}");
+    }
+}
