61457044d712e46c34424d36d2961c2ebd445e54
Kristiyan Enchev <kristiqnenchevv@gmail.com>
2020-12-07 16:11:46 +0200

    New Add

---
+++ Programming Basics/Exercises/04.For Loop/11. Clever Lily/11. Clever Lily/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int age = int.Parse(Console.ReadLine());
+        double priceMachine = double.Parse(Console.ReadLine());
+        double priceToy = double.Parse(Console.ReadLine());
+        double savings = 0;
+        double money = 10;
+        double toys = 0;
+        for (int i = 1; i <= age; i++)
+        {
+            if (i % 2 == 0)
+            {
+                savings += money;
+                savings -= 1;
+                money += 10;
+            }
+            else
+            {
+                toys += 1;
+            }
+        }
+        double total = savings + (toys * priceToy);
+        if (total >= priceMachine)
+        {
+            double extra = total - priceMachine;
+            Console.WriteLine($"Yes! {extra:f2}");
+        }
+        else
+        {
+            double need = priceMachine - total;
+            Console.WriteLine($"No! {need:f2}");
+        }
+    }
+}
