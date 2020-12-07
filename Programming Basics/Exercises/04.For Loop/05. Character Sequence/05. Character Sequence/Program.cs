d88534f7177bc32004575e828225a0d2f31dbe8f
Kristiyan Enchev <kristiqnenchevv@gmail.com>
2020-12-07 16:06:31 +0200

    New Add

---
+++ Programming Basics/Exercises/04.For Loop/05. Character Sequence/05. Character Sequence/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+class Program
+{
+    static void Main(string[] args)
+    {
+        string text = Console.ReadLine();
+        int len = text.Length;
+        for (int i = 0; i < len; i++)
+        {
+            Console.WriteLine(text[i]);
+        }
+
+    }
+}
