fc9d3e274ba588810a8c5bdc25cc68821086ec81
Kristiyan Enchev <kristiqnenchevv@gmail.com>
2020-12-08 20:00:46 +0200

    New Add

---
+++ Programming Basics/Exercises/05.Advanced For Loops/05. Divide Without Remainder/05. Divide Without Remainder/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            double p1 = 0;
+            double p2 = 0;
+            double p3 = 0;
+            for (int i = 1; i <= n; i++)
+            {
+                int num = int.Parse(Console.ReadLine());
+                if (num % 2 == 0)
+                {
+                    p1 += 1;
+                }
+                if (num % 3 == 0)
+                {
+                    p2 += 1;
+                }
+                if (num % 4 == 0)
+                {
+                    p3 += 1;
+                }
+            }
+            Console.WriteLine($"{p1 / n * 100:f2}%");
+            Console.WriteLine($"{p2 / n * 100:f2}%");
+            Console.WriteLine($"{p3 / n * 100:f2}%");
+        }
+    }
+}
