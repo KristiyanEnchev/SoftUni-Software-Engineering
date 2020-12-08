afe1f1e3cac432bd12cc2e3e4a152b69d0f33cf1
Kristiyan Enchev <kristiqnenchevv@gmail.com>
2020-12-08 19:58:23 +0200

    New Add

---
+++ Programming Basics/Exercises/05.Advanced For Loops/02. Half Sum Element/02. Half Sum Element/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+class Program
+{
+    static void Main(string[] args)
+    {
+        int n = int.Parse(Console.ReadLine());
+        int max = int.MinValue;
+        int sum = 0;
+        for (int i = 0; i < n; i++)
+        {
+            int num = int.Parse(Console.ReadLine());
+            sum += num;
+            if (num > max)
+            {
+                max = num;
+            }
+        }
+        if (sum - max == max)
+        {
+            Console.WriteLine("Yes");
+            Console.WriteLine($"Sum = {max}");
+        }
+        else
+        {
+            double dif = (sum - max) - max;
+            Console.WriteLine("No");
+            Console.WriteLine($"Diff = {Math.Abs(dif)}");
+        }
+    }
+}
