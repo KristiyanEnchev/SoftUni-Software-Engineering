0aac52b558f07012a3946e8acff4797b70194f94
Kristiyan Enchev <kristiqnenchevv@gmail.com>
2020-12-08 20:01:30 +0200

    New Add

---
+++ Programming Basics/Exercises/05.Advanced For Loops/06. Salary/06. Salary/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int tabs = int.Parse(Console.ReadLine());
+        double salary = double.Parse(Console.ReadLine());
+        for (int i = 1; i <= tabs; i++)
+        {
+            string website = Console.ReadLine();
+            if (website == "Facebook")
+            {
+                salary -= 150;
+            }
+            else if (website == "Instagram")
+            {
+                salary -= 100;
+            }
+            else if (website == "Reddit")
+            {
+                salary -= 50;
+            }
+        }
+        if (salary <= 0)
+        {
+            Console.WriteLine("You have lost your salary.");
+        }
+        else
+        {
+            Console.WriteLine(salary);
+        }
+    }
+}
