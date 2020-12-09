a53bffa9fa885956a20789e1c2d9a75d71ad3deb
Kristiyan Enchev <kristiqnenchevv@gmail.com>
2020-12-09 18:22:06 +0200

    New Add

---
+++ Programming Basics/Exercises/06.While Loop/08. Graduation pt.2/08. Graduation pt.2/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+class Program
+{
+    static void Main(string[] args)
+    {
+        string name = Console.ReadLine();
+        int grade = 1;
+        double totalCount = 0;
+        int ex = 0;
+        while (grade <= 12)
+        {
+            double num = double.Parse(Console.ReadLine());
+            if (num >= 4.0)
+            {
+                grade += 1;
+                totalCount += num;
+                continue;
+            }
+            else
+            {
+                ex += 1;
+                if (ex > 1)
+                {
+                    Console.WriteLine($"{name} has been excluded at {grade} grade");
+                    break;
+                }
+            }
+        }
+        if (ex < 1)
+        {
+            double average = totalCount / 12;
+            Console.WriteLine($"{name} graduated. Average grade: {average:f2}");
+
+        }
+    }
+}
