662156c5f8ad8d1aece1c37fae81339933486146
Kristiyan Enchev <kristiqnenchevv@gmail.com>
2020-12-09 18:19:58 +0200

    New Add

---
+++ Programming Basics/Exercises/06.While Loop/05. Account Balance/05. Account Balance/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string input = Console.ReadLine();
+        double balance = 0.0;
+        while (input != "NoMoreMoney")
+        {
+            double amount = double.Parse(input);
+            if (amount < 0)
+            {
+                Console.WriteLine("Invalid operation!");
+                break;
+            }
+            balance += amount;
+            Console.WriteLine($"Increase: {amount:F2}");
+            input = Console.ReadLine();
+        }
+        Console.WriteLine($"Total: {balance:F2}");
+    }
+}
