a36d52ac1222a7b3170a9f41c65b15ff711f8588
Kristiyan Enchev <kristiqnenchevv@gmail.com>
2020-12-09 18:20:50 +0200

    New Add

---
+++ Programming Basics/Exercises/06.While Loop/06. Max Number/06. Max Number/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string comand = Console.ReadLine();
+        int max = int.MinValue;
+        while (comand != "Stop")
+        {
+            int num = int.Parse(comand);
+            if (num > max)
+            {
+                max = num;
+            }
+            comand = Console.ReadLine();
+        }
+        Console.WriteLine(max);
+    }
+}
