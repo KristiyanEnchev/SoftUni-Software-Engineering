6c6c947e4d37bb0cd68129f6675fc2a0abe5b651
Kristiyan Enchev <kristiqnenchevv@gmail.com>
2020-12-09 18:19:14 +0200

    New Add

---
+++ Programming Basics/Exercises/06.While Loop/04. Sequence 2k+1/04. Sequence 2k+1/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+class Program
+{
+    static void Main(string[] args)
+    {
+        int num = int.Parse(Console.ReadLine());
+        int n = 1;
+        while (n <= num)
+        {
+            Console.WriteLine(n);
+            n = n * 2 + 1;
+        }
+    }
+}
