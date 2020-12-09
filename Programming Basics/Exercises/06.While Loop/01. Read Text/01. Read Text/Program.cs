1809475123694f831c2a829204efca1614db1584
Kristiyan Enchev <kristiqnenchevv@gmail.com>
2020-12-09 18:17:08 +0200

    New Add

---
+++ Programming Basics/Exercises/06.While Loop/01. Read Text/01. Read Text/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace ConsoleApp19
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                string word = Console.ReadLine();
+                if (word == "Stop")
+                {
+                    break;
+                }
+                Console.WriteLine(word);
+            }
+        }
+    }
+}
