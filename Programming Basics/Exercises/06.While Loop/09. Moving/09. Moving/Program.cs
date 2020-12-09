ea65cf90394fecdeb741ccbafb2d67ff3e233859
Kristiyan Enchev <kristiqnenchevv@gmail.com>
2020-12-09 18:22:51 +0200

    New Add

---
+++ Programming Basics/Exercises/06.While Loop/09. Moving/09. Moving/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        double lenght = double.Parse(Console.ReadLine());
+        double width = double.Parse(Console.ReadLine());
+        double hight = double.Parse(Console.ReadLine());
+        string comand = Console.ReadLine();
+        int box = 0;
+        double cubic = lenght * hight * width;
+        while (comand != "Done")
+        {
+            int count = int.Parse(comand);
+            box += count;
+            if (box > cubic)
+            {
+                Console.WriteLine($"No more free space! You need {box - cubic} Cubic meters more.");
+                break;
+            }
+            comand = Console.ReadLine();
+        }
+        if (cubic < box)
+        {
+
+        }
+        else
+        {
+
+            Console.WriteLine($"{cubic - box} Cubic meters left.");
+        }
+    }
+}
