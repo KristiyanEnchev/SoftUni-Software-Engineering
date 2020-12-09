a80ad1968d275a9756055618c5b6405a5d547beb
Kristiyan Enchev <kristiqnenchevv@gmail.com>
2020-12-09 18:21:27 +0200

    New Add

---
+++ Programming Basics/Exercises/06.While Loop/07. Min Number/07. Min Number/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string comand = Console.ReadLine();
+        int min = int.MaxValue;
+        while (comand != "Stop")
+        {
+            int num = int.Parse(comand);
+            if (num < min)
+            {
+                min = num;
+            }
+            comand = Console.ReadLine();
+        }
+        Console.WriteLine(min);
+    }
+}
