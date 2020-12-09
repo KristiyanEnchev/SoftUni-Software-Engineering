7d31a357b9355895eb0f3417a74ad10f25fcc04d
Kristiyan Enchev <kristiqnenchevv@gmail.com>
2020-12-09 18:18:34 +0200

    New Add

---
+++ Programming Basics/Exercises/06.While Loop/03. Sum Numbers/03. Sum Numbers/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+class Program
+{
+    static void Main(string[] args)
+    {
+        int n = int.Parse(Console.ReadLine());
+        int nan = 0;
+        while (true)
+        {
+            int num = int.Parse(Console.ReadLine());
+            nan += num;
+            if (nan < n)
+            {
+                continue;
+            }
+            else
+            {
+                Console.WriteLine(nan);
+            }
+            break;
+        }
+    }
+}
