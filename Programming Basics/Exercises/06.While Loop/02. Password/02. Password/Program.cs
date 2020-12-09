58b25893a8c053d7747d33b65643494e49ff0689
Kristiyan Enchev <kristiqnenchevv@gmail.com>
2020-12-09 18:17:52 +0200

    New Add

---
+++ Programming Basics/Exercises/06.While Loop/02. Password/02. Password/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+class Program
+{
+    static void Main(string[] args)
+    {
+        string name = Console.ReadLine();
+        string pass = Console.ReadLine();
+        string input = Console.ReadLine();
+        while (input != pass)
+        {
+            input = Console.ReadLine();
+        }
+        Console.WriteLine($"Welcome {name}!");
+    }
+}
