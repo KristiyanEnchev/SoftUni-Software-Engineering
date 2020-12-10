a986ae4c904cca49a13f99cfa647d4e8b48c52f4
Kristiyan Enchev <kristiqnenchevv@gmail.com>
2020-12-10 22:17:09 +0200

    New Add

---
+++ Programming Basics/Exercises/06.While Loop - Exercise/01. Old Books/01. Old Books/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string book = Console.ReadLine();
+        int bookCount = 0;
+        string input = "";
+        while (input != "No More Books")
+        {
+            input = Console.ReadLine();
+            bookCount += 1;
+            if (input == book)
+            {
+                Console.WriteLine($"You checked {bookCount - 1} books and found it.");
+                break;
+            }
+        }
+        if (input == "No More Books")
+        {
+            Console.WriteLine($"The book you search is not here!");
+            Console.WriteLine($"You checked {bookCount - 1} books.");
+        }
+    }
+}
