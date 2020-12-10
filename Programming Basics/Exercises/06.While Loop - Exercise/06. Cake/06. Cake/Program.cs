126c879bf0832780c2279583ad93872609a210e9
Kristiyan Enchev <kristiqnenchevv@gmail.com>
2020-12-10 22:21:09 +0200

    New Add

---
+++ Programming Basics/Exercises/06.While Loop - Exercise/06. Cake/06. Cake/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        double lenght = double.Parse(Console.ReadLine());
+        double width = double.Parse(Console.ReadLine());
+        double cakeSize = lenght * width;
+        string comand = Console.ReadLine();
+        double pieceCount = 0;
+        while (comand != "STOP")
+        {
+            int count = int.Parse(comand);
+            pieceCount += count;
+            if (pieceCount > cakeSize)
+            {
+                Console.WriteLine($"No more cake left! You need {pieceCount - cakeSize} pieces more.");
+                break;
+            }
+            comand = Console.ReadLine();
+        }
+        if (comand == "STOP")
+        {
+            Console.WriteLine($"{cakeSize - pieceCount} pieces are left.");
+        }
+    }
+}
