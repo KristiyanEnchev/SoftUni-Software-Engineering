f6f022213d073e59fa575db5215e6ea6af47dd7f
Kristiyan Enchev <kristiqnenchevv@gmail.com>
2020-12-10 22:18:34 +0200

    New Add

---
+++ Programming Basics/Exercises/06.While Loop - Exercise/03. Vacation/03. Vacation/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace ConsoleApp24
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double excursionPrice = double.Parse(Console.ReadLine());
+            double moneyCount = double.Parse(Console.ReadLine());
+            int dayCount = 0;
+            int spendig = 0;
+            while (moneyCount < excursionPrice && spendig < 5)
+            {
+                string function = Console.ReadLine();
+                double money = double.Parse(Console.ReadLine());
+                dayCount++;
+                if (function == "spend")
+                {
+                    moneyCount -= money;
+                    spendig += 1;
+                    if (moneyCount <= 0)
+                    {
+                        moneyCount = 0;
+                        continue;
+                    }
+                }
+                if (function == "save")
+                {
+                    moneyCount += money;
+                    spendig = 0;
+                }
+            }
+            if (spendig == 5)
+            {
+                Console.WriteLine($"You can't save the money.");
+                Console.WriteLine($"{dayCount}");
+            }
+            if (moneyCount >= excursionPrice)
+            {
+                Console.WriteLine($"You saved the money for {dayCount} days.");
+            }
+        }
+    }
+}
