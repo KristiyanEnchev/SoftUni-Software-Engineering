4574da0d5f70d8bd4d0c08751fd3cdf5047c17fd
Kristiyan Enchev <kristiqnenchevv@gmail.com>
2020-12-10 22:17:54 +0200

    New Add

---
+++ Programming Basics/Exercises/06.While Loop - Exercise/02. Exam Preparation/02. Exam Preparation/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace ConsoleApp24
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int badGradesCount = int.Parse(Console.ReadLine());
+            bool Enough = false;
+            string name = "";
+            double score = 0;
+            int problemNumber = 0;
+            int badGrad = 0;
+            while (!Enough)
+            {
+                string input = Console.ReadLine();
+                if (input == "Enough")
+                {
+                    Enough = true;
+                    break;
+                }
+                double grade = double.Parse(Console.ReadLine());
+                name = input;
+                score += grade;
+                problemNumber++; ;
+                if (grade <= 4.00)
+                {
+                    badGrad++;
+                    if (badGrad >= badGradesCount)
+                    {
+                        break;
+                    }
+                }
+            }
+            if (Enough)
+            {
+                double average = 1.0 * score / problemNumber;
+                Console.WriteLine($"Average score: {average:f2}");
+                Console.WriteLine($"Number of problems: {problemNumber}");
+                Console.WriteLine($"Last problem: {name}");
+            }
+            else
+            {
+                Console.WriteLine($"You need a break, {badGrad} poor grades.");
+            }
+        }
+    }
+}
