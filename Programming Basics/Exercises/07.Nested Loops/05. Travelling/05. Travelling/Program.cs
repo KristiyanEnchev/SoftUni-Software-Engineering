b748648aee5e0208544a32e3e1f0ad27ba52d8df
Kristiyan Enchev <kristiqnenchevv@gmail.com>
2020-12-11 20:15:13 +0200

    New Add

---
+++ Programming Basics/Exercises/07.Nested Loops/05. Travelling/05. Travelling/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Security.Cryptography;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        while (true)
+        {
+            string destination = Console.ReadLine();
+            if (destination == "End")
+            {
+                break;
+            }
+            double budjet = double.Parse(Console.ReadLine());
+            while (budjet > 0)
+            {
+                double sum = double.Parse(Console.ReadLine());
+                budjet -= sum;
+            }
+            Console.WriteLine($"Going to {destination}!");
+        }
+    }
+}
