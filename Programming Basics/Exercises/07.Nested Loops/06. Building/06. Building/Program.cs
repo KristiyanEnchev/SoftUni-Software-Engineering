89d0048ce192311aafbd2f5c9c9f118491c51579
Kristiyan Enchev <kristiqnenchevv@gmail.com>
2020-12-11 20:15:49 +0200

    New Add

---
+++ Programming Basics/Exercises/07.Nested Loops/06. Building/06. Building/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+class Program
+{
+    static void Main(string[] args)
+    {
+        int stores = int.Parse(Console.ReadLine());
+        int rooms = int.Parse(Console.ReadLine());
+        string type = "";
+        for (int rows = stores; rows >= 1; rows--)
+        {
+            for (int colums = 0; colums < rooms; colums++)
+            {
+                if (rows == stores)
+                {
+                    type = "L";
+                }
+                else
+                {
+                    if (rows % 2 == 0)
+                    {
+                        type = "O";
+                    }
+                    else
+                    {
+                        type = "A";
+                    }
+                }
+                Console.Write($"{type}{rows}{colums} ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
