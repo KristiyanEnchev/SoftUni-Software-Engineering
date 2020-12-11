1f54f8c2f6b2ddc99fbbc09f5f27fd0bf880cc4a
Kristiyan Enchev <kristiqnenchevv@gmail.com>
2020-12-11 20:16:28 +0200

    New Add

---
+++ Programming Basics/Exercises/07.Nested Loops/07. Cinema Tickets/07. Cinema Tickets/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int totalTikets = 0;
+        int student = 0;
+        int kid = 0;
+        int standard = 0;
+        while (true)
+        {
+            string movie = Console.ReadLine();
+            if (movie == "Finish")
+            {
+                break;
+            }
+            int capacity = int.Parse(Console.ReadLine());
+            int movieTickets = 0;
+            while (true)
+            {
+                string type = Console.ReadLine();
+                if (type == "End")
+                {
+                    break;
+                }
+                switch (type)
+                {
+                    case "student":
+                        student++;
+                        break;
+                    case "standard":
+                        standard++;
+                        break;
+                    case "kid":
+                        kid++;
+                        break;
+                }
+                movieTickets++;
+                if (movieTickets >= capacity)
+                {
+                    break;
+                }
+            }
+            Console.WriteLine($"{movie} - {100.0 * movieTickets / capacity:f2}% full.");
+            totalTikets += movieTickets;
+        }
+        Console.WriteLine($"Total tickets: {totalTikets}");
+        Console.WriteLine($"{100.0 * student / totalTikets:f2}% student tickets.");
+        Console.WriteLine($"{100.0 * standard / totalTikets:f2}% standard tickets.");
+        Console.WriteLine($"{100.0 * kid / totalTikets:f2}% kids tickets.");
+    }
+}
