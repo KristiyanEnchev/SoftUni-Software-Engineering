c701c1944bab2b240e448de39442b023c4c14369
Kristiyan Enchev <kristiqnenchevv@gmail.com>
2020-12-11 20:13:14 +0200

    New Add

---
+++ Programming Basics/Exercises/07.Nested Loops/02. Multiplication Table/02. Multiplication Table/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        for (int x1 = 1; x1 <= 10; x1++)
+        {
+            for (int x2 = 1; x2 <= 10; x2++)
+            {
+                int sum = x1 * x2;
+                Console.WriteLine($"{x1} * {x2} = {sum}");
+            }
+        }
+    }
+}
