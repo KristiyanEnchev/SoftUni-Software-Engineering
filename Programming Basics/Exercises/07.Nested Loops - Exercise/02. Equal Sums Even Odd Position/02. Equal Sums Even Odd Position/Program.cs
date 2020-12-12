e1a514095bfde99067508c954bd22c18eaa6babc
Kristiyan Enchev <kristiqnenchevv@gmail.com>
2020-12-12 16:22:01 +0200

    New Add

---
+++ Programming Basics/Exercises/07.Nested Loops - Exercise/02. Equal Sums Even Odd Position/02. Equal Sums Even Odd Position/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+class Program
+{
+    static void Main(string[] args)
+    {
+        int first = int.Parse(Console.ReadLine());
+        int second = int.Parse(Console.ReadLine());
+
+        for (int i = first; i <= second; i++)
+        {
+            string num = i.ToString();
+            int even = 0;
+            int odd = 0;
+            for (int j = 0; j < num.Length; j++)
+            {
+                int digit = int.Parse(num[j].ToString());
+                if (j % 2 == 0)
+                {
+                    even += digit;
+                }
+                else
+                {
+                    odd += digit;
+                }
+            }
+            if (even == odd)
+            {
+                Console.Write(i + " ");
+            }
+        }
+    }
+}
