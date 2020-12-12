9e33529c158564aa27340209e56234c4949419dc
Kristiyan Enchev <kristiqnenchevv@gmail.com>
2020-12-12 16:24:55 +0200

    New Add

---
+++ Programming Basics/Exercises/07.Nested Loops - Exercise/06. Special Numbers/06. Special Numbers/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+namespace _10.Special_Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= 9; i++)
+            {
+                for (int j = 1; j <= 9; j++)
+                {
+                    for (int k = 1; k <= 9; k++)
+                    {
+                        for (int m = 1; m <= 9; m++)
+                        {
+                            if (n % i == 0 && n % j == 0 && n % k == 0 && n % m == 0)
+                            {
+                                Console.Write("{0}{1}{2}{3} ", i, j, k, m);
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
