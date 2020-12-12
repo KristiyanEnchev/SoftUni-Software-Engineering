5634ee6f71a12790c0274d9a32138053c879d277
Kristiyan Enchev <kristiqnenchevv@gmail.com>
2020-12-12 16:21:09 +0200

    New Add

---
+++ Programming Basics/Exercises/07.Nested Loops - Exercise/01. Number Pyramid/01. Number Pyramid/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int n = int.Parse(Console.ReadLine());
+        int num = 1;
+        bool end = false;
+        for (int row = 1; row <= n; row++)
+        {
+            for (int colums = 1; colums <= row; colums++)
+            {
+                if (num > n)
+                {
+                    end = true;
+                    break;
+                }
+                Console.Write(num + " ");
+                num++;
+            }
+            if (end)
+            {
+                break;
+            }
+            Console.WriteLine();
+        }
+    }
+}
