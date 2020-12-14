41a638ff3313a8a6b152440c226a9bcfd469a3e0
Kristiyan Enchev <kristiqnenchevv@gmail.com>
2020-12-14 14:23:15 +0200

    New Add

---
+++ Programming Basics/Extra Exercises/Nested-Conditional-Statements-Exercises/01. Match Tickets/01. Match Tickets/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Transactions;
+
+namespace ConsoleApp39
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double budget = double.Parse(Console.ReadLine());
+            string category = Console.ReadLine();
+            int peopleCount = int.Parse(Console.ReadLine());
+            double price = 0;
+            if (peopleCount >= 1 && peopleCount <= 4)
+            {
+                budget *= 0.25;
+            }
+            else if (peopleCount >= 5 && peopleCount <= 9)
+            {
+                budget *= 0.40;
+            }
+            else if (peopleCount >= 10 && peopleCount <= 24)
+            {
+                budget *= 0.50;
+            }
+            else if (peopleCount >= 25 && peopleCount <= 49)
+            {
+                budget *= 0.60;
+            }
+            else if (peopleCount >= 50)
+            {
+                budget *= 0.75;
+            }
+            if (category == "VIP")
+            {
+                price = 499.99;
+            }
+            else if (category == "Normal")
+            {
+                price = 249.99;
+            }
+            double totalMoney = price * peopleCount;
+            if (budget >= totalMoney)
+            {
+                Console.WriteLine($"Yes! You have {budget - totalMoney:f2} leva left.");
+            }
+            else
+            {
+                Console.WriteLine($"Not enough money! You need {totalMoney - budget:f2} leva.");
+            }
+        }
+    }
+}
