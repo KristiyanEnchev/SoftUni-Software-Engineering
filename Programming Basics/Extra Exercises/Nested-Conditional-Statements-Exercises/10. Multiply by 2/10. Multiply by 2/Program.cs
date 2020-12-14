01645aa0fc7e51bb1eb02f1bf3132e34d5ec16b6
Kristiyan Enchev <kristiqnenchevv@gmail.com>
2020-12-14 14:29:29 +0200

    New Add

---
+++ Programming Basics/Extra Exercises/Nested-Conditional-Statements-Exercises/10. Multiply by 2/10. Multiply by 2/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace ConsoleApp39
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                double num = double.Parse(Console.ReadLine());
+                if (num < 0)
+                {
+                    Console.WriteLine("Negative number!");
+                    break;
+                }
+                Console.WriteLine($"Result: {num * 2:f2}");
+            }
+        }
+    }
+}
