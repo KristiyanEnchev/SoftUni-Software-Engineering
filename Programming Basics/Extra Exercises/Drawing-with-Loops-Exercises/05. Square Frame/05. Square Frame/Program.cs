cbb42957b28d651182e790c10c1d0b25c9a037a0
Kristiyan Enchev <kristiqnenchevv@gmail.com>
2020-12-15 12:01:28 +0200

    New Add

---
+++ Programming Basics/Extra Exercises/Drawing-with-Loops-Exercises/05. Square Frame/05. Square Frame/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace ConsoleApp44
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = int.Parse(Console.ReadLine());
+            for (int i = 1; i <= num; i++)
+            {
+                if (i == 1 || i == num)
+                {
+                    Console.Write("+" + " ");
+                }
+                else
+                {
+                    Console.Write("|" + " ");
+                }
+                for (int j = 2; j <= num; j++)
+                {
+                    if (j == num)
+                    {
+                        if (i == 1 || i == num)
+                        {
+                            Console.Write("+");
+                        }
+                        else
+                        {
+                            Console.Write("|");
+                        }
+                    }
+                    else
+                    {
+                        Console.Write("-" + " ");
+                    }
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
