d6d0070382e37fc879152d631f7030007a5dcf78
Kristiyan Enchev <kristiqnenchevv@gmail.com>
2020-12-15 12:02:08 +0200

    New Add

---
+++ Programming Basics/Extra Exercises/Drawing-with-Loops-Exercises/06. Rhombus of Stars/06. Rhombus of Stars/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace ConsoleApp44
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = int.Parse(Console.ReadLine());
+            for (int row = 1; row <= num; row++)
+            {
+                for (int col = 1; col <= num - row; col++)
+                {
+                    Console.Write(" ");
+                }
+                Console.Write("*");
+                for (int col = 1; col < row; col++)
+                {
+                    Console.Write(" *");
+                }
+                Console.WriteLine();
+            }
+            for (int row = num - 1; row >= 1; row--)
+            {
+                for (int col = 1; col <= num - row; col++)
+                {
+                    Console.Write(" ");
+                }
+                Console.Write("*");
+                for (int col = 1; col < row; col++)
+                {
+                    Console.Write(" *");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
