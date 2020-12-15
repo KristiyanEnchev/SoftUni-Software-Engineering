457a20e91d9304c1cd55e8e4ae9ad5879c78b507
Kristiyan Enchev <kristiqnenchevv@gmail.com>
2020-12-15 12:02:50 +0200

    New Add

---
+++ Programming Basics/Extra Exercises/Drawing-with-Loops-Exercises/07. Christmas Tree/07. Christmas Tree/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace ConsoleApp44
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = int.Parse(Console.ReadLine());
+            for (int row = 0; row <= num; row++)
+            {
+                for (int i = 1; i <= num - row; i++)
+                {
+                    Console.Write(" ");
+                }
+                for (int i = 1; i <= row; i++)
+                {
+                    Console.Write("*");
+                }
+                Console.Write(" ");
+                Console.Write("|");
+                Console.Write(" ");
+                for (int i = 1; i <= row; i++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
