f4cd1630f402301b2e89ac40645d333c2885a32e
Kristiyan Enchev <kristiqnenchevv@gmail.com>
2020-12-15 12:00:51 +0200

    New Add

---
+++ Programming Basics/Extra Exercises/Drawing-with-Loops-Exercises/04. Triangle of Dollars/04. Triangle of Dollars/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace ConsoleApp44
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = int.Parse(Console.ReadLine());
+            for (int i = 1; i <= num; i++)
+            {
+                for (int j = 0; j < i; j++)
+                {
+                    Console.Write("$" + " ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
