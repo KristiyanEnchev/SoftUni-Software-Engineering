691c1e23fd8d896403eab6234f1d805ed8065502
Kristiyan Enchev <kristiqnenchevv@gmail.com>
2020-12-15 12:03:53 +0200

    New Add

---
+++ Programming Basics/Extra Exercises/Drawing-with-Loops-Exercises/08. Sunglasses/08. Sunglasses/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Sunglasses
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            Console.WriteLine(new string('*', n * 2) + new string(' ', n) + new string('*', n * 2));
+
+
+
+            for (int i = 0; i < n - 2; i++)
+            {
+
+                Console.Write("*" + new string('/', 2 * n - 2) + "*");
+
+                if (i == (n - 1) / 2 - 1)
+                {
+                    Console.Write(new string('|', n));
+                }
+                else
+                {
+                    Console.Write(new string(' ', n));
+                }
+
+
+                Console.Write("*" + new string('/', 2 * n - 2) + "*");
+
+                Console.WriteLine();
+
+            }
+
+            Console.Write(new string('*', n * 2) + new string(' ', n) + new string('*', n * 2));
+        }
+    }
+}
