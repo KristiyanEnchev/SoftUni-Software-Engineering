eac0d65d20b1207403e194648411bc570eb49041
Kristiyan Enchev <kristiqnenchevv@gmail.com>
2020-12-15 11:59:05 +0200

    New add

---
+++ Programming Basics/Extra Exercises/Drawing-with-Loops-Exercises/01. Rectangle of 10 x 10 Stars/01. Rectangle of 10 x 10 Stars/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace ConsoleApp44
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 10; i++)
+            {
+
+                for (int j = 0; j < 10; j++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
