22b0662efed40a5d65f2f99115ed02c74787efc4
Kristiyan Enchev <kristiqnenchevv@gmail.com>
2020-12-15 11:59:38 +0200

    New add

---
+++ Programming Basics/Extra Exercises/Drawing-with-Loops-Exercises/02. Rectangle of N x N Stars/02. Rectangle of N x N Stars/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace ConsoleApp44
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = int.Parse(Console.ReadLine());
+            for (int i = 1; i <= num; i++)
+            {
+
+                for (int j = 1; j <= num; j++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
