e26d9cbe09e7d2d0ef0bb760610f814f9e4241e1
Kristiyan Enchev <kristiqnenchevv@gmail.com>
2020-12-16 11:15:12 +0200

    New Add

---
+++ Programming Basics/Extra Exercises/For-Loop-Exercises/03. Logistics/03. Logistics/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace ConsoleApp44
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int cargo = int.Parse(Console.ReadLine());
+            double bus = 0;
+            double truck = 0;
+            double train = 0;
+            double totalTons = 0;
+            for (int i = 1; i <= cargo; i++)
+            {
+                int tonsOfCargo = int.Parse(Console.ReadLine());
+                if (tonsOfCargo <= 3)
+                {
+                    bus += tonsOfCargo;
+                    totalTons += tonsOfCargo;
+                }
+                else if (tonsOfCargo >= 4 && tonsOfCargo <= 11)
+                {
+                    truck += tonsOfCargo;
+                    totalTons += tonsOfCargo;
+                }
+                else if (tonsOfCargo >= 12)
+                {
+                    train += tonsOfCargo;
+                    totalTons += tonsOfCargo;
+                }
+            }
+            double averagePrice = (bus * 200 + truck * 175 + train * 120) / totalTons;
+            Console.WriteLine($"{averagePrice:f2}");
+            Console.WriteLine($"{bus / totalTons * 100:f2}%");
+            Console.WriteLine($"{truck / totalTons * 100:f2}%");
+            Console.WriteLine($"{train / totalTons * 100:f2}%");
+        }
+    }
+}
