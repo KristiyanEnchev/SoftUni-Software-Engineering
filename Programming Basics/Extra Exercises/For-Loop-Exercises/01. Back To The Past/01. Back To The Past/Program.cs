938fcff879e5851840014896fb222653e1faa63a
Kristiyan Enchev <kristiqnenchevv@gmail.com>
2020-12-16 11:12:26 +0200

    New Add

---
+++ Programming Basics/Extra Exercises/For-Loop-Exercises/01. Back To The Past/01. Back To The Past/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace ConsoleApp44
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double money = double.Parse(Console.ReadLine());
+            int age = int.Parse(Console.ReadLine());
+            int hisAge = 18;
+            for (int i = 1800; i <= age; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    money -= 12000;
+                }
+                else
+                {
+                    money -= 12000 + 50 * hisAge;
+                }
+                hisAge++;
+            }
+            if (money >= 0)
+            {
+                Console.WriteLine($"Yes! He will live a carefree life and will have {money:f2} dollars left.");
+            }
+            else
+            {
+                Console.WriteLine($"He will need {Math.Abs(money):f2} dollars to survive.");
+            }
+        }
+    }
+}
