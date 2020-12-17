4c72ab7f8a8540404a763acbe8add0eb261baad1
Kristiyan Enchev <kristiqnenchevv@gmail.com>
2020-12-17 12:59:46 +0200

    New Add

---
+++ Programming Basics/Extra Exercises/Nested-Loops-Exercises/01. Unique PIN Codes/01. Unique PIN Codes/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace ConsoleApp44
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int first = int.Parse(Console.ReadLine());
+            int second = int.Parse(Console.ReadLine());
+            int thurd = int.Parse(Console.ReadLine());
+            for (int i = 1; i <= first; i++)
+            {
+                for (int j = 1; j <= second; j++)
+                {
+                    for (int k = 1; k <= thurd; k++)
+                    {
+                        if (j == 2 || j == 3 || j == 5 || j == 7)
+                        {
+                            if (i % 2 == 0 && k % 2 == 0)
+                            {
+                                Console.WriteLine($"{i} {j} {k}");
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
