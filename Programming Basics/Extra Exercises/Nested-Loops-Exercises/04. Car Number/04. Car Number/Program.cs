2149f1e08115c71bfb76e59e9d9bca9799bf82b2
Kristiyan Enchev <kristiqnenchevv@gmail.com>
2020-12-17 13:01:37 +0200

    New Add

---
+++ Programming Basics/Extra Exercises/Nested-Loops-Exercises/04. Car Number/04. Car Number/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace ConsoleApp44
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int from = int.Parse(Console.ReadLine());
+            int to = int.Parse(Console.ReadLine());
+            for (int i = from; i <= to; i++)
+            {
+                for (int j = from; j <= to; j++)
+                {
+                    for (int k = from; k <= to; k++)
+                    {
+                        for (int l = from; l <= to; l++)
+                        {
+                            if (i % 2 == 0 && l % 2 != 0)
+                            {
+                                if (i > l)
+                                {
+                                    if ((j + k) % 2 == 0)
+                                    {
+                                        Console.Write($"{i}{j}{k}{l} ");
+                                    }
+                                }
+                            }
+                            else if (i % 2 != 0 && l % 2 == 0)
+                            {
+                                if (i > l)
+                                {
+                                    if ((j + k) % 2 == 0)
+                                    {
+                                        Console.Write($"{i}{j}{k}{l} ");
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
