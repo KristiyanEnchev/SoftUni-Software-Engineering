fc4483188ca0ead55b20389a25dbe39357c94941
Kristiyan Enchev <kristiqnenchevv@gmail.com>
2020-12-17 13:04:45 +0200

    New Add

---
+++ Programming Basics/Extra Exercises/Nested-Loops-Exercises/09. Sum of Two Numbers/09. Sum of Two Numbers/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+class Program
+{
+    static void Main(string[] args)
+    {
+        int start = int.Parse(Console.ReadLine());
+        int end = int.Parse(Console.ReadLine());
+        if (end <= start)
+        {
+            end = int.Parse(Console.ReadLine());
+        }
+        int magicNumber = int.Parse(Console.ReadLine());
+        int count = 0;
+        for (int i = start; i <= end; i++)
+        {
+            for (int j = start; j <= end; j++)
+            {
+                count++;
+                if (i + j == magicNumber)
+                {
+                    Console.WriteLine($"Combination N:{count} ({i} + {j} = {magicNumber})");
+                    return;
+                }
+            }
+        }
+        Console.WriteLine($"{count} combinations - neither equals {magicNumber}");
+    }
+}
