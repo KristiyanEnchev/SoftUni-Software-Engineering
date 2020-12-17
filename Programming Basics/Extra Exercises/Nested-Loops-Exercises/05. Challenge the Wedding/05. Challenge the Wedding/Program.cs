fd921e575ea8c5ce0a91a240c0b1176d5d0f79fb
Kristiyan Enchev <kristiqnenchevv@gmail.com>
2020-12-17 13:02:11 +0200

    NEw Add

---
+++ Programming Basics/Extra Exercises/Nested-Loops-Exercises/05. Challenge the Wedding/05. Challenge the Wedding/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace ConsoleApp44
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int men = int.Parse(Console.ReadLine());
+            int women = int.Parse(Console.ReadLine());
+            int maxCapacity = int.Parse(Console.ReadLine());
+            int count = 0;
+            for (int j = 1; j <= men; j++)
+            {
+                for (int k = 1; k <= women; k++)
+                {
+                    Console.Write($"({j} <-> {k}) ");
+                    count++;
+                    if (count == maxCapacity)
+                    {
+                        break;
+                    }
+                    if (j == men && k == women)
+                    {
+                        break;
+                    }
+                }
+                if (count == maxCapacity)
+                {
+                    break;
+                }
+            }
+        }
+    }
+}
