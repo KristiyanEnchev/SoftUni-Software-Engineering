02cf816cb616fd12a2157de8d306f12e3690cff7
Kristiyan Enchev <kristiqnenchevv@gmail.com>
2020-12-17 13:00:22 +0200

    New Add

---
+++ Programming Basics/Extra Exercises/Nested-Loops-Exercises/02. Letters Combinations/02. Letters Combinations/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace ConsoleApp44
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char from = char.Parse(Console.ReadLine());
+            char to = char.Parse(Console.ReadLine());
+            char skip = char.Parse(Console.ReadLine());
+            int count = 0;
+            for (char j = from; j <= to; j++)
+            {
+                for (char k = from; k <= to; k++)
+                {
+                    for (char l = from; l <= to; l++)
+                    {
+                        if (j != skip && k != skip && l != skip)
+                        {
+                            Console.Write($"{j}{k}{l} ");
+                            count++;
+                        }
+                    }
+                }
+            }
+            Console.Write(count);
+        }
+    }
+}
