2e8f77ed860b11d080139ea453b6cbff951c399f
Kristiyan Enchev <kristiqnenchevv@gmail.com>
2020-12-17 13:05:19 +0200

    New Add

---
+++ Programming Basics/Extra Exercises/Nested-Loops-Exercises/10. Profit/10. Profit/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+class Program
+{
+    static void Main(string[] args)
+    {
+        int lev1 = int.Parse(Console.ReadLine());
+        int lev2 = int.Parse(Console.ReadLine());
+        int lev5 = int.Parse(Console.ReadLine());
+        int money = int.Parse(Console.ReadLine());
+        for (int i = 0; i <= lev1; i++)
+        {
+            for (int j = 0; j <= lev2; j++)
+            {
+                for (int k = 0; k <= lev5; k++)
+                {
+                    if (i * 1 + j * 2 + k * 5 == money)
+                    {
+                        Console.WriteLine($"{i} * 1 lv. + {j} * 2 lv. + {k} * 5 lv. = {money} lv.");
+                    }
+                }
+            }
+        }
+    }
+}
