1e712638894d042dae29177d961b1baf4d5bb10f
Kristiyan Enchev <kristiqnenchevv@gmail.com>
2020-12-17 13:07:11 +0200

    New Add

---
+++ Programming Basics/Extra Exercises/Nested-Loops-Exercises/13. Prime Pairs/13. Prime Pairs/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int firstStart = int.Parse(Console.ReadLine());
+        int secondStart = int.Parse(Console.ReadLine());
+        int maxFirt = int.Parse(Console.ReadLine());
+        int maxSecond = int.Parse(Console.ReadLine());
+        for (int i = firstStart; i <= firstStart + maxFirt; i++)
+        {
+            for (int j = secondStart; j <= secondStart + maxSecond; j++)
+            {
+                if (i % 2 != 0 && i % 3 != 0 && i % 5 != 0 && i % 7 != 0 && j % 2 != 0 && j % 3 != 0 && j % 5 != 0 && j % 7 != 0)
+                {
+                    Console.WriteLine($"{i}{j}");
+                }
+            }
+        }
+    }
+}
