02af5c5d0c707c530dc3f6ac742bc943f30039ab
Kristiyan Enchev <kristiqnenchevv@gmail.com>
2020-12-17 13:04:10 +0200

    New Add

---
+++ Programming Basics/Extra Exercises/Nested-Loops-Exercises/08. Secret Door's Lock/08. Secret Door's Lock/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+class Program
+{
+    static void Main(string[] args)
+    {
+        int firstTop = int.Parse(Console.ReadLine());
+        int secondTop = int.Parse(Console.ReadLine());
+        int thurdTop = int.Parse(Console.ReadLine());
+        for (int i = 1; i <= firstTop; i++)
+        {
+            for (int j = 1; j <= secondTop; j++)
+            {
+                for (int k = 1; k <= thurdTop; k++)
+                {
+                    if (i % 2 == 0 && k % 2 == 0)
+                    {
+                        if (j == 2 || j == 3 || j == 5 || j == 7)
+                        {
+                            Console.WriteLine($"{i} {j} {k}");
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
