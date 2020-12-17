791bac08d3d59aba2f366d87d8b3f16cc4f7897a
Kristiyan Enchev <kristiqnenchevv@gmail.com>
2020-12-17 13:02:51 +0200

    New Add

---
+++ Programming Basics/Extra Exercises/Nested-Loops-Exercises/06. Wedding Seats/06. Wedding Seats/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace ConsoleApp44
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char sector = char.Parse(Console.ReadLine());
+            int row = int.Parse(Console.ReadLine());
+            int oddSitCount = int.Parse(Console.ReadLine());
+            int count = 0;
+            for (char i = 'A'; i <= sector; i++)
+            {
+                for (int j = 1; j <= row; j++)
+                {
+                    if (j % 2 != 0)
+                    {
+                        for (int k = 97; k < 97 + oddSitCount; k++)
+                        {
+                            Console.WriteLine($"{i}{j}{(char)k}");
+                            count++;
+                        }
+                    }
+                    else
+                    {
+                        for (int t = 97; t < 97 + oddSitCount + 2; t++)
+                        {
+                            Console.WriteLine($"{i}{j}{(char)t}");
+                            count++;
+                        }
+                    }
+                }
+                row++;
+            }
+            Console.WriteLine(count);
+        }
+    }
+}
