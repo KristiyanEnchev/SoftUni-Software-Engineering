d3fb1e638b32bf10310548d116ab670c28cef999
Kristiyan Enchev <kristiqnenchevv@gmail.com>
2020-12-18 18:44:23 +0200

    New Add

---
+++ Programming Basics/Extra Exercises/While-Loop-Exercises/02. Report System/02. Report System/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+
+namespace ConsoleApp43
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double totalAmount = double.Parse(Console.ReadLine());
+            double moneyGathered = 0;
+            string comand = Console.ReadLine();
+            int transCount = 1;
+            double cardPay = 0;
+            double cashPay = 0;
+            int cardCount = 0;
+            int cashCount = 0;
+            bool end = false;
+            while (true)
+            {
+                if (comand == "End")
+                {
+                    end = true;
+                    break;
+                }
+                double price = double.Parse(comand);
+                if (transCount % 2 == 0)
+                {
+                    if (price < 10)
+                    {
+                        Console.WriteLine("Error in transaction!");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Product sold!");
+                        cardCount++;
+                        cardPay += price;
+                        moneyGathered += price;
+                    }
+                }
+                else
+                {
+                    if (price > 100)
+                    {
+                        Console.WriteLine("Error in transaction!");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Product sold!");
+                        cashCount++;
+                        cashPay += price;
+                        moneyGathered += price;
+                    }
+                }
+                if (moneyGathered >= totalAmount)
+                {
+                    break;
+                }
+                comand = Console.ReadLine();
+                transCount++;
+            }
+            if (end)
+            {
+                Console.WriteLine("Failed to collect required money for charity.");
+            }
+            else
+            {
+                Console.WriteLine($"Average CS: {cashPay / cashCount:f2}");
+                Console.WriteLine($"Average CC: {cardPay / cardCount:f2}");
+            }
+        }
+    }
+}
