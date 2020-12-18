68121d660f8ce628e74056877b740dab96b48394
Kristiyan Enchev <kristiqnenchevv@gmail.com>
2020-12-18 18:46:11 +0200

    New Add

---
+++ Programming Basics/Extra Exercises/While-Loop-Exercises/05. Average Number/05. Average Number/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace ConsoleApp44
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = int.Parse(Console.ReadLine());
+            double total = 0;
+            for (int i = 1; i <= num; i++)
+            {
+                double num1 = double.Parse(Console.ReadLine());
+                total += num1;
+            }
+            Console.WriteLine($"{total / num:f2}");
+        }
+    }
+}
