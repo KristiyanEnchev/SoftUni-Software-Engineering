b4da8d1dc87c10d1eff247a0bc32a262be0c0d25
Kristiyan Enchev <kristiqnenchevv@gmail.com>
2020-12-18 18:45:00 +0200

    New Add

---
+++ Programming Basics/Extra Exercises/While-Loop-Exercises/03. Stream Of Letters/03. Stream Of Letters/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+
+namespace ConsoleApp43
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string comand = Console.ReadLine();
+            int c = 0;
+            int o = 0;
+            int n = 0;
+            string word = "";
+            string finalWord = "";
+            while (true)
+            {
+                if (comand == "End")
+                {
+                    break;
+                }
+                char simbol = char.Parse(comand);
+                if (simbol >= 65 && simbol <= 90 || simbol >= 97 && simbol <= 122)
+                {
+
+                    if (simbol == 'c')
+                    {
+                        if (c > 0)
+                        {
+                            word += char.ToString(simbol);
+                        }
+                        c++;
+                    }
+                    else if (simbol == 'o')
+                    {
+                        if (o > 0)
+                        {
+                            word += char.ToString(simbol);
+                        }
+                        o++;
+                    }
+                    else if (simbol == 'n')
+                    {
+                        if (n > 0)
+                        {
+                            word += char.ToString(simbol);
+                        }
+                        n++;
+                    }
+                    else
+                    {
+                        word += char.ToString(simbol);
+                    }
+                    if (c > 0 && o > 0 & n > 0)
+                    {
+                        finalWord += word;
+                        c = 0;
+                        o = 0;
+                        n = 0;
+                        word = " ";
+                    }
+                }
+                comand = Console.ReadLine();
+            }
+            Console.WriteLine(finalWord);
+        }
+    }
+}
