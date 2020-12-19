e026b1c3bf3760f55c8082412107c959c53fcdac
Kristiyan Enchev <kristiqnenchevv@gmail.com>
2020-12-19 19:02:35 +0200

    New Add

---
+++ Programming Basics/Exams/Programming Basics Online Exam - 6 and 7 July 2019/04. Renovation/04. Renovation/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace ConsoleApp40
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double hight = double.Parse(Console.ReadLine());
+            double width = double.Parse(Console.ReadLine());
+            double procent = double.Parse(Console.ReadLine());
+            double procentage = procent * 0.01;
+            double area = hight * width * 4;
+            double totalForPainting = Math.Ceiling(area - (area * procentage));
+            bool tired = false;
+            while (true)
+            {
+                string comand = Console.ReadLine();
+                if (comand == "Tired!")
+                {
+                    tired = true;
+                    break;
+                }
+                double paintCount = double.Parse(comand);
+                totalForPainting -= paintCount;
+                if (totalForPainting <= 0)
+                {
+                    break;
+                }
+            }
+            if (tired)
+            {
+                Console.WriteLine($"{totalForPainting} quadratic m left.");
+            }
+            else if (totalForPainting < 0)
+            {
+                Console.WriteLine($"All walls are painted and you have {Math.Abs(totalForPainting)} l paint left!");
+            }
+            else if (totalForPainting == 0)
+            {
+                Console.WriteLine($"All walls are painted! Great job, Pesho!");
+            }
+        }
+    }
+}
