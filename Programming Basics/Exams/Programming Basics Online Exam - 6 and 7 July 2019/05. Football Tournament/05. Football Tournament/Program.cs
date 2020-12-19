1a920a9d5fc289296f2b1e532c698aa791f148f6
Kristiyan Enchev <kristiqnenchevv@gmail.com>
2020-12-19 19:02:59 +0200

    New Add

---
+++ Programming Basics/Exams/Programming Basics Online Exam - 6 and 7 July 2019/05. Football Tournament/05. Football Tournament/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace ConsoleApp40
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string teamName = Console.ReadLine();
+            int gameCount = int.Parse(Console.ReadLine());
+            int pointsCount = 0;
+            double w = 0;
+            double d = 0;
+            double l = 0;
+            double procent = 0;
+            for (int i = 1; i <= gameCount; i++)
+            {
+                char condition = char.Parse(Console.ReadLine());
+                if (condition == 'W')
+                {
+                    w++;
+                    pointsCount += 3;
+                }
+                else if (condition == 'D')
+                {
+                    d++;
+                    pointsCount += 1;
+                }
+                else if (condition == 'L')
+                {
+                    l++;
+                    pointsCount += 0;
+                }
+                procent = w / i * 100;
+            }
+            if (gameCount < 1)
+            {
+                Console.WriteLine($"{teamName} hasn't played any games during this season.");
+            }
+            else
+            {
+                Console.WriteLine($"{teamName} has won {pointsCount} points during this season.");
+                Console.WriteLine("Total stats:");
+                Console.WriteLine($"## W: {w}");
+                Console.WriteLine($"## D: {d}");
+                Console.WriteLine($"## L: {l}");
+                Console.WriteLine($"Win rate: {procent:f2}%");
+            }
+        }
+    }
+}
