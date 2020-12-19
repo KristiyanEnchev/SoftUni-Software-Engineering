a8164ecba084e87d5e02b44296e891bd8d627302
Kristiyan Enchev <kristiqnenchevv@gmail.com>
2020-12-19 19:03:42 +0200

    New Add

---
+++ Programming Basics/Exams/Programming Basics Online Exam - 6 and 7 July 2019/06. Name Game/06. Name Game/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace ConsoleApp40
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string winerName = "";
+            int points = 0;
+            double topPoints = double.MinValue;
+            string name = Console.ReadLine();
+            while (name != "Stop")
+            {
+                for (int i = 0; i < name.Length; i++)
+                {
+                    char letter = (char)name[i];
+                    int ASCII = letter;
+                    double num = double.Parse(Console.ReadLine());
+                    if (num == ASCII)
+                    {
+                        points += 10;
+                    }
+                    else
+                    {
+                        points += 2;
+                    }
+                }
+                if (points >= topPoints)
+                {
+                    topPoints = points;
+                    winerName = name;
+                }
+                points = 0;
+                name = Console.ReadLine();
+            }
+            Console.WriteLine($"The winner is {winerName} with {topPoints} points!");
+        }
+    }
+}
