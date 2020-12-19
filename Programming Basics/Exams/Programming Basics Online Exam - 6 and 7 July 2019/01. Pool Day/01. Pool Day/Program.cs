898fa7e67c8f6fb199a4326877e17a108915cff6
Kristiyan Enchev <kristiqnenchevv@gmail.com>
2020-12-19 18:33:48 +0200

    New Add

---
+++ Programming Basics/Exams/Programming Basics Online Exam - 6 and 7 July 2019/01. Pool Day/01. Pool Day/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace ConsoleApp40
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double peopleCount = double.Parse(Console.ReadLine());
+            double entrencePrice = double.Parse(Console.ReadLine());
+            double deckchairPrice = double.Parse(Console.ReadLine());
+            double umbrellaPrice = double.Parse(Console.ReadLine());
+            double entranceMoney = peopleCount * entrencePrice;
+            double deckchairCount = Math.Ceiling(peopleCount * 0.75);
+            double deckchairMoney = deckchairCount * deckchairPrice;
+            double umbrelaCount = Math.Ceiling(peopleCount / 2);
+            double umbrelaMoney = umbrelaCount * umbrellaPrice;
+            double total = umbrelaMoney + deckchairMoney + entranceMoney;
+            Console.WriteLine($"{total:f2} lv.");
+
+        }
+    }
+}
