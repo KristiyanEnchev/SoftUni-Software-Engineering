fa408fcc2ee4c0502a3e447d5cef79d2d4b58b2e
Kristiyan Enchev <kristiqnenchevv@gmail.com>
2020-12-20 22:13:29 +0200

    New Add

---
+++ Programming Basics/Exams/Programming Basics Online Exam - 6 and 7 April 2019/02. Godzilla vs. Kong/02. Godzilla vs. Kong/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Transactions;
+
+namespace ConsoleApp41
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double budget = double.Parse(Console.ReadLine());
+            int peopleCount = int.Parse(Console.ReadLine());
+            double clothsPrice = double.Parse(Console.ReadLine());
+            double decorPrice = budget * 0.10;
+            if (peopleCount > 150)
+            {
+                clothsPrice *= 0.90;
+            }
+            double totalPrice = decorPrice + clothsPrice * peopleCount;
+            if (budget < totalPrice)
+            {
+                Console.WriteLine("Not enough money!");
+                Console.WriteLine($"Wingard needs {totalPrice - budget:f2} leva more.");
+            }
+            else
+            {
+                Console.WriteLine("Action!");
+                Console.WriteLine($"Wingard starts filming with {budget - totalPrice:f2} leva left.");
+            }
+        }
+    }
+}
