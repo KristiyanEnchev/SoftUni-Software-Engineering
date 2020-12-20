ac855a7381b7105c6fb660b267b280a4c42da61d
Kristiyan Enchev <kristiqnenchevv@gmail.com>
2020-12-20 22:14:01 +0200

    New Add

---
+++ Programming Basics/Exams/Programming Basics Online Exam - 6 and 7 April 2019/04. Cinema Voucher/04. Cinema Voucher/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+
+namespace ConsoleApp41
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double vaucherPrice = double.Parse(Console.ReadLine());
+            int ticketsCount = 0;
+            int productCount = 0;
+            while (true)
+            {
+                if (vaucherPrice <= 0)
+                {
+                    break;
+                }
+                string stock = Console.ReadLine();
+                if (stock == "End")
+                {
+                    break;
+                }
+                if (stock.Length > 8)
+                {
+                    double moviePrice = 0;
+                    for (int i = 0; i < stock.Length; i++)
+                    {
+                        char letter = (char)stock[i];
+                        int ascii = letter;
+                        if (i < 2)
+                        {
+                            moviePrice += ascii;
+                            if (vaucherPrice < 0)
+                            {
+                                break;
+                            }
+                        }
+                        else
+                        {
+                            break;
+                        }
+                    }
+                    vaucherPrice -= moviePrice;
+                    if (vaucherPrice >= 0)
+                    {
+                        ticketsCount++;
+                    }
+                }
+                else if (stock.Length <= 8)
+                {
+                    for (int i = 0; i < stock.Length; i++)
+                    {
+                        char letter = (char)stock[i];
+                        int ascii = letter;
+                        if (i <= 0)
+                        {
+                            vaucherPrice -= ascii;
+                            if (vaucherPrice < 0)
+                            {
+                                break;
+                            }
+                            else if (vaucherPrice >= 0)
+                            {
+                                productCount++;
+                            }
+                        }
+                        else
+                        {
+                            break;
+                        }
+                    }
+                }
+            }
+            Console.WriteLine($"{ticketsCount}");
+            Console.WriteLine($"{productCount}");
+        }
+    }
+}
