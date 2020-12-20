a013f473301f1a0a6ee59a57204b26a42bdbfda2
Kristiyan Enchev <kristiqnenchevv@gmail.com>
2020-12-20 22:13:13 +0200

    New Add

---
+++ Programming Basics/Exams/Programming Basics Online Exam - 6 and 7 April 2019/01. Oscars ceremony/01. Oscars ceremony/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace ConsoleApp41
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double rent = double.Parse(Console.ReadLine());
+            double statuePrice = rent * 0.70;
+            double foodPrice = statuePrice * 0.85;
+            double music = foodPrice / 2;
+            double totalMoneyNeeded = rent + statuePrice + foodPrice + music;
+            Console.WriteLine($"{totalMoneyNeeded:f2}");
+        }
+    }
+}
