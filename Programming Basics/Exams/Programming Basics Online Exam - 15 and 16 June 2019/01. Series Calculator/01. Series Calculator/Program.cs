4a1706b7cb944176fb1284ce0c3b2c0b9d1d80fa
Kristiyan Enchev <kristiqnenchevv@gmail.com>
2020-12-21 17:37:51 +0200

    New Add

---
+++ Programming Basics/Exams/Programming Basics Online Exam - 15 and 16 June 2019/01. Series Calculator/01. Series Calculator/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Globalization;
+
+namespace ConsoleApp33
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = Console.ReadLine();
+            int seasonCount = int.Parse(Console.ReadLine());
+            int episodeCount = int.Parse(Console.ReadLine());
+            double time = double.Parse(Console.ReadLine());
+
+            double timeforAseason = episodeCount * (time * 1.20) + 10;
+            double totalTime = timeforAseason * seasonCount;
+            Console.WriteLine($"Total time needed to watch the {name} series is {totalTime} minutes.");
+
+        }
+    }
+}
