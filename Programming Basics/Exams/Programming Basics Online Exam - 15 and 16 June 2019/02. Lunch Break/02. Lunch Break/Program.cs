64f2857dfdb5839896e3cbce9062451323d6976d
Kristiyan Enchev <kristiqnenchevv@gmail.com>
2020-12-21 17:38:00 +0200

    New Add

---
+++ Programming Basics/Exams/Programming Basics Online Exam - 15 and 16 June 2019/02. Lunch Break/02. Lunch Break/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Globalization;
+
+namespace ConsoleApp33
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = Console.ReadLine();
+            int timeForsieris = int.Parse(Console.ReadLine());
+            int breakk = int.Parse(Console.ReadLine());
+
+            double lunchTime = breakk / 8.00;
+            double chillTime = breakk / 4.00;
+
+            double total = chillTime + lunchTime + timeForsieris;
+
+            if (breakk >= total)
+            {
+                Console.WriteLine($"You have enough time to watch {name} and left with {Math.Ceiling(breakk - total)} minutes free time.");
+            }
+            else
+            {
+                Console.WriteLine($"You don't have enough time to watch {name}, you need {Math.Ceiling(total - breakk)} more minutes.");
+            }
+        }
+    }
+}
