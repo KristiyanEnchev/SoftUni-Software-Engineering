7b48e2e6b3e3405e08f78ae5cd4cbd34de9bf549
Kristiyan Enchev <kristiqnenchevv@gmail.com>
2020-12-21 17:39:45 +0200

    New Add

---
+++ Programming Basics/Exams/Programming Basics Online Exam - 15 and 16 June 2019/06. Movie Tickets/06. Movie Tickets/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace ConsoleApp33
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a1 = int.Parse(Console.ReadLine());
+            int a2 = int.Parse(Console.ReadLine());
+            int n = int.Parse(Console.ReadLine());
+            int num = 0;
+            int num2 = 0;
+            int num3 = 0;
+            for (int i = a1; i <= a2 - 1; i++)
+            {
+                char letter = (char)i;
+                if (i % 2 != 0)
+                {
+                    double total = 0;
+                    for (int j = 1; j <= n - 1; j++)
+                    {
+                        for (int k = 1; k <= (n / 2) - 1; k++)
+                        {
+                            total = i + j + k;
+                            if (total % 2 != 0)
+                            {
+                                num = i;
+                                num2 = j;
+                                num3 = k;
+                                Console.WriteLine($"{letter}-{num2}{num3}{num}");
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
