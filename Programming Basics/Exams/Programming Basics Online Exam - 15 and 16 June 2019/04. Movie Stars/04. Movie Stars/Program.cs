3d9436d0110e687a8da3e9a80d37e1eaae53fcdd
Kristiyan Enchev <kristiqnenchevv@gmail.com>
2020-12-21 17:39:00 +0200

    New Add

---
+++ Programming Basics/Exams/Programming Basics Online Exam - 15 and 16 June 2019/04. Movie Stars/04. Movie Stars/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace ConsoleApp34
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double budget = double.Parse(Console.ReadLine());
+            bool action = false;
+            while (budget >= 0)
+            {
+                string name = Console.ReadLine();
+                if (name == "ACTION")
+                {
+                    action = true;
+                    break;
+                }
+                if (name.Length <= 15)
+                {
+                    double money = double.Parse(Console.ReadLine());
+                    budget -= money;
+                }
+                else
+                {
+                    budget -= budget * 0.20;
+                }
+            }
+            if (action)
+            {
+                Console.WriteLine($"We are left with {budget:f2} leva.");
+            }
+            else if (budget <= 0)
+            {
+                Console.WriteLine($"We need {Math.Abs(budget):F2} leva for our actors.");
+            }
+        }
+    }
+}
