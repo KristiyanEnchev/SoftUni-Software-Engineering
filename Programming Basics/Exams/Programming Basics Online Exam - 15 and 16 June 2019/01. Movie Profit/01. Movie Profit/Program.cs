60b64e721d38178c041abd22c75f0c8737ebed76
Kristiyan Enchev <kristiqnenchevv@gmail.com>
2020-12-21 17:37:38 +0200

    New Add

---
+++ Programming Basics/Exams/Programming Basics Online Exam - 15 and 16 June 2019/01. Movie Profit/01. Movie Profit/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+class Program
+{
+    static void Main(string[] args)
+    {
+        string movieName = Console.ReadLine();
+        int days = int.Parse(Console.ReadLine());
+        int ticketCount = int.Parse(Console.ReadLine());
+        double ticketPrice = double.Parse(Console.ReadLine());
+        double procent = double.Parse(Console.ReadLine());
+
+        double moneyADay = ticketPrice * ticketCount;
+        double totalprice = moneyADay * days;
+        double moneyForCinema = (totalprice * procent) / 100;
+        Console.WriteLine($"The profit from the movie {movieName} is {totalprice - moneyForCinema:f2} lv.");
+    }
+}
