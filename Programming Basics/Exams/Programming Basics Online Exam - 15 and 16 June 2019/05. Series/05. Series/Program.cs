95c4677f873d241c0f31f56774fd8d7da43337c3
Kristiyan Enchev <kristiqnenchevv@gmail.com>
2020-12-21 17:39:10 +0200

    New Add

---
+++ Programming Basics/Exams/Programming Basics Online Exam - 15 and 16 June 2019/05. Series/05. Series/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace ConsoleApp33
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double budget = double.Parse(Console.ReadLine());
+            int sierisCount = int.Parse(Console.ReadLine());
+            if (sierisCount < 1 || sierisCount > 10)
+            {
+                sierisCount = int.Parse(Console.ReadLine());
+            }
+            double totalPrice = 0;
+            for (int i = 1; i <= sierisCount; i++)
+            {
+                string name = Console.ReadLine();
+                double price = double.Parse(Console.ReadLine());
+                if (name == "Thrones")
+                {
+                    price /= 2;
+                }
+                else if (name == "Lucifer")
+                {
+                    price *= 0.60;
+                }
+                else if (name == "Protector")
+                {
+                    price *= 0.70;
+                }
+                else if (name == "TotalDrama")
+                {
+                    price *= 0.80;
+                }
+                else if (name == "Area")
+                {
+                    price *= 0.90;
+                }
+                totalPrice += price;
+            }
+            if (budget >= totalPrice)
+            {
+                Console.WriteLine($"You bought all the series and left with {budget - totalPrice:f2} lv.");
+            }
+            else
+            {
+                Console.WriteLine($"You need {totalPrice - budget:f2} lv. more to buy the series!");
+            }
+        }
+    }
+}
