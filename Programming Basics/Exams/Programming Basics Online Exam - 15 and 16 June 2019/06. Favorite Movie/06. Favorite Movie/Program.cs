a53fd11ce52b236df11136823929a6319fe105b3
Kristiyan Enchev <kristiqnenchevv@gmail.com>
2020-12-21 17:39:33 +0200

    New Add

---
+++ Programming Basics/Exams/Programming Basics Online Exam - 15 and 16 June 2019/06. Favorite Movie/06. Favorite Movie/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace ConsoleApp33
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string movie = Console.ReadLine();
+            int movieCount = 0;
+            double bestPoints = 0;
+            string nameOfTheBEstMovie = "";
+            while (movie != "STOP")
+            {
+                movieCount++;
+                int lenght = movie.Length;
+                if (movieCount >= 7)
+                {
+                    break;
+                }
+                double totalCount = 0;
+                for (int i = 0; i < lenght; i++)
+                {
+                    char leter = movie[i];
+                    int leterAsInt = leter;
+                    if (char.IsUpper(leter))
+                    {
+                        leterAsInt -= lenght;
+                    }
+                    if (char.IsLower(leter))
+                    {
+                        leterAsInt -= lenght * 2;
+                    }
+                    totalCount += leterAsInt;
+
+                }
+                if (totalCount > bestPoints)
+                {
+                    nameOfTheBEstMovie = movie;
+                    bestPoints = totalCount;
+                }
+                movie = Console.ReadLine();
+            }
+            if (movieCount >= 7)
+            {
+                Console.WriteLine("The limit is reached.");
+            }
+            Console.WriteLine($"The best movie for you is {nameOfTheBEstMovie} with {bestPoints} ASCII sum.");
+        }
+    }
+}
