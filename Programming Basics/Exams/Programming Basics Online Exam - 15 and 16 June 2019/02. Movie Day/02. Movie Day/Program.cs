f4f2cec64f68a8b8cfe67bd8c0c493716130c5d5
Kristiyan Enchev <kristiqnenchevv@gmail.com>
2020-12-21 17:38:25 +0200

    New Add

---
+++ Programming Basics/Exams/Programming Basics Online Exam - 15 and 16 June 2019/02. Movie Day/02. Movie Day/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace ConsoleApp34
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double timeForShoot = double.Parse(Console.ReadLine());
+            double shootingCount = double.Parse(Console.ReadLine());
+            double timeForShooting = double.Parse(Console.ReadLine());
+            double timeForPreparation = timeForShoot * 0.15;
+            double time = shootingCount * timeForShooting + timeForPreparation;
+            if (time <= timeForShoot)
+            {
+                Console.WriteLine($"You managed to finish the movie on time! You have {Math.Round(timeForShoot - time)} minutes left!");
+            }
+            else
+            {
+                Console.WriteLine($"Time is up! To complete the movie you need {Math.Round(time - timeForShoot)} minutes.");
+            }
+        }
+    }
+}
