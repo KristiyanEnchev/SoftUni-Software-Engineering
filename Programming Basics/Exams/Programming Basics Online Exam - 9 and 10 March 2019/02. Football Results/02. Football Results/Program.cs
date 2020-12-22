1849ce1221d3e81153867173f6566797c799f9cb
Kristiyan Enchev <kristiqnenchevv@gmail.com>
2020-12-22 17:16:39 +0200

    New Add

---
+++ Programming Basics/Exams/Programming Basics Online Exam - 9 and 10 March 2019/02. Football Results/02. Football Results/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+
+namespace ConsoleApp42
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int won = 0;
+            int loose = 0;
+            int draw = 0;
+            string firstResult = Console.ReadLine();
+            int num = firstResult[0];
+            int num2 = firstResult[2];
+            if (num > num2)
+            {
+                won++;
+            }
+            else if (num2 > num)
+            {
+                loose++;
+            }
+            else
+            {
+                draw++;
+            }
+            string thurdResult = Console.ReadLine();
+            int numm = thurdResult[0];
+            int numm2 = thurdResult[2];
+            if (numm > numm2)
+            {
+                won++;
+            }
+            else if (numm2 > numm)
+            {
+                loose++;
+            }
+            else
+            {
+                draw++;
+            }
+            string secondResult = Console.ReadLine();
+            int num1 = secondResult[0];
+            int num3 = secondResult[2];
+            if (num1 > num3)
+            {
+                won++;
+            }
+            else if (num3 > num1)
+            {
+                loose++;
+            }
+            else
+            {
+                draw++;
+            }
+            Console.WriteLine($"Team won {won} games.");
+            Console.WriteLine($"Team lost {loose} games.");
+            Console.WriteLine($" Drawn games: {draw}");
+        }
+    }
+}
