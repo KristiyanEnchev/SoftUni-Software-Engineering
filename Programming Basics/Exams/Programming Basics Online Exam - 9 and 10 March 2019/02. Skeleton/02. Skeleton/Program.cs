e273ef8e7bdfbf18a23e6532d6f33ff74b51fd5a
Kristiyan Enchev <kristiqnenchevv@gmail.com>
2020-12-22 17:16:49 +0200

    New Add

---
+++ Programming Basics/Exams/Programming Basics Online Exam - 9 and 10 March 2019/02. Skeleton/02. Skeleton/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace ConsoleApp42
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double minuts = double.Parse(Console.ReadLine());
+            double seconds = double.Parse(Console.ReadLine());
+            double lenght = double.Parse(Console.ReadLine());
+            double secFor100Metters = double.Parse(Console.ReadLine());
+            double min = minuts * 60;
+            double kwotTime = min + seconds;
+            double resistance = lenght / 120;
+            double resistedTime = resistance * 2.5;
+            double personTime = lenght / 100 * secFor100Metters;
+            double total = personTime - resistedTime;
+            if (total <= kwotTime)
+            {
+                Console.WriteLine($"Marin Bangiev won an Olympic quota!");
+                Console.WriteLine($"His time is {total:f3}.");
+            }
+            else
+            {
+                Console.WriteLine($"No, Marin failed! He was {total - kwotTime:f3} second slower.");
+            }
+        }
+    }
+}
