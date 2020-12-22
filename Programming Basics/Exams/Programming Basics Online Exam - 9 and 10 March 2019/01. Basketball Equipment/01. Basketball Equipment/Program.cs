05549473b834f27dd100594f9c837da612c3a942
Kristiyan Enchev <kristiqnenchevv@gmail.com>
2020-12-22 17:16:22 +0200

    New Add

---
+++ Programming Basics/Exams/Programming Basics Online Exam - 9 and 10 March 2019/01. Basketball Equipment/01. Basketball Equipment/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace ConsoleApp43
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double tax = double.Parse(Console.ReadLine());
+            double snekers = tax * 0.60;
+            double outfit = snekers * 0.80;
+            double ball = outfit / 4;
+            double accesory = ball / 5;
+            double total = tax + snekers + outfit + ball + accesory;
+            Console.WriteLine($"{total:f2}");
+        }
+    }
+}
