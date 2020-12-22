9da72cdc0c63d98ce694d832afafa663d9a031df
Kristiyan Enchev <kristiqnenchevv@gmail.com>
2020-12-22 17:17:16 +0200

    New Add

---
+++ Programming Basics/Exams/Programming Basics Online Exam - 9 and 10 March 2019/04. Darts/04. Darts/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Net;
+using System.Runtime;
+
+namespace ConsoleApp42
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string playerName = Console.ReadLine();
+            double point = 301;
+            bool giveUp = false;
+            double unseccesful = 0;
+            double succesful = 0;
+            while (true)
+            {
+                if (point == 0)
+                {
+                    break;
+                }
+                string sectorType = Console.ReadLine();
+                if (sectorType == "Retire")
+                {
+                    giveUp = true;
+                    break;
+                }
+                double pointInSector = double.Parse(Console.ReadLine());
+                if (sectorType == "Double")
+                {
+                    pointInSector *= 2;
+                }
+                else if (sectorType == "Triple")
+                {
+                    pointInSector *= 3;
+                }
+                point -= pointInSector;
+                if (point < 0)
+                {
+                    unseccesful++;
+                    point += pointInSector;
+                    continue;
+                }
+                else
+                {
+                    succesful++;
+                }
+            }
+            if (giveUp)
+            {
+                Console.WriteLine($"{playerName} retired after {unseccesful} unsuccessful shots.");
+            }
+            else
+            {
+                Console.WriteLine($"{playerName} won the leg with {succesful} shots.");
+            }
+        }
+    }
+}
