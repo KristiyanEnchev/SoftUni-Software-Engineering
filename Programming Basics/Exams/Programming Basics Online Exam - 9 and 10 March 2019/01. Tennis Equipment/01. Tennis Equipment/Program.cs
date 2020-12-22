614511a70b244f6bc3a91defc912ff208549651f
Kristiyan Enchev <kristiqnenchevv@gmail.com>
2020-12-22 17:16:30 +0200

    New Add

---
+++ Programming Basics/Exams/Programming Basics Online Exam - 9 and 10 March 2019/01. Tennis Equipment/01. Tennis Equipment/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace ConsoleApp43
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double tenisRocketPrice = double.Parse(Console.ReadLine());
+            int tenisRocketCount = int.Parse(Console.ReadLine());
+            int sneakersCount = int.Parse(Console.ReadLine());
+            double tenisRockets = tenisRocketPrice * tenisRocketCount;
+            double sneakersPrice = tenisRocketPrice / 6;
+            double totalSneakersPrice = sneakersCount * sneakersPrice;
+            double diffrentEquipment = (tenisRockets + totalSneakersPrice) * 0.20;
+            double total = tenisRockets + totalSneakersPrice + diffrentEquipment;
+            Console.WriteLine($"Price to be paid by Djokovic {Math.Floor(total / 8)}");
+            Console.WriteLine($"Price to be paid by sponsors {Math.Ceiling(total - (total / 8))}");
+        }
+    }
+}
