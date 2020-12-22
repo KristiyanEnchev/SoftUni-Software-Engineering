c6d8cf9ed6ae43d19e752d8b0462c637fed49731
Kristiyan Enchev <kristiqnenchevv@gmail.com>
2020-12-22 17:17:53 +0200

    New Add

---
+++ Programming Basics/Exams/Programming Basics Online Exam - 9 and 10 March 2019/05. Tennis Ranklist/05. Tennis Ranklist/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Net;
+using System.Runtime;
+
+namespace ConsoleApp42
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double turnamentCount = double.Parse(Console.ReadLine());
+            double startingPoints = double.Parse(Console.ReadLine());
+            double points = 0;
+            int wonTurnamentsCount = 0;
+            for (int i = 1; i <= turnamentCount; i++)
+            {
+                string finishType = Console.ReadLine();
+                if (finishType == "W")
+                {
+                    points += 2000;
+                    wonTurnamentsCount++;
+                }
+                else if (finishType == "F")
+                {
+                    points += 1200;
+                }
+                else if (finishType == "SF")
+                {
+                    points += 720;
+                }
+            }
+            double averigePoint = points / turnamentCount;
+            double finishPoints = startingPoints + points;
+            double procentageWon = wonTurnamentsCount / turnamentCount * 100;
+            Console.WriteLine($"Final points: {finishPoints}");
+            Console.WriteLine($"Average points: {Math.Floor(averigePoint)}");
+            Console.WriteLine($"{procentageWon:f2}%");
+        }
+    }
+}
