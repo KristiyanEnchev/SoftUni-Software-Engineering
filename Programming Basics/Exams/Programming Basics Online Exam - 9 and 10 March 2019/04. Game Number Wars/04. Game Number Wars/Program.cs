37353766491e8214bc44a12451b77b898bb777ed
Kristiyan Enchev <kristiqnenchevv@gmail.com>
2020-12-22 17:17:37 +0200

    New Add

---
+++ Programming Basics/Exams/Programming Basics Online Exam - 9 and 10 March 2019/04. Game Number Wars/04. Game Number Wars/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace ConsoleApp42
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string firstPlayerName = Console.ReadLine();
+            string secondPlayerName = Console.ReadLine();
+            double firstPoints = 0;
+            double secondPoints = 0;
+            while (true)
+            {
+                string comand = Console.ReadLine();
+                if (comand == "End of game")
+                {
+                    Console.WriteLine($"{firstPlayerName} has {firstPoints} points");
+                    Console.WriteLine($"{secondPlayerName} has {secondPoints} points");
+                    break;
+                }
+                int first = int.Parse(comand);
+                int second = int.Parse(Console.ReadLine());
+                if (first > second)
+                {
+                    firstPoints += first - second;
+                }
+                else if (second > first)
+                {
+                    secondPoints += second - first;
+                }
+                else
+                {
+                    Console.WriteLine("Number wars!");
+                    first = int.Parse(Console.ReadLine());
+                    second = int.Parse(Console.ReadLine());
+                    if (first > second)
+                    {
+                        Console.WriteLine($"{firstPlayerName} is winner with {firstPoints} points");
+                        break;
+                    }
+                    else
+                    {
+                        Console.WriteLine($"{secondPlayerName} is winner with {secondPoints} points");
+                        break;
+                    }
+                }
+            }
+        }
+    }
+}
