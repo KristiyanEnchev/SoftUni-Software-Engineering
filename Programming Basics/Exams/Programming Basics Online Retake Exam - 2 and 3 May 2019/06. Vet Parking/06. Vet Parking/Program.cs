5d9a5c4a2f564702a603b29e84553cd88fbf4359
Kristiyan Enchev <kristiqnenchevv@gmail.com>
2020-12-23 17:44:23 +0200

    New Add

---
+++ Programming Basics/Exams/Programming Basics Online Retake Exam - 2 and 3 May 2019/06. Vet Parking/06. Vet Parking/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace ConsoleApp36
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int dayCount = int.Parse(Console.ReadLine());
+            int hourCount = int.Parse(Console.ReadLine());
+            double price = 0;
+            double total = 0;
+            for (int i = 1; i <= dayCount; i++)
+            {
+                price = 0;
+                for (int j = 1; j <= hourCount; j++)
+                {
+                    if (i % 2 == 0 && j % 2 != 0)
+                    {
+                        price += 2.50;
+                    }
+                    else if (i % 2 != 0 && j % 2 == 0)
+                    {
+                        price += 1.25;
+                    }
+                    else
+                    {
+                        price += 1.00;
+                    }
+                }
+                Console.WriteLine($"Day: {i} - {price:f2} leva");
+                total += price;
+            }
+            Console.WriteLine($"Total: {total:f2} leva");
+        }
+    }
+}
