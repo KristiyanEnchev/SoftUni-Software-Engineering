7d1f48182a1ec98233e712d1b2988f76d0a5938a
Kristiyan Enchev <kristiqnenchevv@gmail.com>
2020-12-23 17:43:58 +0200

    New Add

---
+++ Programming Basics/Exams/Programming Basics Online Retake Exam - 2 and 3 May 2019/04. Tourist Shop/04. Tourist Shop/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace _04.TouristShop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double buget = double.Parse(Console.ReadLine());
+            bool sTop = false;
+            double productsPrice = 0;
+            int count = 0;
+            while (productsPrice <= buget)
+            {
+                string nameOfPrroduct = Console.ReadLine();
+                if (nameOfPrroduct == "Stop")
+                {
+                    sTop = true;
+                    break;
+                }
+                count++;
+                double priceOfProduct = double.Parse(Console.ReadLine());
+                if (count % 3 == 0)
+                {
+                    priceOfProduct /= 2;
+                }
+                productsPrice += priceOfProduct;
+            }
+            if (sTop)
+            {
+                Console.WriteLine($"You bought {count} products for {productsPrice:f2} leva.");
+            }
+            else
+            {
+                Console.WriteLine($"You don't have enough money!");
+                Console.WriteLine($"You need {productsPrice - buget:f2} leva!");
+            }
+        }
+    }
+}
