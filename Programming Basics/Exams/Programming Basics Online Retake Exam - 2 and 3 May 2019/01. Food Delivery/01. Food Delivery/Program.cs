b02bfb6498969c8da44267d9931bfd841dc5f7d5
Kristiyan Enchev <kristiqnenchevv@gmail.com>
2020-12-23 17:43:25 +0200

    New Add

---
+++ Programming Basics/Exams/Programming Basics Online Retake Exam - 2 and 3 May 2019/01. Food Delivery/01. Food Delivery/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace ConsoleApp36
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double chikenMenuCount = double.Parse(Console.ReadLine());
+            double fishMenuCount = double.Parse(Console.ReadLine());
+            double vegitarianMenuCount = double.Parse(Console.ReadLine());
+
+            double total = chikenMenuCount * 10.35 + fishMenuCount * 12.40 + vegitarianMenuCount * 8.15;
+            double totalWithDesert = total * 1.20;
+            double totalPrice = totalWithDesert + 2.50;
+            Console.WriteLine($"Total: {totalPrice:f2}");
+        }
+    }
+}
