ef4c49247be038e1e86d986b6d38cf3e3efb736e
Kristiyan Enchev <kristiqnenchevv@gmail.com>
2020-12-23 17:44:07 +0200

    New Add

---
+++ Programming Basics/Exams/Programming Basics Online Retake Exam - 2 and 3 May 2019/05. Division Without/05. Division Without/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace ConsoleApp36
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double numCount = double.Parse(Console.ReadLine());
+            if (numCount < 1 || numCount > 1000)
+            {
+                numCount = double.Parse(Console.ReadLine());
+            }
+            int p1 = 0;
+            int p2 = 0;
+            int p3 = 0;
+            for (int i = 1; i <= numCount; i++)
+            {
+                double num = double.Parse(Console.ReadLine());
+                if (num % 2 == 0)
+                {
+                    p1++;
+                }
+                if (num % 3 == 0)
+                {
+                    p2++;
+                }
+                if (num % 4 == 0)
+                {
+                    p3++;
+                }
+            }
+            Console.WriteLine($"{p1 / numCount * 100:f2}%");
+            Console.WriteLine($"{p2 / numCount * 100:f2}%");
+            Console.WriteLine($"{p3 / numCount * 100:f2}%");
+        }
+    }
+}
