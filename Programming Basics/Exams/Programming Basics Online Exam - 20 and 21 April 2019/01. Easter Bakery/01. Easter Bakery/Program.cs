159a5146db18b67ab5a349fd5748de470877ace0
Kristiyan Enchev <kristiqnenchevv@gmail.com>
2020-12-24 17:42:07 +0200

    New Add

---
+++ Programming Basics/Exams/Programming Basics Online Exam - 20 and 21 April 2019/01. Easter Bakery/01. Easter Bakery/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace ConsoleApp28
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double FlourPrice = double.Parse(Console.ReadLine());
+            double flourCount = double.Parse(Console.ReadLine());
+            double sugarCount = double.Parse(Console.ReadLine());
+            double eggCount = double.Parse(Console.ReadLine());
+            double mayCount = double.Parse(Console.ReadLine());
+
+            double sugarPrice = FlourPrice * 0.75;
+            double eggPrice = FlourPrice * 1.10;
+            double mayPrice = sugarPrice * 0.20;
+
+            double total = FlourPrice * flourCount + sugarPrice * sugarCount + eggPrice * eggCount + mayPrice * mayCount;
+
+            Console.WriteLine($"{total:f2}");
+        }
+    }
+}
