cdfca8cfb265ff8d16044595935454a40a94e37b
Kristiyan Enchev <kristiqnenchevv@gmail.com>
2020-12-24 17:42:38 +0200

    New Add

---
+++ Programming Basics/Exams/Programming Basics Online Exam - 20 and 21 April 2019/02. Easter Party/02. Easter Party/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace ConsoleApp28
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double peopleCount = double.Parse(Console.ReadLine());
+            double entrencePrice = double.Parse(Console.ReadLine());
+            double budget = double.Parse(Console.ReadLine());
+            double cakePrice = budget * 0.10;
+
+            if (peopleCount >= 10 && peopleCount <= 15)
+            {
+                entrencePrice *= 0.85;
+            }
+            else if (peopleCount > 15 && peopleCount <= 20)
+            {
+                entrencePrice *= 0.80;
+            }
+            else if (peopleCount > 20)
+            {
+                entrencePrice *= 0.75;
+            }
+
+            double total = (peopleCount * entrencePrice) + cakePrice;
+
+            if (total <= budget)
+            {
+                Console.WriteLine($"It is party time! {budget - total:f2} leva left.");
+            }
+            else
+            {
+                Console.WriteLine($"No party! {total - budget:f2} leva needed.");
+            }
+        }
+    }
+}
