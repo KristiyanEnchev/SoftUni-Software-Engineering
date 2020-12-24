7f18898592e28f057fe2d2088bf3c202583f13a5
Kristiyan Enchev <kristiqnenchevv@gmail.com>
2020-12-24 17:42:55 +0200

    New Add

---
+++ Programming Basics/Exams/Programming Basics Online Exam - 20 and 21 April 2019/03. Painting Eggs/03. Painting Eggs/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Security.Cryptography;
+
+namespace ConsoleApp28
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string eggSize = Console.ReadLine();
+            string eggColor = Console.ReadLine();
+            double eggCount = double.Parse(Console.ReadLine());
+            double price = 0;
+
+            if (eggSize == "Large")
+            {
+                switch (eggColor)
+                {
+                    case "Red":
+                        price = 16;
+                        break;
+                    case "Green":
+                        price = 12;
+                        break;
+                    case "Yellow":
+                        price = 9;
+                        break;
+                }
+            }
+            else if (eggSize == "Medium")
+            {
+                switch (eggColor)
+                {
+                    case "Red":
+                        price = 13;
+                        break;
+                    case "Green":
+                        price = 9;
+                        break;
+                    case "Yellow":
+                        price = 7;
+                        break;
+                }
+            }
+            else if (eggSize == "Small")
+            {
+                switch (eggColor)
+                {
+                    case "Red":
+                        price = 9;
+                        break;
+                    case "Green":
+                        price = 8;
+                        break;
+                    case "Yellow":
+                        price = 5;
+                        break;
+                }
+            }
+            double total = (eggCount * price) * 0.65;
+            Console.WriteLine($"{total:f2} leva.");
+        }
+    }
+}
