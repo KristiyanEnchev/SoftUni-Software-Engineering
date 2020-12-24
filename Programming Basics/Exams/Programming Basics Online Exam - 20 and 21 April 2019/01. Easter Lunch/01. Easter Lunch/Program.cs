2fa414b62e27ed107575687a6fd01961cfb8b771
Kristiyan Enchev <kristiqnenchevv@gmail.com>
2020-12-24 17:42:15 +0200

    New Add

---
+++ Programming Basics/Exams/Programming Basics Online Exam - 20 and 21 April 2019/01. Easter Lunch/01. Easter Lunch/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace ConsoleApp28
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double easterBreadCount = double.Parse(Console.ReadLine());
+            double eggCount = double.Parse(Console.ReadLine());
+            double cockieCount = double.Parse(Console.ReadLine());
+
+            double easterBreadPrice = 3.20;
+            double eggPrice = 4.35;
+            double cockiePrice = 5.40;
+            double eggPaintPrice = (eggCount * 12) * 0.15;
+
+            double total = easterBreadCount * easterBreadPrice + eggCount * eggPrice + cockiePrice * cockieCount + eggPaintPrice;
+
+            Console.WriteLine($"{total:f2}");
+
+        }
+    }
+}
