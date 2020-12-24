e608863305b55bdf2ab581e9dde4bf466e5c64fb
Kristiyan Enchev <kristiqnenchevv@gmail.com>
2020-12-24 17:43:12 +0200

    New Add

---
+++ Programming Basics/Exams/Programming Basics Online Exam - 20 and 21 April 2019/04. Easter Shop/04. Easter Shop/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+class Program
+{
+    static void Main(string[] args)
+    {
+        double eggCount = double.Parse(Console.ReadLine());
+        string comand = Console.ReadLine();
+        double egsSold = 0;
+        double eggsFill = 0;
+        double curentEggs = eggCount;
+        while (comand != "Close")
+        {
+            int eggs = int.Parse(Console.ReadLine());
+            if (comand == "Buy")
+            {
+                curentEggs -= eggs;
+                if (curentEggs < 0)
+                {
+                    break;
+                }
+                egsSold += eggs;
+            }
+            else if (comand == "Fill")
+            {
+                curentEggs += eggs;
+                eggsFill += eggs;
+            }
+            comand = Console.ReadLine();
+        }
+        if (comand == "Close")
+        {
+            Console.WriteLine($"Store is closed!");
+            Console.WriteLine($"{egsSold} eggs sold.");
+        }
+        else
+        {
+            Console.WriteLine($"Not enough eggs in store!");
+            Console.WriteLine($"You can buy only {(eggCount + eggsFill) - egsSold}.");
+        }
+    }
+}
