3357225da507e8f16f08f6b911c20842be33dfe7
Kristiyan Enchev <kristiqnenchevv@gmail.com>
2020-12-24 17:43:36 +0200

    New Add

---
+++ Programming Basics/Exams/Programming Basics Online Exam - 20 and 21 April 2019/06. Easter Competition/06. Easter Competition/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace ConsoleApp31
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int easterBreadCount = int.Parse(Console.ReadLine());
+            double bestGrade = 0;
+            string bestChefName = "";
+            for (int i = 1; i <= easterBreadCount; i++)
+            {
+                string chefName = Console.ReadLine();
+                double gradeCount = 0;
+                bool stop = false;
+                while (true)
+                {
+                    string comand = Console.ReadLine();
+                    if (comand == "Stop")
+                    {
+                        stop = true;
+                        break;
+                    }
+                    int grade = int.Parse(comand);
+                    if (grade <= 0 || grade > 10)
+                    {
+                        grade = int.Parse(comand);
+                        continue;
+                    }
+                    gradeCount += grade;
+
+                }
+                if (stop)
+                {
+                    Console.WriteLine($"{chefName} has {gradeCount} points.");
+                }
+                if (gradeCount > bestGrade)
+                {
+                    Console.WriteLine($"{chefName} is the new number 1!");
+                    bestChefName = chefName;
+                    bestGrade = gradeCount;
+                }
+            }
+            Console.WriteLine($"{bestChefName} won competition with {bestGrade} points!");
+        }
+    }
+}
