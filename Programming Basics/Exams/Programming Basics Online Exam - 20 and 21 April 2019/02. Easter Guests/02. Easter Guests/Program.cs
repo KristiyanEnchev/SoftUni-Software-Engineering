eb0cca591cc3c3682ba546f5fa218e28b60a25d0
Kristiyan Enchev <kristiqnenchevv@gmail.com>
2020-12-24 17:42:23 +0200

    New Add

---
+++ Programming Basics/Exams/Programming Basics Online Exam - 20 and 21 April 2019/02. Easter Guests/02. Easter Guests/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace ConsoleApp28
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double peopleCount = double.Parse(Console.ReadLine());
+            double budget = double.Parse(Console.ReadLine());
+
+            double easterBreadCount = Math.Ceiling(peopleCount / 3);
+            double eggCount = peopleCount * 2;
+
+            double totalEasterBreadPrice = easterBreadCount * 4.00;
+            double totalEggPrice = eggCount * 0.45;
+            double totalPrice = totalEasterBreadPrice + totalEggPrice;
+
+            if (totalPrice <= budget)
+            {
+                Console.WriteLine($"Lyubo bought {easterBreadCount} Easter bread and {eggCount} eggs.");
+                Console.WriteLine($"He has {budget - totalPrice:f2} lv. left.");
+            }
+            else
+            {
+                Console.WriteLine("Lyubo doesn't have enough money.");
+                Console.WriteLine($"He needs {totalPrice - budget:f2} lv. more.");
+            }
+        }
+    }
+}
