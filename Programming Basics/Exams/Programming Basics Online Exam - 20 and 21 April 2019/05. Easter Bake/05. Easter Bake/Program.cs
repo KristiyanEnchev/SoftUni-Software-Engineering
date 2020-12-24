2f3f4d263bcb82adfa12f779cb461e01c8188795
Kristiyan Enchev <kristiqnenchevv@gmail.com>
2020-12-24 17:43:21 +0200

    New Add

---
+++ Programming Basics/Exams/Programming Basics Online Exam - 20 and 21 April 2019/05. Easter Bake/05. Easter Bake/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int easterBreadCount = int.Parse(Console.ReadLine());
+        double totalSugar = 0;
+        double totalFlouer = 0;
+        int sugar = 0;
+        int flouer = 0;
+        for (int i = 0; i < easterBreadCount; i++)
+        {
+            int sugarCount = int.Parse(Console.ReadLine());
+            int flouerCount = int.Parse(Console.ReadLine());
+            totalSugar += sugarCount;
+            totalFlouer += flouerCount;
+            if (sugarCount >= sugar)
+            {
+                sugar = sugarCount;
+            }
+            if (flouerCount >= flouer)
+            {
+                flouer = flouerCount;
+            }
+        }
+        double sugarPacks = Math.Ceiling(totalSugar / 950);
+        double flouerPacks = Math.Ceiling(totalFlouer / 750);
+
+        Console.WriteLine($"Sugar: {sugarPacks}");
+        Console.WriteLine($"Flour: {flouerPacks}");
+        Console.WriteLine($"Max used flour is {flouer} grams, max used sugar is {sugar} grams.");
+
+    }
+}
