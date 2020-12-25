9b2d1759c41975b600a624a73bdceb5cbe7dd47b
Kristiyan Enchev <kristiqnenchevv@gmail.com>
2020-12-25 19:59:17 +0200

    New Add

---
+++ Programming Basics/Exams/Programming Basics Online Exam - 28 and 29 March 2020/05. Care of Puppy/05. Care of Puppy/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Text.RegularExpressions;
+using System.Threading;
+
+namespace _10.Special_Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int foodInKilos = int.Parse(Console.ReadLine());
+            int foodInGrams = foodInKilos * 1000;
+            int totalFoodEaten = 0;
+            string comand = Console.ReadLine();
+            while (comand != "Adopted")
+            {
+                int foodEaten = int.Parse(comand);
+                totalFoodEaten += foodEaten;
+                comand = Console.ReadLine();
+            }
+            if (foodInGrams >= totalFoodEaten)
+            {
+                Console.WriteLine($"Food is enough! Leftovers: {foodInGrams - totalFoodEaten} grams.");
+            }
+            else
+            {
+                Console.WriteLine($"Food is not enough. You need {totalFoodEaten - foodInGrams} grams more.");
+            }
+        }
+    }
+}
