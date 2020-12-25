8e1ca73e933987bdba27b95e6d3fc5b04f30e2b5
Kristiyan Enchev <kristiqnenchevv@gmail.com>
2020-12-25 19:58:03 +0200

    New Add

---
+++ Programming Basics/Exams/Programming Basics Online Exam - 28 and 29 March 2020/02. Mountain Run/02. Mountain Run/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        double recordInSeconds = double.Parse(Console.ReadLine());
+        double recordInMeters = double.Parse(Console.ReadLine());
+        double timeForOneMeter = double.Parse(Console.ReadLine());
+
+        double second = recordInMeters * timeForOneMeter;
+        double delay = Math.Floor(recordInMeters / 50) * 30;
+
+        double totalTime = second + delay;
+        if (totalTime >= recordInSeconds)
+        {
+            Console.WriteLine($"No! He was {totalTime - recordInSeconds:f2} seconds slower.");
+        }
+        else
+        {
+            Console.WriteLine($" Yes! The new record is {totalTime:f2} seconds.");
+        }
+    }
+}
