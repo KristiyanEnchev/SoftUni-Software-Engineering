cd1ff316cba79e3935e61163a4ef817b79f7b5dc
Kristiyan Enchev <kristiqnenchevv@gmail.com>
2020-12-25 19:57:53 +0200

    New Add

---
+++ Programming Basics/Exams/Programming Basics Online Exam - 28 and 29 March 2020/02. Cat Walking/02. Cat Walking/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Threading;
+
+namespace _10.Special_Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int minutesWalk = int.Parse(Console.ReadLine());
+            int walksCount = int.Parse(Console.ReadLine());
+            int caloriesForDay = int.Parse(Console.ReadLine());
+
+            int totalMinutsForDay = minutesWalk * walksCount;
+            int totalCaloriesUsed = totalMinutsForDay * 5;
+            int caloriesNeeded = caloriesForDay / 2;
+            if (caloriesNeeded <= totalCaloriesUsed)
+            {
+                Console.WriteLine($"Yes, the walk for your cat is enough. Burned calories per day: {totalCaloriesUsed}.");
+            }
+            else
+            {
+                Console.WriteLine($"No, the walk for your cat is not enough. Burned calories per day: {totalCaloriesUsed}.");
+            }
+        }
+    }
+}
