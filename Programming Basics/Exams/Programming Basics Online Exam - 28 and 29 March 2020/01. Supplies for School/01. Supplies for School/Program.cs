27c7a3ac982eb790b554a5697346eeacbb9e0740
Kristiyan Enchev <kristiqnenchevv@gmail.com>
2020-12-25 19:57:44 +0200

    New Add

---
+++ Programming Basics/Exams/Programming Basics Online Exam - 28 and 29 March 2020/01. Supplies for School/01. Supplies for School/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Text;
+
+namespace ConsoleApp28
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int pen = int.Parse(Console.ReadLine());
+            int marker = int.Parse(Console.ReadLine());
+            double chemical = double.Parse(Console.ReadLine());
+            double procentDiscount = double.Parse(Console.ReadLine());
+            double procent = procentDiscount * 0.01;
+
+            double totalPenPrice = pen * 5.80;
+            double totalMarkerPrice = marker * 7.20;
+            double totalChemicalPrice = chemical * 1.20;
+            double totalPrice = totalPenPrice + totalMarkerPrice + totalChemicalPrice;
+            double discount = totalPrice * procent;
+            double total = totalPrice - discount;
+            Console.WriteLine($"{total:f3}");
+        }
+    }
+}
