a262cf3c52cebf44a3dfee8113c9dd7960e7a2fb
Kristiyan Enchev <kristiqnenchevv@gmail.com>
2020-12-25 19:57:34 +0200

    New Add

---
+++ Programming Basics/Exams/Programming Basics Online Exam - 28 and 29 March 2020/01. Change Bureau/01. Change Bureau/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+namespace _10.Special_Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int bitcoinCount = int.Parse(Console.ReadLine());
+            double chineseUana = double.Parse(Console.ReadLine());
+            double comisionProcent = double.Parse(Console.ReadLine());
+
+            double procent = comisionProcent * 0.01;
+            double UanaInEuro = ((chineseUana * 0.15) * 1.76) / 1.95;
+            double bitcoinInEuro = (bitcoinCount * 1168) / 1.95;
+            double totalMoney = bitcoinInEuro + UanaInEuro;
+            double totalAfterComision = totalMoney - (totalMoney * procent);
+
+            Console.WriteLine($"{totalAfterComision:f2}");
+        }
+    }
+}
