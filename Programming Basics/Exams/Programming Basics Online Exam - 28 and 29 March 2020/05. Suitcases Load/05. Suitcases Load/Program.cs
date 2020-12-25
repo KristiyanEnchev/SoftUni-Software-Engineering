a5ef0ff529c6444aa9c45ad10130a8f09e77be75
Kristiyan Enchev <kristiqnenchevv@gmail.com>
2020-12-25 19:59:31 +0200

    New Add

---
+++ Programming Basics/Exams/Programming Basics Online Exam - 28 and 29 March 2020/05. Suitcases Load/05. Suitcases Load/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        double capacity = double.Parse(Console.ReadLine());
+        int bagCount = 0;
+        int bagProcesed = 0;
+        string comand = Console.ReadLine();
+        while (capacity > 0)
+        {
+            bagProcesed++;
+            double bagCapacity = double.Parse(comand);
+            if (bagProcesed % 3 == 0)
+            {
+                bagCapacity *= 1.10;
+            }
+            capacity -= bagCapacity;
+            if (capacity <= 0)
+            {
+                break;
+            }
+            bagCount++;
+            comand = Console.ReadLine();
+            if (comand == "End")
+            {
+                break;
+            }
+        }
+        if (capacity <= 0)
+        {
+            Console.WriteLine("No more space!");
+        }
+        else if (comand == "End")
+        {
+            Console.WriteLine("Congratulations! All suitcases are loaded!");
+        }
+        Console.WriteLine($"Statistic: {bagCount} suitcases loaded.");
+    }
+}
