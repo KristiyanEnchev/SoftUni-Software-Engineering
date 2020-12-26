b179e0c257ecee530dd3a7d53d0a4a159171e595
Kristiyan Enchev <kristiqnenchevv@gmail.com>
2020-12-26 19:09:16 +0200

    New Add

---
+++ Programming Basics/Exams/Programming Basics Online Exam - 22 and 23 August 2020/02. Illidan/02. Illidan/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace ConsoleApp46
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int peopleCount = int.Parse(Console.ReadLine());
+            int power = int.Parse(Console.ReadLine());
+            int ilidansHelth = int.Parse(Console.ReadLine());
+            int totalPower = peopleCount * power;
+            if (totalPower > ilidansHelth)
+            {
+                Console.WriteLine($"Illidan has been slain! You defeated him with {totalPower - ilidansHelth} points.");
+            }
+            else
+            {
+                Console.WriteLine($"You are not prepared! You need {ilidansHelth - totalPower} more points.");
+            }
+        }
+    }
+}
