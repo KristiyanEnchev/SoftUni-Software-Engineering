9f94785d0e8c1685303886f57445e21d884b6ee6
Kristiyan Enchev <kristiqnenchevv@gmail.com>
2020-12-26 19:09:43 +0200

    New Add

---
+++ Programming Basics/Exams/Programming Basics Online Exam - 22 and 23 August 2020/05. Everest/05. Everest/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace ConsoleApp46
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int start = 5364;
+            int end = 8848;
+            int dayCount = 1;
+            while (true)
+            {
+                string actionTaken = Console.ReadLine();
+                if (actionTaken == "END")
+                {
+                    break;
+                }
+                int metersTaken = int.Parse(Console.ReadLine());
+                if (dayCount >= 5)
+                {
+                    break;
+                }
+                start += metersTaken;
+                if (actionTaken == "Yes")
+                {
+                    dayCount++;
+                }
+                else if (actionTaken == "No")
+                {
+                    continue;
+                }
+                if (start >= end)
+                {
+                    break;
+                }
+            }
+            if (start >= end)
+            {
+                Console.WriteLine($"Goal reached for {dayCount} days!");
+            }
+            else
+            {
+                Console.WriteLine("Failed!");
+                Console.WriteLine($"{start}");
+            }
+        }
+    }
+}
