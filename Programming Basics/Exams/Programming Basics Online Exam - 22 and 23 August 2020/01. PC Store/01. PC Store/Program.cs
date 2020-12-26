f1cb8ae0b2a41aad5022054f45a57bfc7f749de3
Kristiyan Enchev <kristiqnenchevv@gmail.com>
2020-12-26 19:09:02 +0200

    New Add

---
+++ Programming Basics/Exams/Programming Basics Online Exam - 22 and 23 August 2020/01. PC Store/01. PC Store/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace ConsoleApp46
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double procesorPrice = double.Parse(Console.ReadLine());
+            double videoCardPrice = double.Parse(Console.ReadLine());
+            double ramPrice = double.Parse(Console.ReadLine());
+            int ramCount = int.Parse(Console.ReadLine());
+            double discount = double.Parse(Console.ReadLine());
+            double procesorInLeva = procesorPrice * 1.57;
+            double videoInLeva = videoCardPrice * 1.57;
+            double ramInLeva = ramPrice * 1.57;
+            double totalRam = ramInLeva * ramCount;
+            double procesorDiscount = procesorInLeva * discount;
+            double videoDiscount = videoInLeva * discount;
+            double totalPricesorPrice = procesorInLeva - procesorDiscount;
+            double totalvideo = videoInLeva - videoDiscount;
+            double total = totalPricesorPrice + totalvideo + totalRam;
+            Console.WriteLine($"Money needed - {total:f2} leva.");
+        }
+    }
+}
