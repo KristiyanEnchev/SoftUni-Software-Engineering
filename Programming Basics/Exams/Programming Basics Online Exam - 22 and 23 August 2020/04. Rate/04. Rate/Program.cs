3b4003591fbd8944a8c760c35f69c09998c956ef
Kristiyan Enchev <kristiqnenchevv@gmail.com>
2020-12-26 19:09:34 +0200

    New Add

---
+++ Programming Basics/Exams/Programming Basics Online Exam - 22 and 23 August 2020/04. Rate/04. Rate/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+class Program
+{
+    static void Main()
+    {
+        decimal sum = decimal.Parse(Console.ReadLine());
+        int monthCount = int.Parse(Console.ReadLine());
+
+        decimal totalll = sum;
+
+        for (int i = 1; i <= monthCount; i++)
+        {
+            totalll = (1.00m + 0.027m) * totalll;
+        }
+        Console.WriteLine("Simple interest rate: {0:f2} lv. ", (1.00m + monthCount * 0.03m) * sum);
+        Console.WriteLine("Complex interest rate: {0:f2} lv.", totalll);
+
+        if ((1.00m + monthCount * 0.03m) * sum > totalll)
+        {
+            Console.WriteLine("Choose a simple interest rate. You will win {0:f2} lv.", (1.00m + monthCount * 0.03m) * sum - totalll);
+        }
+        else
+        {
+            Console.WriteLine("Choose a complex interest rate. You will win {0:f2} lv.", totalll - (1.00m + monthCount * 0.03m) * sum);
+        }
+    }
+}
