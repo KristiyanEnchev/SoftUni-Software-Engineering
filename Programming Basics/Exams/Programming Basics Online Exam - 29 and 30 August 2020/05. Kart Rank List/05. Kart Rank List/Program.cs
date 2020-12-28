4765ab28d37591069cddc117f0b875baa6f841e0
Kristiyan Enchev <kristiqnenchevv@gmail.com>
2020-12-28 12:43:38 +0200

    New Add

---
+++ Programming Basics/Exams/Programming Basics Online Exam - 29 and 30 August 2020/05. Kart Rank List/05. Kart Rank List/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace ConsoleApp52
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string comand = Console.ReadLine();
+            int gold = 0;
+            int silver = 0;
+            int bronze = 0;
+            int bestTime = int.MaxValue;
+            string bestName = "";
+            while (comand != "Finish")
+            {
+                string name = comand;
+                int minuts = int.Parse(Console.ReadLine());
+                int sec = int.Parse(Console.ReadLine());
+                int totalSec = minuts * 60 + sec;
+                if (totalSec <= 120 && totalSec > 85)
+                {
+                    bronze++;
+                }
+                else if (totalSec <= 85 && totalSec >= 55)
+                {
+                    silver++;
+                }
+                else if (totalSec < 55)
+                {
+                    gold++;
+                }
+                if (totalSec < bestTime)
+                {
+                    bestTime = totalSec;
+                    bestName = name;
+                }
+                comand = Console.ReadLine();
+            }
+            int bestMin = bestTime / 60;
+            int bestSec = bestTime % 60;
+            Console.WriteLine($"With {bestMin} minutes and {bestSec} seconds {bestName} is the winner of the day!");
+            Console.WriteLine($"Today's prizes are {gold} Gold {silver} Silver and {bronze} Bronze cards!");
+        }
+    }
+}
