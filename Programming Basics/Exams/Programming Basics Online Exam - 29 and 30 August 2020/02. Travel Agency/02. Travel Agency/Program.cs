e822c99ea323aa2c5af79bd225b1645226029bf8
Kristiyan Enchev <kristiqnenchevv@gmail.com>
2020-12-28 12:43:09 +0200

    New Add

---
+++ Programming Basics/Exams/Programming Basics Online Exam - 29 and 30 August 2020/02. Travel Agency/02. Travel Agency/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace ConsoleApp52
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int ticketCount = int.Parse(Console.ReadLine());
+            double budget = double.Parse(Console.ReadLine());
+            double ticketPrice = double.Parse(Console.ReadLine());
+            double totalPrice = ticketPrice * ticketCount;
+            if (budget >= totalPrice)
+            {
+                Console.WriteLine($"You can sell your client {ticketCount} tickets for the price of {ticketPrice * ticketCount}$!");
+                Console.WriteLine($"Your client should become a change of {budget - totalPrice}$!");
+            }
+            else
+            {
+                Console.WriteLine($"The budget of {budget}$ is not enough. Your client can't buy {ticketCount} tickets with this budget!");
+            }
+        }
+    }
+}
