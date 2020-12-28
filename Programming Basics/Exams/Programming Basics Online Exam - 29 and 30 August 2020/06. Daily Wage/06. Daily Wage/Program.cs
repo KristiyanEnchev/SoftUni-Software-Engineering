c71b4be335524c224f2e33c991c8d7e5e29df505
Kristiyan Enchev <kristiqnenchevv@gmail.com>
2020-12-28 12:43:47 +0200

    New Add

---
+++ Programming Basics/Exams/Programming Basics Online Exam - 29 and 30 August 2020/06. Daily Wage/06. Daily Wage/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace ConsoleApp52
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int rowCount = int.Parse(Console.ReadLine());
+            int placeCount = int.Parse(Console.ReadLine());
+            double total = 0;
+            for (int i = 1; i <= rowCount; i++)
+            {
+                double strawberryCount = 0;
+                double blackberryCount = 0;
+                if (i % 2 != 0)
+                {
+                    for (int j = 1; j <= placeCount; j++)
+                    {
+                        strawberryCount += 3.50;
+                    }
+                }
+                else
+                {
+                    for (int j = 1; j <= placeCount; j++)
+                    {
+                        if (j % 3 != 0)
+                        {
+                            blackberryCount += 5.00;
+                        }
+                    }
+                }
+                total += strawberryCount + blackberryCount;
+            }
+            double finalTotal = total * 0.80;
+            Console.WriteLine($"Total: {finalTotal:f2} lv.");
+        }
+    }
+}
