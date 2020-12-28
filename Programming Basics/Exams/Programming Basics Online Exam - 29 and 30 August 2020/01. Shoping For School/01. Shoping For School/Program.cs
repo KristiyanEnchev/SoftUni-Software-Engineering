2b2100a846c979b04afd409ef19c60ac5ceb37e9
Kristiyan Enchev <kristiqnenchevv@gmail.com>
2020-12-28 12:42:51 +0200

    New Add

---
+++ Programming Basics/Exams/Programming Basics Online Exam - 29 and 30 August 2020/01. Shoping For School/01. Shoping For School/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace ConsoleApp52
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double pencilCount = double.Parse(Console.ReadLine());
+            double colorPenCount = double.Parse(Console.ReadLine());
+            double drawindBookCount = double.Parse(Console.ReadLine());
+            double noteBookCount = double.Parse(Console.ReadLine());
+            double pencil = pencilCount * 4.75;
+            double colorPen = colorPenCount * 1.80;
+            double drawingBook = drawindBookCount * 6.50;
+            double noteBook = noteBookCount * 2.50;
+            double total = pencil + colorPen + drawingBook + noteBook;
+            double finalTotal = total - ((total * 5) / 100);
+            Console.WriteLine($"Your total is {finalTotal:f2}lv");
+        }
+    }
+}
