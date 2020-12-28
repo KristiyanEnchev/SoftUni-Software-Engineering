7bb11c3ecaafb8176e7bf4c5c238eb176af6ac19
Kristiyan Enchev <kristiqnenchevv@gmail.com>
2020-12-28 12:43:30 +0200

    New Add

---
+++ Programming Basics/Exams/Programming Basics Online Exam - 29 and 30 August 2020/04. Gifts from Santa/04. Gifts from Santa/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace ConsoleApp52
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int m = int.Parse(Console.ReadLine());
+            int s = int.Parse(Console.ReadLine());
+            for (int i = m; i >= n; i--)
+            {
+                if (i % 2 == 0 && i % 3 == 0)
+                {
+                    if (i == s)
+                    {
+                        Environment.Exit(0);
+                    }
+                    Console.Write($"{i} ");
+                }
+            }
+        }
+    }
+}
