874c44553a2119c340305d41b44c55663d1a57fa
Kristiyan Enchev <kristiqnenchevv@gmail.com>
2020-12-29 21:56:21 +0200

    New Add

---
+++ Programming Fundamentals/Lab & Exercices/Basic Syntax Lab/12. Even Number/12. Even Number/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace ConsoleApp53
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = int.Parse(Console.ReadLine());
+            while (num % 2 != 0)
+            {
+                Console.WriteLine("Please write an even number.");
+                num = int.Parse(Console.ReadLine());
+            }
+            Console.WriteLine($"The number is: {Math.Abs(num)}");
+        }
+    }
+}
