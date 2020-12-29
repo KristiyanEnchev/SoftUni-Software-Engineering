50027084662e3600f1bae5be0a1a3213b7078158
Kristiyan Enchev <kristiqnenchevv@gmail.com>
2020-12-29 21:55:40 +0200

    New Add

---
+++ Programming Fundamentals/Lab & Exercices/Basic Syntax Lab/07. Theatre Promotion/07. Theatre Promotion/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+
+namespace ConsoleApp53
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string day = Console.ReadLine();
+            int age = int.Parse(Console.ReadLine());
+            double price = 0;
+            if (age >= 0 && age <= 18)
+            {
+                switch (day)
+                {
+                    case "Weekday":
+                        price = 12;
+                        break;
+                    case "Weekend":
+                        price = 15;
+                        break;
+                    case "Holiday":
+                        price = 5;
+                        break;
+                }
+                Console.WriteLine($"{price}$");
+            }
+            else if (age >= 19 && age <= 64)
+            {
+                switch (day)
+                {
+                    case "Weekday":
+                        price = 18;
+                        break;
+                    case "Weekend":
+                        price = 20;
+                        break;
+                    case "Holiday":
+                        price = 12;
+                        break;
+                }
+                Console.WriteLine($"{price}$");
+            }
+            else if (age >= 65 && age <= 122)
+            {
+                switch (day)
+                {
+                    case "Weekday":
+                        price = 12;
+                        break;
+                    case "Weekend":
+                        price = 15;
+                        break;
+                    case "Holiday":
+                        price = 10;
+                        break;
+                }
+                Console.WriteLine($"{price}$");
+            }
+            else
+            {
+                Console.WriteLine("Error!");
+            }
+        }
+    }
+}
