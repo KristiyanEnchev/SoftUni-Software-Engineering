3275d962553a862ae9cce7da30d6a114637720b6
Kristiyan Enchev <kristiqnenchevv@gmail.com>
2020-12-29 21:55:56 +0200

    New Add

---
+++ Programming Fundamentals/Lab & Exercices/Basic Syntax Lab/09. Sum of Odd Numbers/09. Sum of Odd Numbers/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace ConsoleApp53
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = int.Parse(Console.ReadLine());
+            int total = 0;
+            for (int i = 1; i <= num * 2; i++)
+            {
+                if (i % 2 != 0)
+                {
+                    Console.WriteLine(i);
+                    total += i;
+                }
+            }
+            Console.WriteLine($"Sum: {total}");
+        }
+    }
+}
