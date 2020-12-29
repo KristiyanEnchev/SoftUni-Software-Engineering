96a8a68fa5959391dc12d5bef87e48b5b9d2265f
Kristiyan Enchev <kristiqnenchevv@gmail.com>
2020-12-29 21:55:05 +0200

    New Add

---
+++ Programming Fundamentals/Lab & Exercices/Basic Syntax Lab/04. Back In 30 Minutes/04. Back In 30 Minutes/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace ConsoleApp53
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int houres = int.Parse(Console.ReadLine());
+            int min = int.Parse(Console.ReadLine());
+            int endMin = min + 30;
+            if (endMin > 59)
+            {
+                houres++;
+                if (houres > 23)
+                {
+                    houres -= 24;
+                }
+                endMin -= 60;
+            }
+            Console.WriteLine($"{houres}:{endMin:d2}");
+        }
+    }
+}
