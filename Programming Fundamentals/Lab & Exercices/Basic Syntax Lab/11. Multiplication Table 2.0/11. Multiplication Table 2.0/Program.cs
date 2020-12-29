7c7d22eeb9997f37500f22dd0484bcff1a83d32a
Kristiyan Enchev <kristiqnenchevv@gmail.com>
2020-12-29 21:56:12 +0200

    New Add

---
+++ Programming Fundamentals/Lab & Exercices/Basic Syntax Lab/11. Multiplication Table 2.0/11. Multiplication Table 2.0/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace ConsoleApp53
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = int.Parse(Console.ReadLine());
+            int multiplier = int.Parse(Console.ReadLine());
+            if (multiplier <= 10)
+            {
+                for (int i = multiplier; i <= 10; i++)
+                {
+                    Console.WriteLine($"{num} X {i} = {num * i}");
+                }
+            }
+            else
+            {
+                Console.WriteLine($"{num} X {multiplier} = {num * multiplier}");
+            }
+        }
+    }
+}
