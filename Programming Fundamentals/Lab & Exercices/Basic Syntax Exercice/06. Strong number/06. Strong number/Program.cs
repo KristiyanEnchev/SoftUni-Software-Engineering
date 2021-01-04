2369d46d5e48378fad9028a3587dee6bf8e82303
Kristiyan Enchev <kristiqnenchevv@gmail.com>
2021-01-04 23:29:30 +0200

    New Add

---
+++ Programming Fundamentals/Lab & Exercices/Basic Syntax Exercice/06. Strong number/06. Strong number/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+namespace ConsoleApp54
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = int.Parse(Console.ReadLine());
+            string numm = num.ToString();
+            int total = 0;
+            for (int i = 0; i < numm.Length; i++)
+            {
+                char r = numm[i];
+                int t = int.Parse(r.ToString());
+                int k = 1;
+                for (int j = t; j >= 1; j--)
+                {
+                    k *= j;
+                }
+                total += k;
+            }
+            if (total == num)
+            {
+                Console.WriteLine("yes");
+            }
+            else
+            {
+                Console.WriteLine("no");
+            }
+        }
+    }
+}
