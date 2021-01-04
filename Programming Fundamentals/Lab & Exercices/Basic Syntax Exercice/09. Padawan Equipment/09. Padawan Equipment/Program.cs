b7845f393261f0f31bf40f6eacb2f588cee832e7
Kristiyan Enchev <kristiqnenchevv@gmail.com>
2021-01-04 23:30:11 +0200

    New Add

---
+++ Programming Fundamentals/Lab & Exercices/Basic Syntax Exercice/09. Padawan Equipment/09. Padawan Equipment/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace ConsoleApp54
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double budget = double.Parse(Console.ReadLine());
+            int studentCount = int.Parse(Console.ReadLine());
+            double lightSaberPrice = double.Parse(Console.ReadLine());
+            double robePrice = double.Parse(Console.ReadLine());
+            double beltPrice = double.Parse(Console.ReadLine());
+
+            double extraLightsabers = Math.Ceiling(studentCount * 1.10);
+            int freeBelt = studentCount / 6;
+
+            double totalLightsaber = extraLightsabers * lightSaberPrice;
+            double totalbelts = (studentCount - freeBelt) * beltPrice;
+            double totalrobe = robePrice * studentCount;
+            double total = totalLightsaber + totalbelts + totalrobe;
+            if (budget >= total)
+            {
+                Console.WriteLine($"The money is enough - it would cost {total:f2}lv.");
+            }
+            else
+            {
+                Console.WriteLine($"Ivan Cho will need {total - budget:f2}lv more.");
+            }
+        }
+    }
+}
