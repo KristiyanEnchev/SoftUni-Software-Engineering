9995f6a75b6fbbcf12c191c90b83c679651a1101
Kristiyan Enchev <kristiqnenchevv@gmail.com>
2021-01-04 23:28:28 +0200

    New Add

---
+++ Programming Fundamentals/Lab & Exercices/Basic Syntax Exercice/01. Ages/01. Ages/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+class program
+{
+    static void Main()
+    {
+        int age = int.Parse(Console.ReadLine());
+        if (age >= 0 && age <= 2)
+        {
+            Console.WriteLine("baby");
+        }
+        else if (age >= 3 && age <= 13)
+        {
+            Console.WriteLine("child");
+        }
+        else if (age >= 14 && age <= 19)
+        {
+            Console.WriteLine("teenager");
+        }
+        else if (age >= 20 && age <= 65)
+        {
+            Console.WriteLine("adult");
+        }
+        else if (age > 65)
+        {
+            Console.WriteLine("elder");
+        }
+    }
+}
