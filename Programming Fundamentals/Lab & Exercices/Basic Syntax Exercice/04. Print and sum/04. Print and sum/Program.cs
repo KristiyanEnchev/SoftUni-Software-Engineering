8deb50ebb73dd76367b1360139b0baea83e8160c
Kristiyan Enchev <kristiqnenchevv@gmail.com>
2021-01-04 23:29:05 +0200

    New Add

---
+++ Programming Fundamentals/Lab & Exercices/Basic Syntax Exercice/04. Print and sum/04. Print and sum/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+class program
+{
+    static void Main()
+    {
+        int strat = int.Parse(Console.ReadLine());
+        int end = int.Parse(Console.ReadLine());
+        int total = 0;
+        for (int i = strat; i <= end; i++)
+        {
+            total += i;
+            Console.Write($"{i} ");
+        }
+        Console.WriteLine();
+        Console.WriteLine($"Sum: {total}");
+    }
+}
