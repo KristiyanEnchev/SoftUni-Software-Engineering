4ce6a8597329686783f60267f0d8bb325cef1e6b
Kristiyan Enchev <kristiqnenchevv@gmail.com>
2021-01-05 21:20:35 +0200

    New Add

---
+++ Programming Fundamentals/Extra Exercices/Basic Syntax - More Exercise/01. Sort Numbers/01. Sort Numbers/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+namespace ConsoleApp55
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1 = int.Parse(Console.ReadLine());
+            int num2 = int.Parse(Console.ReadLine());
+            int num3 = int.Parse(Console.ReadLine());
+
+            if (num1 >= num2 && num1 >= num3)
+            {
+                Console.WriteLine(num1);
+                if (num2 > num3)
+                {
+                    Console.WriteLine(num2);
+                    Console.WriteLine(num3);
+                }
+                else
+                {
+                    Console.WriteLine(num3);
+                    Console.WriteLine(num2);
+                }
+            }
+            else if (num2 >= num1 && num2 >= num3)
+            {
+                Console.WriteLine(num2);
+                if (num1 > num3)
+                {
+                    Console.WriteLine(num1);
+                    Console.WriteLine(num3);
+                }
+                else
+                {
+                    Console.WriteLine(num3);
+                    Console.WriteLine(num1);
+                }
+            }
+            else if (num3 >= num2 && num3 >= num1)
+            {
+                Console.WriteLine(num3);
+                if (num2 > num1)
+                {
+                    Console.WriteLine(num2);
+                    Console.WriteLine(num1);
+                }
+                else
+                {
+                    Console.WriteLine(num1);
+                    Console.WriteLine(num2);
+                }
+            }
+        }
+    }
+}
