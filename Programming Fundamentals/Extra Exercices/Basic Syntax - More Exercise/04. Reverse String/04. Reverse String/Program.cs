b787a4840823915e859e6834ec88a3ddb7c5a5ae
Kristiyan Enchev <kristiqnenchevv@gmail.com>
2021-01-05 21:21:03 +0200

    New Add

---
+++ Programming Fundamentals/Extra Exercices/Basic Syntax - More Exercise/04. Reverse String/04. Reverse String/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace ConsoleApp55
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string comand = Console.ReadLine();
+            string output = "";
+
+            for (int i = comand.Length - 1; i >= 0; i--)
+            {
+                output += comand[i];
+            }
+            Console.WriteLine(output);
+        }
+    }
+}
