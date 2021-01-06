99daaa41c25f5baa2fa8e8415165f422424532f0
Kristiyan Enchev <kristiqnenchevv@gmail.com>
2021-01-06 23:00:50 +0200

    New Add

---
+++ Programming Fundamentals/Lab & Exercices/Data Types and Variables - Lab/12. Refactor Special Numbers/12. Refactor Special Numbers/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int num = int.Parse(Console.ReadLine());
+        for (int i = 1; i <= num; i++)
+        {
+            int summ = 0;
+            Console.Write($"{i} -> ");
+            for (int j = 0; j < i.ToString().Length; j++)
+            {
+                int nummm = int.Parse(i.ToString()[j].ToString());
+                summ += nummm;
+            }
+            if (summ == 5 || summ == 7 || summ == 11)
+            {
+                Console.Write("True");
+            }
+            else
+            {
+                Console.Write("False");
+            }
+            Console.WriteLine();
+        }
+    }
+}
