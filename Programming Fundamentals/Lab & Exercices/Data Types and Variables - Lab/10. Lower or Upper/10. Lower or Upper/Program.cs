86a36fdad30735113324f19131788fe913020e7a
Kristiyan Enchev <kristiqnenchevv@gmail.com>
2021-01-06 23:00:32 +0200

    New Add

---
+++ Programming Fundamentals/Lab & Exercices/Data Types and Variables - Lab/10. Lower or Upper/10. Lower or Upper/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        char letter = char.Parse(Console.ReadLine());
+        int digit = (int)letter;
+        if (digit > 64 && digit < 91)
+        {
+            Console.WriteLine("upper-case");
+        }
+        else if (digit > 96 && digit < 123)
+        {
+            Console.WriteLine("lower-case");
+        }
+    }
+}
