9571eeac699e7392ed1ba53f410d9739da4c4ede
Kristiyan Enchev <kristiqnenchevv@gmail.com>
2021-01-06 23:00:07 +0200

    New Add

---
+++ Programming Fundamentals/Lab & Exercices/Data Types and Variables - Lab/07. Concat Names/07. Concat Names/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string firstName = Console.ReadLine();
+        string lastName = Console.ReadLine();
+        string delimiter = Console.ReadLine();
+        Console.Write($"{firstName}{delimiter}{lastName}");
+    }
+}
