573b7f97cb338bf9e334ea48ba6ac4c42b3e6284
Kristiyan Enchev <kristiqnenchevv@gmail.com>
2021-01-06 22:59:22 +0200

    New Add

---
+++ Programming Fundamentals/Lab & Exercices/Data Types and Variables - Lab/03. Exact Sum of Real Numbers/03. Exact Sum of Real Numbers/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int num = int.Parse(Console.ReadLine());
+        decimal sum = 0;
+        for (int i = 1; i <= num; i++)
+        {
+            decimal num1 = decimal.Parse(Console.ReadLine());
+            sum += num1;
+        }
+        Console.WriteLine(sum);
+    }
+}
