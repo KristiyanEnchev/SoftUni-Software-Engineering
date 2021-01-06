fb19be66d98544a285eacd4ec38f8a3d8ae82327
Kristiyan Enchev <kristiqnenchevv@gmail.com>
2021-01-06 22:59:48 +0200

    New Add

---
+++ Programming Fundamentals/Lab & Exercices/Data Types and Variables - Lab/06. Reversed Chars/06. Reversed Chars/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        char one = char.Parse(Console.ReadLine());
+        char two = char.Parse(Console.ReadLine());
+        char three = char.Parse(Console.ReadLine());
+        Console.Write($"{three} ");
+        Console.Write($"{two} ");
+        Console.Write($"{one}");
+    }
+}
