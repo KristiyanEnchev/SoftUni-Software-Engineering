acd44026df2a02dceda5403e3d1c4b7e54880a5a
Kristiyan Enchev <kristiqnenchevv@gmail.com>
2021-01-06 22:59:06 +0200

    New Add

---
+++ Programming Fundamentals/Lab & Exercices/Data Types and Variables - Lab/01. Convert Meters/01. Convert Meters/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int metters = int.Parse(Console.ReadLine());
+        decimal kilometers = metters / 1000m;
+        Console.WriteLine($"{kilometers:f2}");
+    }
+}
