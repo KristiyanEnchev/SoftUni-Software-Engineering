4cdef673055aad210ef0a40f10b758d536a32bcc
Kristiyan Enchev <kristiqnenchevv@gmail.com>
2021-01-06 22:59:14 +0200

    New Add

---
+++ Programming Fundamentals/Lab & Exercices/Data Types and Variables - Lab/02. Pounds to Dollars/02. Pounds to Dollars/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        decimal pound = decimal.Parse(Console.ReadLine());
+        decimal dolar = pound * 1.31m;
+        Console.WriteLine($"{dolar:f3}");
+    }
+}
