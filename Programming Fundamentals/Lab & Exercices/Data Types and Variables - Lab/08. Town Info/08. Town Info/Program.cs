e7273ac2e83237e9e42c7a32d467aa3b2b4ea0cb
Kristiyan Enchev <kristiqnenchevv@gmail.com>
2021-01-06 23:00:15 +0200

    New Add

---
+++ Programming Fundamentals/Lab & Exercices/Data Types and Variables - Lab/08. Town Info/08. Town Info/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string town = Console.ReadLine();
+        string population = Console.ReadLine();
+        string area = Console.ReadLine();
+        Console.WriteLine($"Town {town} has population of {population} and area {area} square km.");
+    }
+}
