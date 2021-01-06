e4367d21693d0af15d7bfeed8f765283e7c7c4fe
Kristiyan Enchev <kristiqnenchevv@gmail.com>
2021-01-06 23:00:24 +0200

    New Add

---
+++ Programming Fundamentals/Lab & Exercices/Data Types and Variables - Lab/09. Chars to String/09. Chars to String/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        char one = char.Parse(Console.ReadLine());
+        char two = char.Parse(Console.ReadLine());
+        char three = char.Parse(Console.ReadLine());
+        string name = one.ToString() + two.ToString() + three.ToString();
+        Console.WriteLine(name);
+    }
+}
