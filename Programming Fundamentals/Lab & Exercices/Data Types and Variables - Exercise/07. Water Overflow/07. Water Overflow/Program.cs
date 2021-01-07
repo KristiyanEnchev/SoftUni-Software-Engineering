b96ada67ba72a3b26d57c2b2b5e546bdb6cfe461
Kristiyan Enchev <kristiqnenchevv@gmail.com>
2021-01-07 23:25:17 +0200

    New Add

---
+++ Programming Fundamentals/Lab & Exercices/Data Types and Variables - Exercise/07. Water Overflow/07. Water Overflow/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace ConsoleApp56
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int capacity = 255;
+            int pour = 0;
+            int numberOfPours = int.Parse(Console.ReadLine());
+            for (int i = 1; i <= numberOfPours; i++)
+            {
+                int litersToAdd = int.Parse(Console.ReadLine());
+                if (litersToAdd <= capacity)
+                {
+                    capacity -= litersToAdd;
+                    pour += litersToAdd;
+                }
+                else
+                {
+                    Console.WriteLine("Insufficient capacity!");
+                }
+            }
+            Console.WriteLine(pour);
+        }
+    }
+}
