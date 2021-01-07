7d0c048dbb5d98a67184cc582eea2bf3857c908b
Kristiyan Enchev <kristiqnenchevv@gmail.com>
2021-01-07 23:25:00 +0200

    New Add

---
+++ Programming Fundamentals/Lab & Exercices/Data Types and Variables - Exercise/05. Print Part Of ASCII Table/05. Print Part Of ASCII Table/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace ConsoleApp56
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberFromASCII = int.Parse(Console.ReadLine());
+            int numberToASCII = int.Parse(Console.ReadLine());
+            for (int i = numberFromASCII; i <= numberToASCII; i++)
+            {
+                char letter = (char)i;
+                Console.Write($"{letter} ");
+            }
+        }
+    }
+}
