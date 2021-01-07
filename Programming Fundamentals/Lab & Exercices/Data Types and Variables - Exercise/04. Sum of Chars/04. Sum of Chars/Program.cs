137ae992f6b0b4a86e70cd907f8e03b64495187e
Kristiyan Enchev <kristiqnenchevv@gmail.com>
2021-01-07 23:24:52 +0200

    New Add

---
+++ Programming Fundamentals/Lab & Exercices/Data Types and Variables - Exercise/04. Sum of Chars/04. Sum of Chars/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace ConsoleApp56
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberOfLinesToFowoll = int.Parse(Console.ReadLine());
+            int sumOfASCIIOfLetters = 0;
+            for (int i = 1; i <= numberOfLinesToFowoll; i++)
+            {
+                char letter = char.Parse(Console.ReadLine());
+                int numberOfLetter = letter;
+                sumOfASCIIOfLetters += numberOfLetter;
+            }
+            Console.WriteLine($"The sum equals: {sumOfASCIIOfLetters}");
+        }
+    }
+}
