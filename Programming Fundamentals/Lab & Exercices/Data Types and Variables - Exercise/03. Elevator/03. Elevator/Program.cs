3ab88379d9951242ba35437066397ee469e2443d
Kristiyan Enchev <kristiqnenchevv@gmail.com>
2021-01-07 23:24:43 +0200

    New Add

---
+++ Programming Fundamentals/Lab & Exercices/Data Types and Variables - Exercise/03. Elevator/03. Elevator/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace ConsoleApp56
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int peopleNeededToBeLift = int.Parse(Console.ReadLine());
+            int capacityOfElevator = int.Parse(Console.ReadLine());
+            int fullCourses = peopleNeededToBeLift / capacityOfElevator;
+            int peopleLeftToLift = peopleNeededToBeLift % capacityOfElevator;
+            if (peopleNeededToBeLift % capacityOfElevator <= 0)
+            {
+                Console.WriteLine(fullCourses);
+            }
+            else
+            {
+                Console.WriteLine(fullCourses + 1);
+            }
+        }
+    }
+}
