a1619b514475583117b05d2791046f8c80fe0127
Kristiyan Enchev <kristiqnenchevv@gmail.com>
2021-01-07 23:25:33 +0200

    New Add

---
+++ Programming Fundamentals/Lab & Exercices/Data Types and Variables - Exercise/09. Spice Must Flow/09. Spice Must Flow/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace ConsoleApp56
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double startingYield = double.Parse(Console.ReadLine());
+            int dayCount = 0;
+            double Yield = 0;
+            if (startingYield >= 100)
+            {
+                while (startingYield >= 100)
+                {
+                    Yield += startingYield - 26;
+                    dayCount++;
+                    startingYield -= 10;
+                }
+                Console.WriteLine(dayCount);
+                Console.WriteLine(Yield - 26);
+
+
+            }
+            else
+            {
+                Console.WriteLine(dayCount);
+                Console.WriteLine(Yield);
+            }
+        }
+    }
+}
