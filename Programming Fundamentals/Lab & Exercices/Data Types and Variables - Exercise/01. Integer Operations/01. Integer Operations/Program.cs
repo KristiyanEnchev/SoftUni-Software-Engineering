ac442a3235209bc286c0450b8118ec88d578510a
Kristiyan Enchev <kristiqnenchevv@gmail.com>
2021-01-07 23:24:26 +0200

    New Add

---
+++ Programming Fundamentals/Lab & Exercices/Data Types and Variables - Exercise/01. Integer Operations/01. Integer Operations/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace ConsoleApp56
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int one = int.Parse(Console.ReadLine());
+            int two = int.Parse(Console.ReadLine());
+            int sum = one + two;
+            int three = int.Parse(Console.ReadLine());
+            int sumOfDivision = sum / three;
+            int four = int.Parse(Console.ReadLine());
+            int result = four * sumOfDivision;
+            Console.WriteLine(result);
+        }
+    }
+}
