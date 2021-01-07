c7d50450d32e383b4847a6b37b2511620b1cd530
Kristiyan Enchev <kristiqnenchevv@gmail.com>
2021-01-07 23:25:25 +0200

    New Add

---
+++ Programming Fundamentals/Lab & Exercices/Data Types and Variables - Exercise/08. Beer Kegs/08. Beer Kegs/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace ConsoleApp56
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int lines = int.Parse(Console.ReadLine());
+            double bigestVolume = double.MinValue;
+            string bestKeg = "";
+            for (int i = 1; i <= lines; i++)
+            {
+                string model = Console.ReadLine();
+                double radius = double.Parse(Console.ReadLine());
+                int height = int.Parse(Console.ReadLine());
+                double volume = Math.PI * radius * radius * height;
+                if (volume >= bigestVolume)
+                {
+                    bigestVolume = volume;
+                    bestKeg = model;
+                }
+            }
+            Console.WriteLine(bestKeg);
+        }
+    }
+}
