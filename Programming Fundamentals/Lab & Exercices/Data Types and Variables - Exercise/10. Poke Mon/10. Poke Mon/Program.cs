28c9d8519c8aed36c342827539a4e2b8fbe0187f
Kristiyan Enchev <kristiqnenchevv@gmail.com>
2021-01-07 23:25:43 +0200

    New Add

---
+++ Programming Fundamentals/Lab & Exercices/Data Types and Variables - Exercise/10. Poke Mon/10. Poke Mon/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int N = int.Parse(Console.ReadLine());
+        int M = int.Parse(Console.ReadLine());
+        int Y = int.Parse(Console.ReadLine());
+
+        int pokedCount = 0;
+        decimal halfValue = N * 0.50m;
+
+        while (N >= M)
+        {
+            if (N == halfValue)
+            {
+                if (Y > 0)
+                {
+                    N = N / Y;
+                    if (N < M)
+                    {
+                        break;
+                    }
+                }
+            }
+            N -= M;
+            pokedCount++;
+        }
+        Console.WriteLine(N);
+        Console.WriteLine(pokedCount);
+    }
+}
