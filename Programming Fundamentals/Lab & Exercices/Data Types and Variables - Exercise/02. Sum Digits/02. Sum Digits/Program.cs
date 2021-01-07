c458001734935b1c3372aa4ad352b5d8461584c0
Kristiyan Enchev <kristiqnenchevv@gmail.com>
2021-01-07 23:24:35 +0200

    New Add

---
+++ Programming Fundamentals/Lab & Exercices/Data Types and Variables - Exercise/02. Sum Digits/02. Sum Digits/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace ConsoleApp56
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = int.Parse(Console.ReadLine());
+            int sum = 0;
+            for (int i = 0; i < num.ToString().Length; i++)
+            {
+                int digit = int.Parse(num.ToString()[i].ToString());
+                sum += digit;
+            }
+            Console.WriteLine(sum);
+        }
+    }
+}
