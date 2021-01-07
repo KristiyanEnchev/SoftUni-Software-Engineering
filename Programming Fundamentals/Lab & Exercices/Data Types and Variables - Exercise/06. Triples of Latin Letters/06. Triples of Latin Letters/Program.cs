ef8cee17ca45b7ac5d4ccfec951b194f437ffa79
Kristiyan Enchev <kristiqnenchevv@gmail.com>
2021-01-07 23:25:08 +0200

    New Add

---
+++ Programming Fundamentals/Lab & Exercices/Data Types and Variables - Exercise/06. Triples of Latin Letters/06. Triples of Latin Letters/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace ConsoleApp56
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberOfLetter = int.Parse(Console.ReadLine());
+            for (int i = 0; i < numberOfLetter; i++)
+            {
+                for (int j = 0; j < numberOfLetter; j++)
+                {
+                    for (int k = 0; k < numberOfLetter; k++)
+                    {
+                        char firstLetter = (char)('a' + i);
+                        char secondLetter = (char)('a' + j);
+                        char ThurdLetter = (char)('a' + k);
+                        Console.WriteLine($"{firstLetter}{secondLetter}{ThurdLetter}");
+                    }
+                }
+            }
+        }
+    }
+}
