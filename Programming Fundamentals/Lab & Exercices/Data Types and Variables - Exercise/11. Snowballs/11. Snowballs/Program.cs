ee1078de718ee49aba26b9a4faca387d2de63ce6
Kristiyan Enchev <kristiqnenchevv@gmail.com>
2021-01-07 23:25:51 +0200

    New Add

---
+++ Programming Fundamentals/Lab & Exercices/Data Types and Variables - Exercise/11. Snowballs/11. Snowballs/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Numerics;
+
+namespace SnowBalls
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int snowballsMadeN = int.Parse(Console.ReadLine());
+            BigInteger biggestSnowBall = 0;
+            string result = string.Empty;
+
+            for (int i = 0; i < snowballsMadeN; i++)
+            {
+                int snowballsSnow = int.Parse(Console.ReadLine());
+                int snowballsTime = int.Parse(Console.ReadLine());
+                int snowballsQuality = int.Parse(Console.ReadLine());
+
+                BigInteger snowballValue = BigInteger.Pow((snowballsSnow / snowballsTime), snowballsQuality);
+
+                if (snowballValue > biggestSnowBall)
+                {
+                    biggestSnowBall = snowballValue;
+                    result = ($"{snowballsSnow} : {snowballsTime} = {snowballValue} ({snowballsQuality})");
+                }
+            }
+            Console.WriteLine(result);
+        }
+    }
+}
