f2517b40eb3a833b23f37efdae01b5cc1d48801a
Kristiyan Enchev <kristiqnenchevv@gmail.com>
2021-01-08 23:30:04 +0200

    New Add

---
+++ Programming Fundamentals/Lab & Exercices/Arrays - Lab/04. Reverse Array of Strings/04. Reverse Array of Strings/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Linq;
+
+namespace ConsoleApp63
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] arr = Console.ReadLine()
+                .Split()
+                .ToArray();
+            Console.WriteLine(string.Join(" ", arr.Reverse()));
+        }
+    }
+}
