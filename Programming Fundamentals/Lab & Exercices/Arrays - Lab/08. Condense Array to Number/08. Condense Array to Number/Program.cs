4868c73c1dc3fab44ea9c415e64897c2ea2814d4
Kristiyan Enchev <kristiqnenchevv@gmail.com>
2021-01-08 23:30:42 +0200

    New Add

---
+++ Programming Fundamentals/Lab & Exercices/Arrays - Lab/08. Condense Array to Number/08. Condense Array to Number/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Linq;
+
+namespace ConsoleApp63
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = Console.ReadLine()
+                               .Split()
+                               .Select(int.Parse)
+                               .ToArray();
+
+
+            while (arr.Length != 1)
+            {
+                int[] arr2 = new int[arr.Length - 1];
+
+                for (int i = 0; i < arr.Length - 1; i++)
+                {
+                    arr2[i] = arr[i] + arr[i + 1];
+                }
+
+                arr = arr2;
+            }
+
+            Console.WriteLine(string.Join(' ', arr));
+        }
+    }
+}
