b86073e97f3d3a8f855274089749185e27a2f296
Kristiyan Enchev <kristiqnenchevv@gmail.com>
2021-01-08 23:29:53 +0200

    New Add

---
+++ Programming Fundamentals/Lab & Exercices/Arrays - Lab/03. Rounding Numbers/03. Rounding Numbers/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Linq;
+
+namespace ConsoleApp63
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double[] arr = Console.ReadLine()
+                .Split()
+                .Select(double.Parse)
+                .ToArray();
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                Console.WriteLine($"{Convert.ToDecimal(arr[i])} => {Convert.ToDecimal(Math.Round(arr[i], MidpointRounding.AwayFromZero))}");
+            }
+        }
+    }
+}
