f7734e99f14c3e8281999b2aa9782664ecd3cd1c
Kristiyan Enchev <kristiqnenchevv@gmail.com>
2021-01-08 23:30:22 +0200

    New Add

---
+++ Programming Fundamentals/Lab & Exercices/Arrays - Lab/06. Even and Odd Subtraction/06. Even and Odd Subtraction/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Linq;
+
+namespace ConsoleApp63
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToArray();
+
+            int evenNumbers = 0;
+            int oddNumberst = 0;
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (arr[i] % 2 == 0)
+                {
+                    evenNumbers += arr[i];
+                }
+                else
+                {
+                    oddNumberst += arr[i];
+                }
+            }
+
+            Console.WriteLine(evenNumbers - oddNumberst);
+        }
+    }
+}
+
