00945e93432ff8bfe384eb79f2ed922c85908e3b
Kristiyan Enchev <kristiqnenchevv@gmail.com>
2021-01-08 23:29:32 +0200

    New Add

---
+++ Programming Fundamentals/Lab & Exercices/Arrays - Lab/01. Day of Week/01. Day of Week/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace ConsoleApp63
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = int.Parse(Console.ReadLine());
+            string[] daysOfTheWeek =
+            {
+            "Monday",
+            "Tuesday",
+            "Wednesday",
+            "Thursday",
+            "Friday",
+            "Saturday",
+            "Sunday"
+            };
+
+            if (num < 1 || num > daysOfTheWeek.Length)
+            {
+                Console.WriteLine("Invalid day!");
+            }
+            else
+            {
+                Console.WriteLine(daysOfTheWeek[num - 1]);
+            }
+        }
+    }
+}
