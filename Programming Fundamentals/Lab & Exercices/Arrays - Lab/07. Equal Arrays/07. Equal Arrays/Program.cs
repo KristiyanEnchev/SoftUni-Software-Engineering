4fb03de1bc82270e07fc96de9e368e846f1ad91d
Kristiyan Enchev <kristiqnenchevv@gmail.com>
2021-01-08 23:30:32 +0200

    New Add

---
+++ Programming Fundamentals/Lab & Exercices/Arrays - Lab/07. Equal Arrays/07. Equal Arrays/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Linq;
+
+namespace ConsoleApp63
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToArray();
+
+            int[] arr2 = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToArray();
+
+            int sum = 0;
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (arr[i] != arr2[i])
+                {
+                    Console.WriteLine($"Arrays are not identical. Found difference at {i} index");
+                    break;
+                }
+                else
+                {
+                    sum++;
+                }
+            }
+            if (sum == arr.Length)
+            {
+                Console.WriteLine($"Arrays are identical. Sum: {arr.Sum()}");
+            }
+        }
+    }
+}
