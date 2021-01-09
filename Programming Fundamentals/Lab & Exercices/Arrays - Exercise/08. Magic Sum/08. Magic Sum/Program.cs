7bc1a601e1a0522a7d8110fe05cb80c9285478a8
Kristiyan Enchev <kristiqnenchevv@gmail.com>
2021-01-09 16:37:46 +0200

    New Add

---
+++ Programming Fundamentals/Lab & Exercices/Arrays - Exercise/08. Magic Sum/08. Magic Sum/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Linq;
+
+namespace ConsoleApp63
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = Console.ReadLine()
+                .Split(" ")
+                .Select(int.Parse)
+                .ToArray();
+
+            int magicNumber = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                for (int j = i + 1; j < arr.Length; j++)
+                {
+                    if (arr[i] + arr[j] == magicNumber)
+                    {
+                        Console.WriteLine($"{arr[i]} { arr[j]}");
+                        break;
+                    }
+                }
+            }
+        }
+    }
+}
