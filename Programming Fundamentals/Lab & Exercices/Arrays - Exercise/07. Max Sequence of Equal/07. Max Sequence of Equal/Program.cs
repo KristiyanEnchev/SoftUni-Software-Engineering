99c54de3be89bfd6ad83377799bd1d292630450b
Kristiyan Enchev <kristiqnenchevv@gmail.com>
2021-01-09 16:37:36 +0200

    New Add

---
+++ Programming Fundamentals/Lab & Exercices/Arrays - Exercise/07. Max Sequence of Equal/07. Max Sequence of Equal/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Linq;
+
+namespace ConsoleApp63
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = Console.ReadLine()
+                .Split(" ")
+                .Select(int.Parse)
+                .ToArray();
+
+            int count = 1;
+            int longestline = 1;
+            int index = 1;
+
+            for (int i = 0; i < arr.Length - 1; i++)
+            {
+                if (arr[i] == arr[i + 1])
+                {
+                    count++;
+                    if (count > longestline)
+                    {
+                        longestline = count;
+                        index = arr[i];
+                    }
+                }
+                else
+                {
+                    count = 1;
+                }
+            }
+            for (int i = 0; i < longestline; i++)
+            {
+                Console.Write($"{index} ");
+            }
+        }
+    }
+}
