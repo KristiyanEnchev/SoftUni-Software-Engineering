64a9f5e68be30b1641f9727bc4401dc6297d2524
Kristiyan Enchev <kristiqnenchevv@gmail.com>
2021-01-09 16:36:58 +0200

    New Add

---
+++ Programming Fundamentals/Lab & Exercices/Arrays - Exercise/03. Zig-Zag Arrays/03. Zig-Zag Arrays/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Linq;
+
+namespace ConsoleApp63
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberOfLines = int.Parse(Console.ReadLine());
+
+            int[] firstArray = new int[numberOfLines];
+            int[] secondArray = new int[numberOfLines];
+
+            for (int i = 0; i < numberOfLines; i++)
+            {
+                int[] input = Console.ReadLine()
+                    .Split()
+                    .Select(int.Parse)
+                    .ToArray();
+
+                for (int j = 0; j < input.Length; j++)
+                {
+                    if (i % 2 == 0)
+                    {
+                        firstArray[i] = input[j];
+                        secondArray[i] = input[j + 1];
+                        break;
+                    }
+                    else
+                    {
+                        firstArray[i] = input[j + 1];
+                        secondArray[i] = input[j];
+                        break;
+                    }
+                }
+            }
+
+            Console.WriteLine(string.Join(' ', firstArray));
+            Console.WriteLine(string.Join(' ', secondArray));
+        }
+    }
+}
