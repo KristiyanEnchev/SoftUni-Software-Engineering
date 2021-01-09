3e08e2267f8aa991b5f456fbdd0d671cd989e261
Kristiyan Enchev <kristiqnenchevv@gmail.com>
2021-01-09 16:36:44 +0200

    New Add

---
+++ Programming Fundamentals/Lab & Exercices/Arrays - Exercise/02. Common Elements/02. Common Elements/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Linq;
+
+namespace ConsoleApp63
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] firstArray = Console.ReadLine()
+                .Split().ToArray();
+
+            string[] secondArray = Console.ReadLine()
+                .Split().ToArray();
+
+            string comonElemnts = string.Empty;
+
+            for (int i = 0; i < secondArray.Length; i++)
+            {
+                string secondElement = secondArray[i];
+                for (int j = 0; j < firstArray.Length; j++)
+                {
+                    string element = firstArray[j];
+                    if (element == secondElement)
+                    {
+                        comonElemnts += element + " ";
+                    }
+                }
+            }
+
+            Console.WriteLine(comonElemnts);
+        }
+    }
+}
