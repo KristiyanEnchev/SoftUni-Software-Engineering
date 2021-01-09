18d6cb95a564e2631ecd5589dad567ef682ecb9a
Kristiyan Enchev <kristiqnenchevv@gmail.com>
2021-01-09 16:37:07 +0200

    New Add

---
+++ Programming Fundamentals/Lab & Exercices/Arrays - Exercise/04. Array Rotation/04. Array Rotation/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Linq;
+
+namespace ConsoleApp63
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToArray();
+
+            int rotationCount = int.Parse(Console.ReadLine());
+
+            int[] newSequance = new int[array.Length];
+
+            if (rotationCount > array.Length)
+            {
+                rotationCount = rotationCount - array.Length;
+            }
+            for (int i = rotationCount; i < array.Length; i++)
+            {
+                int num = array[i];
+                newSequance[i - rotationCount] = num;
+            }
+
+            for (int i = 0; i < rotationCount; i++)
+            {
+                int num = array[i];
+                newSequance[i + array.Length - rotationCount] = num;
+            }
+
+            Console.WriteLine(string.Join(' ', newSequance));
+        }
+    }
+}
