fc80678cf4dab793efaf74677c32aad04c738280
Kristiyan Enchev <kristiqnenchevv@gmail.com>
2021-01-09 16:36:33 +0200

    New Add

---
+++ Programming Fundamentals/Lab & Exercices/Arrays - Exercise/01. Train/01. Train/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Linq;
+
+namespace ConsoleApp63
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int wagonCount = int.Parse(Console.ReadLine());
+            int[] pasengerSequanceInWagons = new int[wagonCount];
+
+            for (int i = 0; i < wagonCount; i++)
+            {
+                int pasangerCount = int.Parse(Console.ReadLine());
+                pasengerSequanceInWagons[i] = pasangerCount;
+            }
+            Console.WriteLine(string.Join(" ", pasengerSequanceInWagons));
+            Console.WriteLine(pasengerSequanceInWagons.Sum());
+        }
+    }
+}
+
