d45377771f8f96800d9fcbc8af0c72586f5894b7
Kristiyan Enchev <kristiqnenchevv@gmail.com>
2021-01-10 22:37:24 +0200

    New Add

---
+++ Programming Fundamentals/Extra Exercices/Data Types and Variables - More Exercise/02. From Left to The Righ/02. From Left to The Righ/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Linq;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int inputNumber = int.Parse(Console.ReadLine());
+
+        for (int i = 1; i <= inputNumber; i++)
+        {
+            string[] input = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .ToArray();
+
+            long num1 = long.Parse(input[0]);
+            long num2 = long.Parse(input[1]);
+
+            if (num1 > num2)
+            {
+                long sum = 0;
+                while (num1 != 0)
+                {
+                    long digit = num1 % 10;
+                    sum += digit;
+                    num1 /= 10;
+                }
+                Console.WriteLine(Math.Abs(sum));
+            }
+            else
+            {
+                long sum = 0;
+                while (num2 != 0)
+                {
+                    long digit = num2 % 10;
+                    sum += digit;
+                    num2 /= 10;
+                }
+                Console.WriteLine(Math.Abs(sum));
+            }
+
+        }
+    }
+}
