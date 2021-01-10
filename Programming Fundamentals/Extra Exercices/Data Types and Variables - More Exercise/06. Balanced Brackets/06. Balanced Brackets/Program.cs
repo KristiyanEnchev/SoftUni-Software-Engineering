809d2dde47e2bcc3856084531626d75879120f16
Kristiyan Enchev <kristiqnenchevv@gmail.com>
2021-01-10 22:38:05 +0200

    New Add

---
+++ Programming Fundamentals/Extra Exercices/Data Types and Variables - More Exercise/06. Balanced Brackets/06. Balanced Brackets/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int numberOfoperations = int.Parse(Console.ReadLine());
+        int counterOpen = 0;
+        int counterClose = 0;
+        for (int i = 1; i <= numberOfoperations; i++)
+        {
+            string operation = Console.ReadLine();
+            if (operation == "(")
+            {
+                counterOpen++;
+            }
+            else if (operation == ")")
+            {
+                counterClose++;
+                if (counterOpen - counterClose != 0)
+                {
+                    Console.WriteLine("UNBALANCED");
+                    return;
+                }
+            }
+        }
+        if (counterClose == counterOpen)
+        {
+            Console.WriteLine("BALANCED");
+        }
+        else
+        {
+            Console.WriteLine("UNBALANCED");
+        }
+    }
+}
