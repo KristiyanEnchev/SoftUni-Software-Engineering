e6fc916c8d6e0690faf0e5bc08a8a80981a19cec
Kristiyan Enchev <kristiqnenchevv@gmail.com>
2021-01-10 22:37:52 +0200

    New Add

---
+++ Programming Fundamentals/Extra Exercices/Data Types and Variables - More Exercise/05. Decrypting Message/05. Decrypting Message/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int key = int.Parse(Console.ReadLine());
+        int lineToFollow = int.Parse(Console.ReadLine());
+        string message = "";
+        for (int i = 1; i <= lineToFollow; i++)
+        {
+            char letters = char.Parse(Console.ReadLine());
+            char result = (char)(letters + key);
+            message += result;
+        }
+        Console.WriteLine(message);
+    }
+}
