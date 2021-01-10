98c5ba274a018be8d93ecc512379e33fc1473b6e
Kristiyan Enchev <kristiqnenchevv@gmail.com>
2021-01-10 22:37:33 +0200

    New Add

---
+++ Programming Fundamentals/Extra Exercices/Data Types and Variables - More Exercise/03. Floating Equality/03. Floating Equality/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        decimal numA = decimal.Parse(Console.ReadLine());
+        decimal numB = decimal.Parse(Console.ReadLine());
+
+        decimal eps = 0.000001m;
+
+        if (Math.Abs(numA - numB) < eps)
+        {
+            Console.WriteLine("True");
+        }
+        else
+        {
+            Console.WriteLine("False");
+        }
+    }
+}
