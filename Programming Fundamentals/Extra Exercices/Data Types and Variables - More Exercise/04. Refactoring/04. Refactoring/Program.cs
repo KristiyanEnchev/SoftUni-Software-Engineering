3250eb7e10bafb05c4a2271686b8136441a61b65
Kristiyan Enchev <kristiqnenchevv@gmail.com>
2021-01-10 22:37:42 +0200

    New Add

---
+++ Programming Fundamentals/Extra Exercices/Data Types and Variables - More Exercise/04. Refactoring/04. Refactoring/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int numbersToBeCheked = int.Parse(Console.ReadLine());
+        for (int firstPrimeNumber = 2; firstPrimeNumber <= numbersToBeCheked; firstPrimeNumber++)
+        {
+            bool isItPrime = true;
+            for (int cepitel = 2; cepitel < firstPrimeNumber; cepitel++)
+            {
+                if (firstPrimeNumber % cepitel == 0)
+                {
+                    isItPrime = false;
+                    break;
+                }
+            }
+            Console.WriteLine("{0} -> {1}", firstPrimeNumber, isItPrime.ToString().ToLower());
+        }
+
+    }
+}
