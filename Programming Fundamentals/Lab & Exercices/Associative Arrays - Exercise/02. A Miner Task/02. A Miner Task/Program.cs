1ae3a52f50ce6e4c2ebf6f5e8886e20c067bd4d1
Kristiyan Enchev <kristiqnenchevv@gmail.com>
2021-01-13 23:34:24 +0200

    New Add

---
+++ Programming Fundamentals/Lab & Exercices/Associative Arrays - Exercise/02. A Miner Task/02. A Miner Task/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string resources = Console.ReadLine();
+            Dictionary<string, int> input = new Dictionary<string, int>();
+
+            while (resources != "stop")
+            {
+                int cuantity = int.Parse(Console.ReadLine());
+
+                if (!input.ContainsKey(resources))
+                {
+                    input.Add(resources, 0);
+                }
+
+                input[resources] += cuantity;
+
+                resources = Console.ReadLine();
+            }
+
+            foreach (var item in input)
+            {
+                Console.WriteLine($"{item.Key} -> {item.Value}");
+            }
+        }
+    }
+}
