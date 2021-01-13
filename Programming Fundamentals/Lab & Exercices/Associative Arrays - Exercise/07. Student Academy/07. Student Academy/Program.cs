fce650523c4445689b5cbe7dfc936ec795ad5f46
Kristiyan Enchev <kristiqnenchevv@gmail.com>
2021-01-13 23:35:05 +0200

    New Add

---
+++ Programming Fundamentals/Lab & Exercices/Associative Arrays - Exercise/07. Student Academy/07. Student Academy/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _08
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            Dictionary<string, List<double>> students = new Dictionary<string, List<double>>();
+
+            for (int i = 0; i < n; i++)
+            {
+                string name = Console.ReadLine();
+                double grade = double.Parse(Console.ReadLine());
+
+                if (!students.ContainsKey(name))
+                {
+                    students.Add(name, new List<double> { grade });
+                }
+                else
+                {
+                    students[name].Add(grade);
+                }
+            }
+
+            foreach (var student in students.OrderByDescending(x => x.Value.Average()).ThenByDescending(x => x.Key))
+            {
+                if (student.Value.Average() >= 4.50)
+                {
+                    Console.WriteLine($"{student.Key} -> {student.Value.Average():f2}");
+                }
+            }
+        }
+    }
+}
