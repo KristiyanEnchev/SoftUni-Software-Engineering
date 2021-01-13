3ea054cc131ef03a4352cd2b328ea3c29e07cd1d
Kristiyan Enchev <kristiqnenchevv@gmail.com>
2021-01-13 23:35:14 +0200

    New Add

---
+++ Programming Fundamentals/Lab & Exercices/Associative Arrays - Exercise/08. Company Users/08. Company Users/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _09
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, List<string>> companys = new Dictionary<string, List<string>>();
+
+            string comand = Console.ReadLine();
+            while (comand != "End")
+            {
+                string[] input = comand.Split(" -> ", StringSplitOptions.RemoveEmptyEntries);
+                string companyName = input[0];
+                string employeId = input[1];
+
+                if (!companys.ContainsKey(companyName))
+                {
+                    companys.Add(companyName, new List<string> { employeId });
+                }
+                else
+                {
+                    if (!companys[companyName].Contains(employeId))
+                    {
+                        companys[companyName].Add(employeId);
+                    }
+                }
+
+                comand = Console.ReadLine();
+            }
+
+            foreach (var company in companys.OrderBy(x => x.Key))
+            {
+                Console.WriteLine($"{company.Key}");
+                foreach (var employee in company.Value)
+                {
+                    Console.WriteLine($"-- {employee}");
+                }
+            }
+        }
+    }
+}
