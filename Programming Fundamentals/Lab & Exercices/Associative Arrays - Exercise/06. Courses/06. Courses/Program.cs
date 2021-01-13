0fd3cb26e306399708f9cca1eb67ddf34b16d57f
Kristiyan Enchev <kristiqnenchevv@gmail.com>
2021-01-13 23:34:57 +0200

    New Add

---
+++ Programming Fundamentals/Lab & Exercices/Associative Arrays - Exercise/06. Courses/06. Courses/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _06
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string comand = Console.ReadLine();
+            Dictionary<string, List<string>> courses = new Dictionary<string, List<string>>();
+
+            while (comand != "end")
+            {
+                string[] cmd = comand.Split(" : ", StringSplitOptions.RemoveEmptyEntries);
+                string courceName = cmd[0];
+                string studentName = cmd[1];
+
+                if (!courses.ContainsKey(courceName))
+                {
+                    courses.Add(courceName, new List<string>());
+                }
+                courses[courceName].Add(studentName);
+
+                comand = Console.ReadLine();
+            }
+
+            foreach (var item in courses.OrderByDescending(x => x.Value.Count))
+            {
+                Console.WriteLine($"{item.Key}: {item.Value.Count}");
+                foreach (var itemm in item.Value.OrderBy(x => x))
+                {
+                    Console.WriteLine($"-- {itemm}");
+                }
+            }
+        }
+    }
+}
