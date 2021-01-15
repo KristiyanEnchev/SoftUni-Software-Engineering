aa23426deebbef93f52129da32175d646d18fa41
Kristiyan Enchev <kristiqnenchevv@gmail.com>
2021-01-15 12:13:30 +0200

    New Add

---
+++ Programming Fundamentals/Lab & Exercices/Associative Arrays - Lab/05. Word Filter/05. Word Filter/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ConsoleApp4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> list = Console.ReadLine()
+                .Split()
+                .Where(w => w.Length % 2 == 0)
+                .ToList();
+
+            Console.WriteLine(string.Join(Environment.NewLine, list));
+
+        }
+    }
+}
