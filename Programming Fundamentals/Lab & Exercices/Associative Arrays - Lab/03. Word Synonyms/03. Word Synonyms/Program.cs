5c0214051a9915d46d74d4b615cb073d10d3f65f
Kristiyan Enchev <kristiqnenchevv@gmail.com>
2021-01-15 12:13:13 +0200

    New Add

---
+++ Programming Fundamentals/Lab & Exercices/Associative Arrays - Lab/03. Word Synonyms/03. Word Synonyms/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = int.Parse(Console.ReadLine());
+
+            Dictionary<string, List<string>> dic = new Dictionary<string, List<string>>();
+
+            for (int i = 0; i < num; i++)
+            {
+                string word = Console.ReadLine();
+                string synonim = Console.ReadLine();
+
+                if (!dic.ContainsKey(word))
+                {
+                    dic.Add(word, new List<string>());
+                    dic[word].Add(synonim);
+                }
+                else
+                {
+                    dic[word].Add(synonim);
+                }
+            }
+
+            foreach (var item in dic)
+            {
+                Console.WriteLine($"{item.Key} - {string.Join(", ", item.Value)}");
+            }
+        }
+    }
+}
