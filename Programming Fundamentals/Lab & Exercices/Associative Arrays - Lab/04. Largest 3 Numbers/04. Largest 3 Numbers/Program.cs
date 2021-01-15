913a839fe9ae1e5d025ddadcca313df1efa81d1c
Kristiyan Enchev <kristiqnenchevv@gmail.com>
2021-01-15 12:13:22 +0200

    New Add

---
+++ Programming Fundamentals/Lab & Exercices/Associative Arrays - Lab/04. Largest 3 Numbers/04. Largest 3 Numbers/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> num = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToList();
+
+            List<int> sorted = num.OrderByDescending(n => n).ToList();
+
+            if (sorted.Count > 3)
+            {
+                for (int i = 0; i < 3; i++)
+                {
+                    Console.Write(sorted[i] + " ");
+                }
+            }
+            else
+            {
+                Console.WriteLine(string.Join(" ", sorted));
+            }
+        }
+    }
+}
