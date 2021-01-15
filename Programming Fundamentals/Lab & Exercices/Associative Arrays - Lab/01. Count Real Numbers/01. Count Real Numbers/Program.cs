0210d7de48f513ed74ba0f19d905b5cfd612b5e1
Kristiyan Enchev <kristiqnenchevv@gmail.com>
2021-01-15 12:12:55 +0200

    New Add

---
+++ Programming Fundamentals/Lab & Exercices/Associative Arrays - Lab/01. Count Real Numbers/01. Count Real Numbers/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ConsoleApp74
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double[] number = Console.ReadLine()
+                .Split()
+                .Select(double.Parse)
+                .ToArray();
+
+            SortedDictionary<double, int> dic = new SortedDictionary<double, int>();
+
+            foreach (var num in number)
+            {
+                if (dic.ContainsKey(num))
+                {
+                    dic[num]++;
+                }
+                else
+                {
+                    dic.Add(num, 1);
+                }
+            }
+
+            foreach (var num in dic)
+            {
+                Console.WriteLine($"{num.Key} -> {num.Value}");
+            }
+
+        }
+    }
+}
