b821110961a391dbe940175ec5b51f4a5cc66d4d
Kristiyan Enchev <kristiqnenchevv@gmail.com>
2021-01-15 12:13:03 +0200

    New Add

---
+++ Programming Fundamentals/Lab & Exercices/Associative Arrays - Lab/02. Odd Occurrences/02. Odd Occurrences/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] words = Console.ReadLine().Split();
+
+            Dictionary<string, int> name = new Dictionary<string, int>();
+
+            foreach (var word in words)
+            {
+                string lower = word.ToLower();
+                if (name.ContainsKey(lower))
+                {
+                    name[lower]++;
+                }
+                else
+                {
+                    name.Add(lower, 1);
+                }
+            }
+
+            foreach (var item in name)
+            {
+                if (item.Value % 2 != 0)
+                {
+                    Console.Write(item.Key + " ");
+                }
+            }
+        }
+    }
+}
