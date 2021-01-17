985e823d203e50b2e24eab3285cb4c3e9bcceccf
Kristiyan Enchev <kristiqnenchevv@gmail.com>
2021-01-17 20:30:17 +0200

    New Add

---
+++ Programming Fundamentals/Lab & Exercices/Lists - Exercise/01. Train/01. Train/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace List_Exercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> wagons = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToList();
+
+            int maxCapacity = int.Parse(Console.ReadLine());
+
+            string comand = Console.ReadLine();
+            while (comand != "end")
+            {
+                string[] input = comand.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+                if (input[0] == "Add")
+                {
+                    if (int.Parse(input[1]) >= 0)
+                    {
+                        wagons.Add(int.Parse(input[1]));
+                    }
+                }
+                else
+                {
+                    if (int.Parse(input[0]) >= 0)
+                    {
+                        for (int i = 0; i < wagons.Count; i++)
+                        {
+                            if (wagons[i] + int.Parse(input[0]) <= maxCapacity)
+                            {
+                                wagons[i] += int.Parse(input[0]);
+                                break;
+                            }
+                        }
+                    }
+                }
+
+                comand = Console.ReadLine();
+            }
+
+            Console.WriteLine(string.Join(" ", wagons));
+        }
+    }
+}
