8adfae4ec9e568ea7e82ed4fe949ead1657686c0
Kristiyan Enchev <kristiqnenchevv@gmail.com>
2021-01-17 20:30:36 +0200

    New Add

---
+++ Programming Fundamentals/Lab & Exercices/Lists - Exercise/03. House Party/03. House Party/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+
+namespace _3._House_Party
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            List<string> names = new List<string>();
+            int count = 0;
+            for (int i = 0; i < n; i++)
+            {
+                string[] name = Console.ReadLine().Split();
+                if (names.Contains(name[0]) && name[1] == "is" && name[2] == "going!")
+                {
+                    Console.WriteLine($"{name[0]} is already in the list!");
+                }
+                else
+                {
+                    if (name[2] == "going!")
+                    {
+                        names.Add(name[0]);
+                        count++;
+                    }
+                }
+                if (count > 0)
+                {
+                    if (name[2] == "not")
+                    {
+                        if (!names.Contains(name[0]))
+                        {
+                            Console.WriteLine($"{name[0]} is not in the list!");
+                        }
+                        else
+                        {
+                            names.Remove(name[0]);
+                        }
+                    }
+                }
+                else
+                {
+                    Console.WriteLine($"{name[0]} is not in the list!");
+                }
+            }
+            Console.WriteLine(string.Join("\n", names));
+        }
+    }
+}
