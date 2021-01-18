8577b6cd49311ae6355af2fabbd03b9b4606a693
Kristiyan Enchev <kristiqnenchevv@gmail.com>
2021-01-18 16:29:19 +0200

    New Add

---
+++ Programming Fundamentals/Lab & Exercices/Lists - Lab/02. Gauss' Trick/02. Gauss' Trick/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace _02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<double> sumEquals = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(double.Parse)
+                .ToList();
+            int maxIndex = sumEquals.Count / 2;
+            int originalnumber = sumEquals.Count;
+            for (int i = 0; i < maxIndex; i++)
+            {
+                sumEquals[i] += sumEquals[sumEquals.Count - 1];
+                sumEquals.Remove(sumEquals[sumEquals.Count - 1]);
+            }
+
+            Console.WriteLine(string.Join(" ", sumEquals));
+        }
+    }
+}
