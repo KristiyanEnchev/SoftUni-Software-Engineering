ca53bc2c4143daf33cf063f450a342838cf5d02d
Kristiyan Enchev <kristiqnenchevv@gmail.com>
2021-01-18 16:29:11 +0200

    New Add

---
+++ Programming Fundamentals/Lab & Exercices/Lists - Lab/01. Sum Adjacent Equal Numbers/01. Sum Adjacent Equal Numbers/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Lists_Lab
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<double> sumEquals = Console.ReadLine()
+                .Split()
+                .Select(double.Parse)
+                .ToList();
+
+            for (int i = 0; i < sumEquals.Count - 1; i++)
+            {
+                if (sumEquals[i] == sumEquals[i + 1])
+                {
+                    sumEquals[i] += sumEquals[i + 1];
+                    sumEquals.RemoveAt(i + 1);
+                    i = -1;
+                }
+            }
+            Console.WriteLine(string.Join(" ", sumEquals));
+        }
+    }
+}
