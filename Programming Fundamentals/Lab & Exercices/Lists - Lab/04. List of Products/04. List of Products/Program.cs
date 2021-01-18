e788c092d29ba8ab20276e306589d1c1e76ab38c
Kristiyan Enchev <kristiqnenchevv@gmail.com>
2021-01-18 16:29:35 +0200

    New Add

---
+++ Programming Fundamentals/Lab & Exercices/Lists - Lab/04. List of Products/04. List of Products/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _04
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = int.Parse(Console.ReadLine());
+
+            List<string> productList = new List<string>(num);
+
+            for (int i = 0; i < num; i++)
+            {
+                string product = Console.ReadLine();
+                productList.Add(product);
+            }
+
+            productList.Sort();
+            int number = 0;
+            for (int i = 0; i < productList.Count; i++)
+            {
+                number += 1;
+                productList[i] = $"{number}.{productList[i]}";
+            }
+
+            Console.WriteLine(string.Join(Environment.NewLine, productList));
+        }
+    }
+}
