3bfde145239ceb53d5595e3c6af226aa018b0aa6
Kristiyan Enchev <kristiqnenchevv@gmail.com>
2021-01-19 18:38:21 +0200

    New Add

---
+++ Programming Fundamentals/Lab & Exercices/Methods - Exercise/02. Vowels Count/02. Vowels Count/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string word = Console.ReadLine().ToLower();
+
+        string cowelCount = VowolCountInTheWord(word);
+        Console.WriteLine(cowelCount);
+    }
+
+    static string VowolCountInTheWord(string num)
+    {
+        int count = 0;
+
+        for (int i = 0; i < num.Length; i++)
+        {
+            char letter = num[i];
+            if (letter == 'a' || letter == 'e' ||
+                letter == 'i' || letter == 'o' ||
+                letter == 'u')
+            {
+                count++;
+            }
+        }
+
+        return count.ToString();
+    }
+}
