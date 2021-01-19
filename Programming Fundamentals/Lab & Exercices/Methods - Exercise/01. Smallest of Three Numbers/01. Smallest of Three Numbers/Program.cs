5a05c8b190c763edb2d8048eb8c11c5872b2825c
Kristiyan Enchev <kristiqnenchevv@gmail.com>
2021-01-19 18:38:11 +0200

    New Add

---
+++ Programming Fundamentals/Lab & Exercices/Methods - Exercise/01. Smallest of Three Numbers/01. Smallest of Three Numbers/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int numOne = int.Parse(Console.ReadLine());
+        int numTwo = int.Parse(Console.ReadLine());
+        int numThree = int.Parse(Console.ReadLine());
+
+        Console.WriteLine(Comparision(numOne, numTwo, numThree));
+    }
+
+    static int Comparision(int one, int two, int three)
+    {
+        int result = 0;
+
+        if (one <= two && one <= three)
+        {
+            result = one;
+        }
+        else if (two <= one && two <= three)
+        {
+            result = two;
+        }
+        else if (three <= one && three <= two)
+        {
+            result = three;
+        }
+
+        return result;
+    }
+}
