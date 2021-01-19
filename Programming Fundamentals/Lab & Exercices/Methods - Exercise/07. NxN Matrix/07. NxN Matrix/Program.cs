3a063cc4a3780bd0d5c5d36bd220e9ac1638dc1a
Kristiyan Enchev <kristiqnenchevv@gmail.com>
2021-01-19 18:39:06 +0200

    New Add

---
+++ Programming Fundamentals/Lab & Exercices/Methods - Exercise/07. NxN Matrix/07. NxN Matrix/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace ConsoleApp64
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int matrixNumber = int.Parse(Console.ReadLine());
+
+            Matrix(matrixNumber);
+        }
+
+        public static void Matrix(int matrix)
+        {
+            for (int roll = 0; roll < matrix; roll++)
+            {
+                for (int coll = 0; coll < matrix; coll++)
+                {
+                    Console.Write($"{matrix} ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
