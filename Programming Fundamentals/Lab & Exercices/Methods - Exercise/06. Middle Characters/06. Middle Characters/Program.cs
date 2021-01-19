0c0ae95a2e84ad5f6367ffe31ab17e128cf5894c
Kristiyan Enchev <kristiqnenchevv@gmail.com>
2021-01-19 18:38:53 +0200

    New Add

---
+++ Programming Fundamentals/Lab & Exercices/Methods - Exercise/06. Middle Characters/06. Middle Characters/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace ConsoleApp64
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string word = Console.ReadLine();
+
+            string middleChars = FidingMiddleChars(word);
+            Console.WriteLine(middleChars);
+        }
+
+        public static string FidingMiddleChars(string word)
+        {
+            string newWord = string.Empty;
+
+            for (int i = 0; i < word.Length / 2 + 1; i++)
+            {
+                char letter = word[i];
+                if (word.Length % 2 == 0)
+                {
+                    if (i == word.Length / 2)
+                    {
+                        char letter2 = word[i - 1];
+                        newWord += letter2;
+                        newWord += letter;
+                    }
+                }
+                else
+                {
+                    if (i == word.Length / 2)
+                    {
+                        newWord += word[i];
+                    }
+                }
+            }
+
+            return newWord;
+        }
+    }
+}
