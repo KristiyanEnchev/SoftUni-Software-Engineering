35d840090a53c976abc1524823b4d1519da09494
Kristiyan Enchev <kristiqnenchevv@gmail.com>
2021-01-19 18:39:14 +0200

    New Add

---
+++ Programming Fundamentals/Lab & Exercices/Methods - Exercise/08. Factorial Division/08. Factorial Division/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace ConsoleApp64
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numOne = int.Parse(Console.ReadLine());
+            int numTwo = int.Parse(Console.ReadLine());
+
+            double sumOfFirstFactorial = FactorilSum(numOne);
+            double sumOfSecondFactorial = FactorilSum(numTwo);
+            double sumOfFactorials = sumOfFirstFactorial / sumOfSecondFactorial;
+
+            Console.WriteLine($"{sumOfFactorials:f2}");
+        }
+
+        public static double FactorilSum(int number)
+        {
+            double result = 1;
+            while (number != 1)
+            {
+                result = result * number;
+                number = number - 1;
+            }
+            return result;
+        }
+    }
+}
