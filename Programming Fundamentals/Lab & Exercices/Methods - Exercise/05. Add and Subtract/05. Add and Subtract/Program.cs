9a42e10e21a909cdfcf4d7b3d92ea205870269bd
Kristiyan Enchev <kristiqnenchevv@gmail.com>
2021-01-19 18:38:46 +0200

    New Add

---
+++ Programming Fundamentals/Lab & Exercices/Methods - Exercise/05. Add and Subtract/05. Add and Subtract/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace ConsoleApp64
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int first = int.Parse(Console.ReadLine());
+            int second = int.Parse(Console.ReadLine());
+            int thurd = int.Parse(Console.ReadLine());
+
+            int sum = AddingFirstAndSecond(first, second) - thurd;
+            Console.WriteLine(sum);
+        }
+
+        public static int AddingFirstAndSecond(int first, int second)
+        {
+            int sum = first + second;
+
+            return sum;
+        }
+    }
+}
