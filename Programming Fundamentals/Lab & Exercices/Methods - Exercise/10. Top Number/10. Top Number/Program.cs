c4a7622187267b89cc401b26a26133ba7f56dd65
Kristiyan Enchev <kristiqnenchevv@gmail.com>
2021-01-19 18:39:32 +0200

    New Add

---
+++ Programming Fundamentals/Lab & Exercices/Methods - Exercise/10. Top Number/10. Top Number/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace ConsoleApp64
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int rangeTo = int.Parse(Console.ReadLine());
+
+            AllNumberInRange(rangeTo);
+        }
+
+        public static void AllNumberInRange(int num)
+        {
+            for (int i = 1; i <= num; i++)
+            {
+                bool isOdd = false;
+                string number = i.ToString();
+                int sumOfDigits = 0;
+                for (int j = 0; j < number.Length; j++)
+                {
+                    int parsedNumber = (int)number[j];
+                    if (parsedNumber % 2 != 0)
+                    {
+                        isOdd = true;
+                    }
+                    sumOfDigits += parsedNumber;
+                }
+
+                if (isOdd && sumOfDigits % 8 == 0)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+        }
+    }
+}
