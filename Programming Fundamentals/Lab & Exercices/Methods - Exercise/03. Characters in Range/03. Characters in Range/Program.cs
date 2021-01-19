90ffcfd3f863a9352d1c32139e6bcbc87724b238
Kristiyan Enchev <kristiqnenchevv@gmail.com>
2021-01-19 18:38:29 +0200

    New Add

---
+++ Programming Fundamentals/Lab & Exercices/Methods - Exercise/03. Characters in Range/03. Characters in Range/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace CharactersInRange
+{
+    using System;
+
+    public class EntryPoint
+    {
+        public static void Main()
+        {
+            char inputOne = char.Parse(Console.ReadLine());
+            char inputTwo = char.Parse(Console.ReadLine());
+
+            PrintTheCharsBetweenTwoChars(inputOne, inputTwo);
+        }
+
+        private static void PrintTheCharsBetweenTwoChars(char firstCharacter, char secondCharacter)
+        {
+            int startCharacter = Math.Min(firstCharacter, secondCharacter);
+            int endCharacter = Math.Max(firstCharacter, secondCharacter);
+
+            for (int i = ++startCharacter; i < endCharacter; i++)
+            {
+                Console.Write((char)i + " ");
+            }
+
+            Console.WriteLine();
+        }
+    }
+}
