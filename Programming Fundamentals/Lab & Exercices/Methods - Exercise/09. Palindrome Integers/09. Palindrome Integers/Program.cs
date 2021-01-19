9c7a01499016a28589d4b5d53ace64f43b92347d
Kristiyan Enchev <kristiqnenchevv@gmail.com>
2021-01-19 18:39:22 +0200

    New Add

---
+++ Programming Fundamentals/Lab & Exercices/Methods - Exercise/09. Palindrome Integers/09. Palindrome Integers/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace ConsoleApp64
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string comand = string.Empty;
+            while ((comand = Console.ReadLine()) != "END")
+            {
+                string num = comand;
+                string backwords = Exercice(num);
+                if (backwords == num)
+                {
+                    Console.WriteLine("true");
+                }
+                else
+                {
+                    Console.WriteLine("false");
+                }
+            }
+        }
+
+        public static string Exercice(string num)
+        {
+            string backwords = string.Empty;
+
+            for (int i = num.Length - 1; i >= 0; i--)
+            {
+                backwords += num[i];
+            }
+
+            return backwords;
+        }
+    }
+}
