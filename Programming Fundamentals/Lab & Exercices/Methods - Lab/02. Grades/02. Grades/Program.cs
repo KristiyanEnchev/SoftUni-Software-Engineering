e5cd0b1ae6d6fe82fbd2bef2c278e70ed290fdf9
Kristiyan Enchev <kristiqnenchevv@gmail.com>
2021-01-20 21:48:12 +0200

    New Add

---
+++ Programming Fundamentals/Lab & Exercices/Methods - Lab/02. Grades/02. Grades/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Linq;
+
+namespace ConsoleApp63
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Print(double.Parse(Console.ReadLine()));
+        }
+
+        static void Print(double grade)
+        {
+            string gradeValue = string.Empty;
+
+            if (grade >= 2.00 && grade <= 2.99)
+            {
+                gradeValue = "Fail";
+            }
+            else if (grade >= 3.00 && grade <= 3.49)
+            {
+                gradeValue = "Poor";
+            }
+            else if (grade >= 3.50 && grade <= 4.49)
+            {
+                gradeValue = "Good";
+            }
+            else if (grade >= 4.50 && grade <= 5.49)
+            {
+                gradeValue = "Very good";
+            }
+            else if (grade >= 5.50 && grade <= 6.00)
+            {
+                gradeValue = "Excellent";
+            }
+
+            Console.WriteLine(gradeValue);
+        }
+    }
+}
