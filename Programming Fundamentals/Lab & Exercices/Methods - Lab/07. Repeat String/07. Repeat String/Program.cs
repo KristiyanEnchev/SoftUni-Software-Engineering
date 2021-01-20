051296a8a81be977aad7ed56c1b70d7289511aac
Kristiyan Enchev <kristiqnenchevv@gmail.com>
2021-01-20 21:48:51 +0200

    New Add

---
+++ Programming Fundamentals/Lab & Exercices/Methods - Lab/07. Repeat String/07. Repeat String/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace ConsoleApp63
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string word = Console.ReadLine();
+            int repetitionCount = int.Parse(Console.ReadLine());
+
+            Console.WriteLine(Return(word, repetitionCount));
+        }
+
+        private static string Return(string word, int repetition)
+        {
+            string result = "";
+
+            for (int i = 0; i < repetition; i++)
+            {
+                Console.Write(word);
+            }
+
+            return result;
+        }
+    }
+}
