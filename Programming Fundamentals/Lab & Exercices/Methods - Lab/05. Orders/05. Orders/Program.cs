1ad56b3326128b134db96e8fe4bcbe1a70250f15
Kristiyan Enchev <kristiqnenchevv@gmail.com>
2021-01-20 21:48:35 +0200

    New Add

---
+++ Programming Fundamentals/Lab & Exercices/Methods - Lab/05. Orders/05. Orders/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace ConsoleApp63
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string product = Console.ReadLine();
+            int quantity = int.Parse(Console.ReadLine());
+
+            Purchases(product, quantity);
+        }
+
+        private static void Purchases(string type, int count)
+        {
+            double price = 0;
+            switch (type)
+            {
+                case "coffee":
+                    price = count * 1.50;
+                    break;
+                case "water":
+                    price = count * 1.00;
+                    break;
+                case "coke":
+                    price = count * 1.40;
+                    break;
+                case "snacks":
+                    price = count * 2.00;
+                    break;
+            }
+
+            Console.WriteLine($"{price:f2}");
+        }
+    }
+}
