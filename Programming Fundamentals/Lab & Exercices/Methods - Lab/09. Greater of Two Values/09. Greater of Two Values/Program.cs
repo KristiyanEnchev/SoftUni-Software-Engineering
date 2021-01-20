b7efae2b9d1ebcbb80f2b5f1cb158d3156a8031d
Kristiyan Enchev <kristiqnenchevv@gmail.com>
2021-01-20 21:49:06 +0200

    New Add

---
+++ Programming Fundamentals/Lab & Exercices/Methods - Lab/09. Greater of Two Values/09. Greater of Two Values/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+
+namespace ConsoleApp63
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string type = Console.ReadLine().ToLower();
+
+            if (type == "string")
+            {
+                string one = Console.ReadLine();
+                string two = Console.ReadLine();
+
+                string max = Value(one, two);
+                Console.WriteLine(max);
+            }
+            else if (type == "char")
+            {
+                char one = char.Parse(Console.ReadLine());
+                char two = char.Parse(Console.ReadLine());
+
+                char max = Value(one, two);
+                Console.WriteLine(max);
+            }
+            else if (type == "int")
+            {
+                int one = int.Parse(Console.ReadLine());
+                int two = int.Parse(Console.ReadLine());
+
+                int max = Value(one, two);
+                Console.WriteLine(max);
+            }
+        }
+
+        static char Value(char ch1, char ch2)
+        {
+            if (ch1 > ch2)
+            {
+                return ch1;
+            }
+            else
+            {
+                return ch2;
+            }
+        }
+        static int Value(int num1, int num2)
+        {
+            if (num1 > num2)
+            {
+                return num1;
+            }
+            else
+            {
+                return num2;
+            }
+        }
+        static string Value(string word1, string word2)
+        {
+            if (word1.CompareTo(word2) >= 0)
+            {
+                return word1;
+            }
+            else
+            {
+                return word2;
+            }
+        }
+    }
+}
