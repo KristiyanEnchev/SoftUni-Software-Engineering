40af5c1c07ef78a25d889c062457a8df0777fe13
Kristiyan Enchev <kristiqnenchevv@gmail.com>
2021-01-20 21:48:58 +0200

    New Add

---
+++ Programming Fundamentals/Lab & Exercices/Methods - Lab/08. Math Power/08. Math Power/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace ConsoleApp63
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double num = double.Parse(Console.ReadLine());
+            int power = int.Parse(Console.ReadLine());
+
+            double result = Calculation(num, power);
+            Console.WriteLine(result);
+        }
+
+        static double Calculation(double number, int stepen)
+        {
+            double result = 1;
+            for (int i = 0; i < stepen; i++)
+            {
+                result *= number;
+            }
+
+            return result;
+        }
+    }
+}
