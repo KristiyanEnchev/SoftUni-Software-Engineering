ac8fc61124237c82c0859254ae632ea47b666f73
Kristiyan Enchev <kristiqnenchevv@gmail.com>
2021-01-20 21:48:20 +0200

    New Add

---
+++ Programming Fundamentals/Lab & Exercices/Methods - Lab/03. Calculations/03. Calculations/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace ConsoleApp63
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string comand = Console.ReadLine();
+            int numberOne = int.Parse(Console.ReadLine());
+            int numberTwo = int.Parse(Console.ReadLine());
+
+            switch (comand)
+            {
+                case "add":
+                    Adding(numberOne, numberTwo);
+                    break;
+                case "subtract":
+                    Substraction(numberOne, numberTwo);
+                    break;
+                case "multiply":
+                    Multiplication(numberOne, numberTwo);
+                    break;
+                case "divide":
+                    Division(numberOne, numberTwo);
+                    break;
+            }
+        }
+
+        private static void Substraction(int numOne, int numTwo)
+        {
+            Console.WriteLine(numOne - numTwo);
+        }
+        private static void Division(int numOne, int numTwo)
+        {
+            Console.WriteLine(numOne / numTwo);
+        }
+        private static void Multiplication(int numOne, int numTwo)
+        {
+            Console.WriteLine(numOne * numTwo);
+        }
+        private static void Adding(int numOne, int numTwo)
+        {
+            Console.WriteLine(numOne + numTwo);
+        }
+    }
+}
