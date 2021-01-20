8c8fc90c60e2462e145a343639bbe6278ea4cfce
Kristiyan Enchev <kristiqnenchevv@gmail.com>
2021-01-20 21:49:14 +0200

    New Add

---
+++ Programming Fundamentals/Lab & Exercices/Methods - Lab/10. Multiply Evens by Odds/10. Multiply Evens by Odds/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Linq;
+
+namespace ConsoleApp63
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = Math.Abs(int.Parse(Console.ReadLine()));
+
+            int result = Result(num);
+            Console.WriteLine(result);
+        }
+
+        static int Result(int num)
+        {
+            int rezult = OddNumbersInNum(num) * EvenNumbersInNum(num);
+            return rezult;
+        }
+        static int EvenNumbersInNum(int num)
+        {
+
+            int sumOfEvens = 0;
+            while (num != 0)
+            {
+                int nextNum = num % 10;
+
+                if (nextNum % 2 == 0)
+                {
+                    sumOfEvens += nextNum;
+                }
+                num -= nextNum;
+                num /= 10;
+
+            }
+
+            return sumOfEvens;
+        }
+        static int OddNumbersInNum(int num)
+        {
+
+            int sumOfOdds = 0;
+
+            while (num != 0)
+            {
+                int nextNum = num % 10;
+
+                if (nextNum % 2 == 1)
+                {
+                    sumOfOdds += nextNum;
+                }
+                num -= nextNum;
+                num /= 10;
+
+            }
+
+            return sumOfOdds;
+        }
+    }
+}
