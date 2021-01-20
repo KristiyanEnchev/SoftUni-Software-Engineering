e110cfc960668ce1f2d25e4d9dd08324befbc3fa
Kristiyan Enchev <kristiqnenchevv@gmail.com>
2021-01-20 21:48:27 +0200

    New Add

---
+++ Programming Fundamentals/Lab & Exercices/Methods - Lab/04. Printing Triangle/04. Printing Triangle/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace ConsoleApp63
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int input = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i < input; i++)
+            {
+                PrintLine(1, i);
+            }
+
+            for (int i = input; i >= 1; i--)
+            {
+                PrintLine(1, i);
+            }
+
+        }
+
+        static void PrintLine(int start, int end)
+        {
+            for (int i = start; i <= end; i++)
+            {
+                Console.Write(i + " ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
