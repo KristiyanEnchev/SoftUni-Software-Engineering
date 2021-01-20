fa98da7a34e332dca23b506dfc14d86ced3741ed
Kristiyan Enchev <kristiqnenchevv@gmail.com>
2021-01-20 21:49:23 +0200

    New Add

---
+++ Programming Fundamentals/Lab & Exercices/Methods - Lab/11. Math operations/11. Math operations/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace ConsoleApp63
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double numOne = double.Parse(Console.ReadLine());
+            string operatior = Console.ReadLine();
+            double numTwo = double.Parse(Console.ReadLine());
+
+            double result = Operation(numOne, operatior, numTwo);
+            Console.WriteLine(result);
+        }
+
+        static double Operation(double num1, string operatorr, double num2)
+        {
+            double result = 0;
+            if (operatorr == "*")
+            {
+                result = num1 * num2;
+            }
+            else if (operatorr == "/")
+            {
+                result = num1 / num2;
+            }
+            else if (operatorr == "+")
+            {
+                result = num1 + num2;
+            }
+            else if (operatorr == "-")
+            {
+                result = num1 - num2;
+            }
+
+            return result;
+        }
+    }
+}
