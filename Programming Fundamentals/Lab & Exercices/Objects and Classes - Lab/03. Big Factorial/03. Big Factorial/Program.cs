653edf094faceee4aaec1ec722df9eea43340ca2
Kristiyan Enchev <kristiqnenchevv@gmail.com>
2021-01-23 19:35:23 +0200

    New Add

---
+++ Programming Fundamentals/Lab & Exercices/Objects and Classes - Lab/03. Big Factorial/03. Big Factorial/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Numerics;
+
+namespace Object_and_Classes
+{
+    class BigFactorial
+    {
+        static void Main(string[] args)
+        {
+            BigInteger factorial = 1;
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 2; i <= n; i++)
+            {
+                factorial = factorial * i;
+            }
+
+            Console.WriteLine(factorial);
+        }
+    }
+}
