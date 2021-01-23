7b671648340b9d1a0f5b820377d6ac76be49cc35
Kristiyan Enchev <kristiqnenchevv@gmail.com>
2021-01-23 19:35:11 +0200

    New Add

---
+++ Programming Fundamentals/Lab & Exercices/Objects and Classes - Lab/01. Randomize Words/01. Randomize Words/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+
+namespace Objects_and_Classes
+{
+    class RandomizeWords
+    {
+        static void Main(string[] args)
+        {
+            var rand = new Random();
+            List<string> wordsList = Console.ReadLine()
+                .Split(' ')
+                .ToList();
+            int wordsCount = wordsList.Count;
+            for (int i = 0; i < wordsCount - 1; i++)
+            {
+                int j = rand.Next(0, wordsCount);
+                if (i != j)
+                {
+                    string old = wordsList[i];
+                    wordsList[i] = wordsList[j];
+                    wordsList[j] = old;
+                }
+            }
+
+            foreach (string word in wordsList)
+            {
+                Console.WriteLine(word);
+            }
+        }
+    }
+}
