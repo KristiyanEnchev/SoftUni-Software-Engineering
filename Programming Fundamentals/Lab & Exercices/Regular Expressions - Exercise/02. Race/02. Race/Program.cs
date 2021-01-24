8045328b1e308b3b9457c7a2444d3cb53209733e
Kristiyan Enchev <kristiqnenchevv@gmail.com>
2021-01-24 23:40:38 +0200

    New Add

---
+++ Programming Fundamentals/Lab & Exercices/Regular Expressions - Exercise/02. Race/02. Race/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Text.RegularExpressions;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace _02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] people = Console.ReadLine().Split(", ");
+
+            Dictionary<string, int> dic = new Dictionary<string, int>();
+
+            foreach (var item in people)
+            {
+                dic.Add(item, 0);
+            }
+
+            string patternDigit = @"[\W\d]";
+            string patternLeters = @"[\WA-Za-z]";
+
+            string input = Console.ReadLine();
+
+            while (input != "end of race")
+            {
+                string name = Regex.Replace(input, patternDigit, "");
+                string distance = Regex.Replace(input, patternLeters, "");
+                int sum = 0;
+
+                foreach (var digit in distance)
+                {
+                    int currentDigit = int.Parse(digit.ToString());
+                    sum += currentDigit;
+                }
+
+                if (dic.ContainsKey(name))
+                {
+                    dic[name] += sum;
+                }
+
+                input = Console.ReadLine();
+            }
+
+            int count = 1;
+
+            foreach (var kvp in dic.OrderByDescending(x => x.Value))
+            {
+                string text = count == 1 ? "st" : count == 2 ? "nd" : "rd";
+
+                Console.WriteLine($"{count++}{text} place: {kvp.Key}");
+
+                if (count == 4)
+                {
+                    break;
+                }
+            }
+        }
+    }
+}
