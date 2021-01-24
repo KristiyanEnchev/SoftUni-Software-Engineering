ec0add88971a2a423c7151421e8a62e0e5408295
Kristiyan Enchev <kristiqnenchevv@gmail.com>
2021-01-24 23:40:46 +0200

    New Add

---
+++ Programming Fundamentals/Lab & Exercices/Regular Expressions - Exercise/03. SoftUni Bar Income/03. SoftUni Bar Income/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Text.RegularExpressions;
+using System.Linq;
+
+namespace _03
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string pattern = @"%([A-Z]{1}[a-z]+)%[^|$%.]*<(\w+)>[^|$%.]*\|(\d+)\|[^|$%.]*?(\d+\.?\d*)\$";
+
+            Regex regex = new Regex(pattern);
+
+            string input = Console.ReadLine();
+            double total = 0;
+
+            while (input != "end of shift")
+            {
+                Match mach = regex.Match(input);
+
+                if (mach.Success)
+                {
+                    string costomur = mach.Groups[1].Value;
+                    string product = mach.Groups[2].Value;
+                    int count = int.Parse(mach.Groups[3].Value);
+                    double price = double.Parse(mach.Groups[4].Value);
+
+                    double sum = count * price;
+                    total += sum;
+
+                    Console.WriteLine($"{costomur}: {product} - {sum:f2}");
+                }
+
+                input = Console.ReadLine();
+            }
+
+            Console.WriteLine($"Total income: {total:f2}");
+        }
+    }
+}
