82da06b7b3a09792451a213ba406a99e6d11a67f
Kristiyan Enchev <kristiqnenchevv@gmail.com>
2021-01-25 20:33:14 +0200

    New Add

---
+++ Programming Fundamentals/Lab & Exercices/Text Processing - Exercise/03. Extract File/03. Extract File/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Linq;
+
+namespace _03
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] mass = Console.ReadLine().Split("\\");
+            int index = mass.Length - 1;
+            string[] last = mass[index].Split(".");
+
+            Console.WriteLine($"File name: {last[0]}");
+            Console.WriteLine($"File extension: {last[1]}");
+        }
+    }
+}
