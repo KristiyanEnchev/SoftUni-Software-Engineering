96d279d7b2bd521755eeeb810f635bcfcb189ab1
Kristiyan Enchev <kristiqnenchevv@gmail.com>
2021-01-25 20:36:21 +0200

    New Add

---
+++ Programming Fundamentals/Lab & Exercices/Text Processing - Exercise/08. Letters Change Numbers/08. Letters Change Numbers/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Linq;
+
+namespace ConsoleApp75
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] array = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .ToArray();
+
+            double total = 0;
+
+            foreach (var item in array)
+            {
+                string letters = string.Empty;
+                letters += item[0];
+                letters += item[item.Length - 1];
+                string newItem = item.Remove(0, 1);
+                newItem = newItem.Remove(newItem.Length - 1, 1);
+                int number = int.Parse(newItem);
+
+                for (int i = 0; i < letters.Length; i++)
+                {
+                    if (i == 0 && char.IsUpper(letters[i]))
+                    {
+                        double position = letters[i] - 64;
+
+                        total += number / position;
+                    }
+                    else if (i == 0 && char.IsLower(letters[i]))
+                    {
+                        double position = letters[i] - 96;
+
+                        total += number * position;
+                    }
+
+                    if (i == letters.Length - 1 && char.IsUpper(letters[i]))
+                    {
+                        double position = letters[i] - 64;
+
+                        total = total - position;
+                    }
+                    else if (i == letters.Length - 1 && char.IsLower(letters[i]))
+                    {
+
+                        double position = letters[i] - 96;
+
+                        total = total + position;
+                    }
+                }
+            }
+
+            Console.WriteLine($"{total:f2}");
+        }
+    }
+}
