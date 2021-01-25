65fb459d28340ff0acf0d4f44fed41a8b13b1459
Kristiyan Enchev <kristiqnenchevv@gmail.com>
2021-01-25 20:30:27 +0200

    New Add

---
+++ Programming Fundamentals/Lab & Exercices/Regular Expressions - Lab/01. Match Full Name/01. Match Full Name/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace Regexx
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = Console.ReadLine();
+
+            string pattern = @"(\b[A-Z]{1}[a-z]+\s[A-Z][a-z]+\b)";
+
+            MatchCollection colection = Regex.Matches(text, pattern);
+
+            foreach (Match item in colection)
+            {
+                Console.Write(item.Value + " ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
