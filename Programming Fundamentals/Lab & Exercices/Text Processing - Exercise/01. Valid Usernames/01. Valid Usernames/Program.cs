539de6aa65bd0938bbbc1814751ea6d9b632fcc3
Kristiyan Enchev <kristiqnenchevv@gmail.com>
2021-01-25 20:30:57 +0200

    New Add

---
+++ Programming Fundamentals/Lab & Exercices/Text Processing - Exercise/01. Valid Usernames/01. Valid Usernames/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Linq;
+
+namespace Text_processing_Exercice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] pass = Console.ReadLine()
+                .Split(", ", StringSplitOptions.RemoveEmptyEntries)
+                .ToArray();
+
+            foreach (var password in pass)
+            {
+                if (password.Length >= 3 && password.Length <= 16)
+                {
+                    if (password.All(x => char.IsLetterOrDigit(x)) || password.Contains("-") || password.Contains("_"))
+                    {
+                        Console.WriteLine(password);
+                    }
+                }
+            }
+        }
+    }
+}
