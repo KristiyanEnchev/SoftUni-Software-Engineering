a58c6a1dcc957c88fe67dae018ad81401511546a
Kristiyan Enchev <kristiqnenchevv@gmail.com>
2021-01-25 20:30:36 +0200

    New Add

---
+++ Programming Fundamentals/Lab & Exercices/Regular Expressions - Lab/02. Match Phone Number/02. Match Phone Number/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Text.RegularExpressions;
+using System.Linq;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string pattern = @"([+][359]+[-][2])([-]\d{3})([-]\d{4}\b)|([+][359]+[ ][2])([ ]\d{3})([ ]\d{4}\b)";
+
+            string text = Console.ReadLine();
+
+            MatchCollection colection = Regex.Matches(text, pattern);
+
+            string[] machText = colection.Select(X => X.Value.Trim()).ToArray();
+
+            Console.WriteLine(string.Join(", ", machText));
+        }
+    }
+}
