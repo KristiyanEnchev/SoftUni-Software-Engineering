bd485c0f9e264c4db1db99c0a4bb4ffef0fee8a2
Kristiyan Enchev <kristiqnenchevv@gmail.com>
2021-01-25 20:33:27 +0200

    New Add

---
+++ Programming Fundamentals/Lab & Exercices/Text Processing - Exercise/04. Caesar Cipher/04. Caesar Cipher/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Linq;
+
+namespace _04
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string array = Console.ReadLine();
+
+            string encription = string.Empty;
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                int newCharecter = array[i] + 3;
+                char newChar = (char)newCharecter;
+                encription += newChar;
+            }
+
+            Console.WriteLine(encription);
+        }
+    }
+}
