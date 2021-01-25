f894b0f602e316f96b5544f277f109eb215897c8
Kristiyan Enchev <kristiqnenchevv@gmail.com>
2021-01-25 20:35:58 +0200

    New Add

---
+++ Programming Fundamentals/Lab & Exercices/Text Processing - Exercise/06. Replace Repeating Chars/06. Replace Repeating Chars/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Linq;
+
+namespace _06
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string massege = Console.ReadLine();
+
+            for (int i = 0; i < massege.Length - 1; i++)
+            {
+                if (massege[i] == massege[i + 1])
+                {
+                    massege = massege.Remove(i + 1, 1);
+                    i -= 1;
+                }
+            }
+
+            Console.WriteLine(massege);
+        }
+    }
+}
