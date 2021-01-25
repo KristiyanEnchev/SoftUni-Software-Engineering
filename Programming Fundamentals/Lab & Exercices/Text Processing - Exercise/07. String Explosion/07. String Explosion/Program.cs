979b60d045e189e86df1a4bcf18f4cd8d1be32c6
Kristiyan Enchev <kristiqnenchevv@gmail.com>
2021-01-25 20:36:10 +0200

    New Add

---
+++ Programming Fundamentals/Lab & Exercices/Text Processing - Exercise/07. String Explosion/07. String Explosion/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace _07
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string line = Console.ReadLine();
+            int power = 0;
+            for (int i = 0; i < line.Length; i++)
+            {
+                if (line[i] == '>')
+                {
+                    power += int.Parse(line[i + 1].ToString());
+                    continue;
+                }
+
+                if (power > 0)
+                {
+                    line = line.Remove(i, 1);
+                    i--;
+                    power--;
+                }
+            }
+
+            Console.WriteLine(line);
+        }
+    }
+}
