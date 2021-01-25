7cc87676de268c788eb236220858466e5be79b2f
Kristiyan Enchev <kristiqnenchevv@gmail.com>
2021-01-25 20:35:48 +0200

    New Add

---
+++ Programming Fundamentals/Lab & Exercices/Text Processing - Exercise/05. Multiply Big Number/05. Multiply Big Number/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+
+namespace _05
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string num = Console.ReadLine().TrimStart('0');
+            int multiplyer = int.Parse(Console.ReadLine());
+            int temp = 0;
+            var sb = new StringBuilder();
+
+            if (multiplyer == 0 || num == "")
+            {
+                Console.WriteLine(0);
+                return;
+            }
+
+            foreach (var charr in num.Reverse())
+            {
+                int digit = int.Parse(charr.ToString());
+                int result = digit * multiplyer + temp;
+
+                int rest = result % 10;
+                temp = result / 10;
+
+                sb.Insert(0, rest);
+            }
+
+            if (temp > 0)
+            {
+                sb.Insert(0, temp);
+            }
+
+            Console.WriteLine(sb.ToString());
+        }
+    }
+}
