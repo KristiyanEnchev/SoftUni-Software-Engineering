9b1360a78feae652db8fa9426f6a87e51d754c1f
Kristiyan Enchev <kristiqnenchevv@gmail.com>
2021-01-26 20:00:14 +0200

    New Add

---
+++ Programming Fundamentals/Lab & Exercices/Text Processing - Lab/03. Substring/03. Substring/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Linq;
+
+namespace _03
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string one = Console.ReadLine().ToLower();
+            string two = Console.ReadLine().ToLower();
+            int index = two.IndexOf(one);
+
+            while (two.Contains(one))
+            {
+                two = two.Remove(index, one.Length);
+
+                index = two.IndexOf(one);
+            }
+
+            Console.WriteLine(two);
+        }
+    }
+}
