54d46ed98d38fd43362b2b5b941bf89effd962f2
Kristiyan Enchev <kristiqnenchevv@gmail.com>
2021-01-26 20:00:05 +0200

    New Add

---
+++ Programming Fundamentals/Lab & Exercices/Text Processing - Lab/02. Repeat Strings/02. Repeat Strings/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Linq;
+
+namespace _02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] input = Console.ReadLine()
+                .Split()
+                .ToArray();
+
+            foreach (var item in input)
+            {
+                foreach (var letter in item)
+                {
+                    Console.Write(item);
+                }
+            }
+        }
+    }
+}
