9b22e9a29a1baeb78b0a99c30fadd2043d28a323
Kristiyan Enchev <kristiqnenchevv@gmail.com>
2021-01-26 20:00:34 +0200

    New Add

---
+++ Programming Fundamentals/Lab & Exercices/Text Processing - Lab/05. Digits, Letters and Other/05. Digits, Letters and Other/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace _05
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string word = Console.ReadLine();
+
+            string digit = string.Empty;
+            string letter = string.Empty;
+            string elsee = string.Empty;
+
+            foreach (var charecter in word)
+            {
+                char ch = charecter;
+
+                if (ch >= '0' && ch <= '9')
+                {
+                    digit += charecter;
+                }
+                else if (ch >= 'a' && ch <= 'z' || ch >= 'A' && ch <= 'Z')
+                {
+                    letter += charecter;
+                }
+                else
+                {
+                    elsee += charecter;
+                }
+            }
+
+            Console.WriteLine(digit);
+            Console.WriteLine(letter);
+            Console.WriteLine(elsee);
+        }
+    }
+}
