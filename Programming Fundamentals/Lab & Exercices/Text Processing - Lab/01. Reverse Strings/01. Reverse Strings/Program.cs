244dc827a390f75b54629db403ceeb502e7cefcf
Kristiyan Enchev <kristiqnenchevv@gmail.com>
2021-01-26 19:59:56 +0200

    New Add

---
+++ Programming Fundamentals/Lab & Exercices/Text Processing - Lab/01. Reverse Strings/01. Reverse Strings/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Linq;
+
+namespace Text_Processing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string comand = Console.ReadLine();
+            while (comand != "end")
+            {
+                string newWord = string.Empty;
+                for (int i = comand.Length - 1; i >= 0; i--)
+                {
+                    newWord += comand[i];
+                }
+
+                Console.WriteLine($"{comand} = {newWord}");
+
+                comand = Console.ReadLine();
+            }
+        }
+    }
+}
