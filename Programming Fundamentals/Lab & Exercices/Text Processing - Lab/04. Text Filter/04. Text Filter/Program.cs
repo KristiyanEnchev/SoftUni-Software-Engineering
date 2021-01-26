25e818e0a02c1ad53925f26e10ef5d968d32c6a1
Kristiyan Enchev <kristiqnenchevv@gmail.com>
2021-01-26 20:00:25 +0200

    New Add

---
+++ Programming Fundamentals/Lab & Exercices/Text Processing - Lab/04. Text Filter/04. Text Filter/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace _04
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> list = Console.ReadLine()
+                .Split(", ")
+                .ToList();
+
+            string text = Console.ReadLine();
+
+            foreach (var word in list)
+            {
+                string replacment = new string('*', word.Length);
+
+                text = text.Replace(word, replacment);
+            }
+
+            Console.WriteLine(text);
+        }
+    }
+}
