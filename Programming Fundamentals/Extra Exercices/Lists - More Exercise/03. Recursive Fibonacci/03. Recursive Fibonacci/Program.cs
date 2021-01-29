5c31967bc75ab3205314522d4970071ee58847db
Kristiyan Enchev <kristiqnenchevv@gmail.com>
2021-01-29 21:59:10 +0200

    New Add

---
+++ Programming Fundamentals/Extra Exercices/Lists - More Exercise/03. Recursive Fibonacci/03. Recursive Fibonacci/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Linq;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int input = int.Parse(Console.ReadLine());
+        int[] arr = new int[input + 2];
+        arr[0] = 1;
+        arr[1] = 1;
+
+        for (int i = 0; i < arr.Length - 2; i++)
+        {
+            int num = arr[i] + arr[i + 1];
+            arr[i + 2] = num;
+        }
+
+        Console.WriteLine(arr[input - 1]);
+    }
+}
