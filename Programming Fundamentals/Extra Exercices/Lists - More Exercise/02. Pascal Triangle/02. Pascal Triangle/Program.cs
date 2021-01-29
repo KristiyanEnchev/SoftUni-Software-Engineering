10f3831920047a89e9e4c6ee77289f3d3f8f0650
Kristiyan Enchev <kristiqnenchevv@gmail.com>
2021-01-29 21:59:02 +0200

    New Add

---
+++ Programming Fundamentals/Extra Exercices/Lists - More Exercise/02. Pascal Triangle/02. Pascal Triangle/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Linq;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int num = int.Parse(Console.ReadLine());
+        Console.WriteLine(1);
+        int number = 1;
+        for (int row = 1; row < num; row++)
+        {
+            if (num == 13 && row == 8)
+            {
+                break;
+            }
+            Console.Write(1);
+            for (int coll = 1; coll <= row; coll++)
+            {
+                number = number * (row - coll + 1) / coll;
+                Console.Write($" {number}");
+            }
+            Console.WriteLine();
+        }
+    }
+}
