690933fc12795879cb302f89a920ee6316f25d1d
Kristiyan Enchev <kristiqnenchevv@gmail.com>
2021-01-29 21:58:52 +0200

    New Add

---
+++ Programming Fundamentals/Extra Exercices/Lists - More Exercise/01. Encrypt, Sort and Print Array/01. Encrypt, Sort and Print Array/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Linq;
+
+namespace ConsoleApp63
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberOfString = int.Parse(Console.ReadLine());
+
+            int vowelSum = 0;
+            int consonantSum = 0;
+            int nameSum = 0;
+            int[] allNameSum = new int[numberOfString];
+
+            for (int i = 0; i < numberOfString; i++)
+            {
+                string name = Console.ReadLine();
+
+                for (int j = 0; j < name.Length; j++)
+                {
+                    char letter = name[j];
+                    if (letter == 'a' || letter == 'A' || letter == 'u' || letter == 'U' || letter == 'e' ||
+                        letter == 'E' || letter == 'i' || letter == 'I' || letter == 'o' || letter == 'O')
+                    {
+                        vowelSum += name[j] * name.Length;
+                    }
+                    else
+                    {
+                        consonantSum += name[j] / name.Length;
+                    }
+                }
+                nameSum = vowelSum + consonantSum;
+                allNameSum[i] = nameSum;
+                vowelSum = 0;
+                consonantSum = 0;
+            }
+            Array.Sort(allNameSum);
+            for (int i = 0; i < allNameSum.Length; i++)
+            {
+                Console.WriteLine(allNameSum[i]);
+            }
+        }
+    }
+}
