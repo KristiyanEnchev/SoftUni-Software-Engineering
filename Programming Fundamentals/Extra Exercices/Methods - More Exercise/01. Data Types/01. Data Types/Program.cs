88c8bdfd6b19d55ff728f679c4987ef40b3fae8b
Kristiyan Enchev <kristiqnenchevv@gmail.com>
2021-01-31 11:29:03 +0200

    New Add

---
+++ Programming Fundamentals/Extra Exercices/Methods - More Exercise/01. Data Types/01. Data Types/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace ConsoleApp64
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+
+            if (input == "int")
+            {
+                int number = int.Parse(Console.ReadLine());
+                IntegerMethoot(number);
+            }
+            else if (input == "real")
+            {
+                double number = double.Parse(Console.ReadLine());
+                RealNumbersMethood(number);
+            }
+            else if (input == "string")
+            {
+                string result = Console.ReadLine();
+                StringMethoot(result);
+            }
+        }
+
+        public static void IntegerMethoot(int num)
+        {
+            int result = num * 2;
+            Console.WriteLine(result);
+        }
+
+        public static void RealNumbersMethood(double num)
+        {
+            double result = num * 1.5;
+            Console.WriteLine($"{result:f2}");
+        }
+
+        public static void StringMethoot(string number)
+        {
+            string output = $"${number}$";
+            Console.WriteLine(output);
+        }
+    }
+}
