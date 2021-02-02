2af73b32b36798157d894d327d2c8c73aedb341a
Kristiyan Enchev <kristiqnenchevv@gmail.com>
2021-02-02 12:12:06 +0200

    New Add

---
+++ Programming Fundamentals/Extra Exercices/Regular Expressions - More Exercise/3. Post Office/3. Post Office/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace PostOffice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] input = Console.ReadLine().Split("|");
+
+            string firstPart = input[0];
+            string secondPart = input[1];
+            string thirdPart = input[2];
+
+            string firstPattern = @"([#$%*&])(?<capitals>[A-Z]+)(\1)";
+            Match firstMatch = Regex.Match(firstPart, firstPattern);
+            string capitals = firstMatch.Groups["capitals"].Value;
+
+            for (int index = 0; index < capitals.Length; index++)
+            {
+                char startLetter = capitals[index];
+                int ASCIIcode = startLetter;
+
+                string secondPattern = $@"{ASCIIcode}:(?<length>[0-9][0-9])";
+                Match secondMatch = Regex.Match(secondPart, secondPattern);
+                int length = int.Parse(secondMatch.Groups["length"].Value);
+
+                string thirdPattern = $@"(?<=\s|^){startLetter}[^\s]{{{length}}}(?=\s|$)";
+                Match thirdMatch = Regex.Match(thirdPart, thirdPattern);
+                string word = thirdMatch.ToString();
+
+                Console.WriteLine(word);
+            }
+        }
+    }
+}
