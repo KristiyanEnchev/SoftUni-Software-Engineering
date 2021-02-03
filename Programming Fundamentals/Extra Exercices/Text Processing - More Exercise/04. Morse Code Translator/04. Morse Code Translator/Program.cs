c72b55053ad5986d49dc929b12a6ada8de305b1e
Kristiyan Enchev <kristiqnenchevv@gmail.com>
2021-02-03 18:45:53 +0200

    New Add

---
+++ Programming Fundamentals/Extra Exercices/Text Processing - More Exercise/04. Morse Code Translator/04. Morse Code Translator/Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _04
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] sequance = Console.ReadLine()
+                .Split("|", StringSplitOptions.RemoveEmptyEntries);
+
+            Dictionary<string, char> morseCode = MorseCodeInicialization();
+
+            string sentence = string.Empty;
+
+            foreach (var item in sequance)
+            {
+                string[] newSequance = item.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+                foreach (var letter in newSequance)
+                {
+                    if (morseCode.ContainsKey(letter))
+                    {
+                        char ch = morseCode[letter];
+                        sentence += ch;
+                    }
+                }
+                sentence += " ";
+            }
+
+            Console.WriteLine(sentence.ToUpper()); ;
+        }
+
+        private static Dictionary<string, char> MorseCodeInicialization()
+        {
+            Dictionary<string, char> morseCodeDictionary = new Dictionary<string, char>() {
+                                       {".-",'a'},
+                                       {"-...",'b'},
+                                       {"-.-.",'c'},
+                                       {"-..",'d'},
+                                       {".",'e'},
+                                       {"..-.",'f'},
+                                       {"--.",'g'},
+                                       {"....",'h'},
+                                       {"..",'i'},
+                                       {".---",'j'},
+                                       {"-.-",'k'},
+                                       {".-..",'l'},
+                                       {"--",'m'},
+                                       {"-.",'n'},
+                                       {"---",'o'},
+                                       {".--.",'p'},
+                                       {"--.-",'q'},
+                                       {".-.",'r'},
+                                       {"...",'s'},
+                                       {"-",'t'},
+                                       {"..-",'u'},
+                                       {"...-",'v'},
+                                       {".--",'w'},
+                                       {"-..-",'x'},
+                                       {"-.--",'y'},
+                                       {"--..",'z'},
+                                       {"-----",'0'},
+                                       {".----",'1'},
+                                       {"..---",'2'},
+                                       {"...--",'3'},
+                                       {"....-",'4'},
+                                       {".....",'5'},
+                                       {"-....",'6'},
+                                       {"--...",'7'},
+                                       {"---..",'8'},
+                                       {"----.",'9'}
+                                  };
+
+            return morseCodeDictionary;
+        }
+    }
+}
