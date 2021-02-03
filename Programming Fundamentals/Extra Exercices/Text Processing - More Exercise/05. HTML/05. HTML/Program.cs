4b66d07cbee5de1ad611e08c1dae1e8715813a00
Kristiyan Enchev <kristiqnenchevv@gmail.com>
2021-02-03 18:46:02 +0200

    New Add

---
+++ Programming Fundamentals/Extra Exercices/Text Processing - More Exercise/05. HTML/05. HTML/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace _05
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> html = new List<string>();
+
+            string title = Console.ReadLine();
+            html.Add("<h1>");
+            html.Add(title);
+            html.Add("</h1>");
+            string content = Console.ReadLine();
+            html.Add("<article>");
+            html.Add(content);
+            html.Add("</article>");
+
+            while (true)
+            {
+                string coments = Console.ReadLine();
+                if (coments == "end of comments")
+                {
+                    break;
+                }
+
+                html.Add("<div>");
+                html.Add(coments);
+                html.Add("</div>");
+            }
+
+            Console.WriteLine(string.Join(Environment.NewLine, html));
+        }
+    }
+}
