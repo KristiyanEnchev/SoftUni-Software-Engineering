28527f1cad260baa0e47ea925acf527cf7feef44
Kristiyan Enchev <kristiqnenchevv@gmail.com>
2021-02-03 18:45:36 +0200

    New Add

---
+++ Programming Fundamentals/Extra Exercices/Text Processing - More Exercise/02. Ascii Sumator/02. Ascii Sumator/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace _02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char one = char.Parse(Console.ReadLine());
+            char two = char.Parse(Console.ReadLine());
+
+            int numOne = one;
+            int numTwo = two;
+
+            string massage = Console.ReadLine();
+
+            int total = 0;
+
+            for (int i = 0; i < massage.Length; i++)
+            {
+                char charecter = massage[i];
+                int num = charecter;
+                if (num > one && num < two || num > two && num < one)
+                {
+                    total += num;
+                }
+            }
+
+            Console.WriteLine(total);
+        }
+    }
+}
