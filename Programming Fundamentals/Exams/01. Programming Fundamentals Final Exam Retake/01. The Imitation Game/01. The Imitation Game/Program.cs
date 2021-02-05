805abe1e899d5b6330e1f420ad63523863ac919e
Kristiyan Enchev <kristiqnenchevv@gmail.com>
2021-02-05 17:46:05 +0200

    New Add

---
+++ Programming Fundamentals/Exams/01. Programming Fundamentals Final Exam Retake/01. The Imitation Game/01. The Imitation Game/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace ConsoleApp4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string massage = Console.ReadLine();
+
+            string comand = Console.ReadLine();
+            while (comand != "Decode")
+            {
+                string[] input = comand.Split("|");
+                string cmd = input[0];
+
+                if (cmd == "Move")
+                {
+                    int count = int.Parse(input[1]);
+                    string substring = massage.Substring(0, count);
+                    massage = massage.Replace(substring, "");
+                    massage = massage.Insert(massage.Length, substring);
+
+                }
+                else if (cmd == "Insert")
+                {
+                    int index = int.Parse(input[1]);
+                    string substring = input[2];
+
+                    massage = massage.Insert(index, substring);
+                }
+                else if (cmd == "ChangeAll")
+                {
+                    string substring = input[1];
+                    string replacment = input[2];
+
+                    massage = massage.Replace(substring, replacment);
+                }
+
+                comand = Console.ReadLine();
+            }
+
+            Console.WriteLine($"The decrypted message is: {massage}");
+        }
+    }
+}
