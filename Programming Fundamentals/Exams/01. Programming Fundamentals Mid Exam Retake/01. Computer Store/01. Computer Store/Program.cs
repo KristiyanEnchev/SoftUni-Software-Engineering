2b4757df547171c614f9bd17d851dec7e09b2870
Kristiyan Enchev <kristiqnenchevv@gmail.com>
2021-02-05 17:46:33 +0200

    New Add

---
+++ Programming Fundamentals/Exams/01. Programming Fundamentals Mid Exam Retake/01. Computer Store/01. Computer Store/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+
+namespace New_Mid_Exam_Prep
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string comand = Console.ReadLine();
+            double noTaxPrice = 0;
+            double tax = 0;
+            while (true)
+            {
+                if (comand == "special" || comand == "regular")
+                {
+                    break;
+                }
+                double price = double.Parse(comand);
+                if (price < 0)
+                {
+                    Console.WriteLine("Invalid price!");
+                    comand = Console.ReadLine();
+                    continue;
+                }
+                noTaxPrice += price;
+                tax += price * 0.20;
+                comand = Console.ReadLine();
+                if (comand == "special" || comand == "regular")
+                {
+                    break;
+                }
+            }
+            double totalPrice = noTaxPrice + tax;
+
+            if (comand == "special")
+            {
+                if (noTaxPrice > 0)
+                {
+                    totalPrice *= 0.90;
+                }
+                else
+                {
+                    Console.WriteLine("Invalid order!");
+                    Environment.Exit(0);
+                }
+            }
+            else if (comand == "regular")
+            {
+                if (noTaxPrice <= 0)
+                {
+                    Console.WriteLine("Invalid order!");
+                    Environment.Exit(0);
+                }
+            }
+
+            Console.WriteLine("Congratulations you've just bought a new computer!");
+            Console.WriteLine($"Price without taxes: {noTaxPrice:f2}$");
+            Console.WriteLine($"Taxes: {tax:f2}$");
+            Console.WriteLine("-----------");
+            Console.WriteLine($"Total price: {totalPrice:f2}$");
+        }
+    }
+}
