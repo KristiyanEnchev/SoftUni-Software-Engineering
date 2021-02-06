0f8d6eb15e8d875565f3dc8ec223e34752196c85
Kristiyan Enchev <kristiqnenchevv@gmail.com>
2021-02-06 15:20:15 +0200

    New Add

---
+++ Programming Fundamentals/Exams/02. Programming Fundamentals Mid Exam/03. Numbers/03. Numbers/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _03
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> sequance = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToList();
+
+            int avgSequance = sequance.Sum() / sequance.Count;
+
+            List<int> newSequance = new List<int>();
+            for (int i = 0; i < sequance.Count; i++)
+            {
+                if (avgSequance >= 0)
+                {
+                    if (sequance[i] > avgSequance)
+                    {
+                        newSequance.Add(sequance[i]);
+                    }
+                }
+                else
+                {
+                    if (sequance[i] >= avgSequance)
+                    {
+                        newSequance.Add(sequance[i]);
+                    }
+                }
+            }
+            if (newSequance.Count == 0)
+            {
+                Console.WriteLine("No");
+                Environment.Exit(0);
+            }
+            newSequance.Sort();
+            newSequance.Reverse();
+            if (newSequance.Count > 5)
+            {
+                newSequance.RemoveRange(5, newSequance.Count - 5);
+                Console.WriteLine(string.Join(" ", newSequance));
+            }
+            else
+            {
+                Console.WriteLine(string.Join(" ", newSequance));
+            }
+        }
+    }
+}
