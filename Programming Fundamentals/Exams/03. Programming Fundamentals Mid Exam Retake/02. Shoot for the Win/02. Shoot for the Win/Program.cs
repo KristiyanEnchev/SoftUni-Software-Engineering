a593c2787e855d0bd02c2b9da4e707a5ae7f65c9
Kristiyan Enchev <kristiqnenchevv@gmail.com>
2021-02-07 17:01:12 +0200

    New Add

---
+++ Programming Fundamentals/Exams/03. Programming Fundamentals Mid Exam Retake/02. Shoot for the Win/02. Shoot for the Win/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> list = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToList();
+
+            int count = 0;
+            string comand = Console.ReadLine();
+            while (comand != "End")
+            {
+                int index = int.Parse(comand);
+                if (index >= 0 && index <= list.Count - 1)
+                {
+                    int num = list[index];
+                    list[index] = -1;
+                    count++;
+                    for (int i = 0; i < list.Count; i++)
+                    {
+                        if (list[i] > num && list[i] != -1)
+                        {
+                            list[i] -= num;
+                        }
+                        else if (list[i] <= num && list[i] != -1)
+                        {
+                            list[i] += num;
+                        }
+                    }
+                }
+                comand = Console.ReadLine();
+            }
+
+            Console.WriteLine($"Shot targets: {count} -> {string.Join(" ", list)}");
+        }
+    }
+}
