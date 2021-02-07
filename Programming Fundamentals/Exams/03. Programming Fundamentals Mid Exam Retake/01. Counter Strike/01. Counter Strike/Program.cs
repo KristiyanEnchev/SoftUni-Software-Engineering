ccff8e28861697bc046edc8c6fe8c78d62b97e83
Kristiyan Enchev <kristiqnenchevv@gmail.com>
2021-02-07 17:01:04 +0200

    New Add

---
+++ Programming Fundamentals/Exams/03. Programming Fundamentals Mid Exam Retake/01. Counter Strike/01. Counter Strike/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Exam_Prep_04
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = int.Parse(Console.ReadLine());
+
+            int count = 0;
+
+            string comand = Console.ReadLine();
+            while (comand != "End of battle")
+            {
+                int enemy = int.Parse(comand);
+                if (num < enemy)
+                {
+                    Console.WriteLine($"Not enough energy! Game ends with {count} won battles and {num} energy");
+                    Environment.Exit(0);
+                }
+                else
+                {
+                    num -= enemy;
+                    count++;
+                    if (count % 3 == 0)
+                    {
+                        num += count;
+                    }
+                }
+                comand = Console.ReadLine();
+            }
+            Console.WriteLine($"Won battles: {count}. Energy left: {num}");
+        }
+    }
+}
