d631a19604ea430288dbcaac044ddd1f00123326
Kristiyan Enchev <kristiqnenchevv@gmail.com>
2021-02-08 19:33:27 +0200

    New Add

---
+++ Programming Fundamentals/Exams/04. Programming Fundamentals Mid Exam/01. National Court/01. National Court/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Mid_Exam_Exercise_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int capacityOne = int.Parse(Console.ReadLine());
+            int capacityTwo = int.Parse(Console.ReadLine());
+            int capacityThree = int.Parse(Console.ReadLine());
+
+            int studentCount = int.Parse(Console.ReadLine());
+            int totalCapacityForHouur = capacityOne + capacityTwo + capacityThree;
+            int hourCount = 0;
+            int breakTime = 0;
+
+            while (studentCount > 0)
+            {
+                if (hourCount % 3 == 0 && studentCount > 0 && hourCount != 0)
+                {
+                    breakTime++;
+                }
+                hourCount++;
+                studentCount -= totalCapacityForHouur;
+            }
+
+            Console.WriteLine($"Time needed: {hourCount + breakTime}h.");
+        }
+    }
+}
