a0ab04b0705c21389cac6663c374ff9d5a2de174
Kristiyan Enchev <kristiqnenchevv@gmail.com>
2021-02-08 19:33:08 +0200

    New Add

---
+++ Programming Fundamentals/Exams/04. Programming Fundamentals Final Exam/02. Fancy Barcodes/02. Fancy Barcodes/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace _02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string pattern = @"@\#+[A-Z][A-Za-z0-9]{4,}[A-Z]@\#+";
+
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                string input = Console.ReadLine();
+                Match mach = Regex.Match(input, pattern);
+
+                if (mach.Success)
+                {
+                    string compare = mach.Value;
+                    string temp = string.Empty;
+
+                    for (int j = 0; j < compare.Length; j++)
+                    {
+                        if (char.IsDigit(compare[j]))
+                        {
+                            temp += compare[j];
+                        }
+                    }
+
+                    if (temp == "")
+                    {
+                        Console.WriteLine("Product group: 00");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"Product group: {temp}");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Invalid barcode");
+                }
+            }
+        }
+    }
+}
