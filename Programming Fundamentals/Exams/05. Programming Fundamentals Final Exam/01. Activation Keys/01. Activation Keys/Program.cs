f4440606fec892f76d9e78f4416d3972807e69aa
Kristiyan Enchev <kristiqnenchevv@gmail.com>
2021-02-09 19:03:07 +0200

    New Add

---
+++ Programming Fundamentals/Exams/05. Programming Fundamentals Final Exam/01. Activation Keys/01. Activation Keys/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+
+namespace Activation_Key
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string activationKey = Console.ReadLine();
+
+            string comand = Console.ReadLine();
+
+            while (comand != "Generate")
+            {
+                string[] tokens = comand.Split(">>>", StringSplitOptions.RemoveEmptyEntries);
+
+                switch (tokens[0])
+                {
+                    case "Contains":
+                        if (activationKey.IndexOf(tokens[1]) != -1)
+                        {
+                            Console.WriteLine($"{activationKey} contains {tokens[1]}");
+                        }
+                        else
+                        {
+                            Console.WriteLine("Substring not found!");
+                        }
+                        break;
+                    case "Flip":
+                        int startIndex = int.Parse(tokens[2]);
+                        int endIndex = int.Parse(tokens[3]);
+
+                        string firstPart = activationKey.Substring(0, startIndex);
+                        string SecondPart = activationKey.Substring(startIndex, endIndex - startIndex);
+                        string thurdPart = activationKey.Substring(endIndex);
+
+                        if (tokens[1].ToUpper() == "UPPER")
+                        {
+                            SecondPart = SecondPart.ToUpper();
+                        }
+                        else
+                        {
+                            SecondPart = SecondPart.ToLower();
+                        }
+
+                        activationKey = firstPart + SecondPart + thurdPart;
+                        Console.WriteLine(activationKey);
+
+                        break;
+                    case "Slice":
+                        startIndex = int.Parse(tokens[1]);
+                        endIndex = int.Parse(tokens[2]);
+
+                        activationKey = activationKey.Remove(startIndex, endIndex - startIndex);
+                        Console.WriteLine(activationKey);
+
+                        break;
+                }
+
+                comand = Console.ReadLine();
+            }
+
+            Console.WriteLine($"Your activation key is: {activationKey}");
+        }
+    }
+}
