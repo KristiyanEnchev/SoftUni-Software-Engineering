4b608e820494933dae54b94df637bf4145609b3d
Kristiyan Enchev <kristiqnenchevv@gmail.com>
2021-02-09 19:04:04 +0200

    New Add

---
+++ Programming Fundamentals/Exams/05. Programming Fundamentals Mid Exam/03. Inventory/03. Inventory/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _03
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> list = Console.ReadLine()
+                .Split(", ", StringSplitOptions.RemoveEmptyEntries)
+                .ToList();
+
+
+            string comand = Console.ReadLine();
+            while (comand != "Craft!")
+            {
+                string[] input = comand.Split(" - ", StringSplitOptions.RemoveEmptyEntries);
+                string inputComand = input[0];
+                if (inputComand == "Collect")
+                {
+                    string item = input[1];
+                    if (!list.Contains(item))
+                    {
+                        list.Add(item);
+                    }
+                }
+                else if (inputComand == "Drop")
+                {
+                    string item = input[1];
+                    if (list.Contains(item))
+                    {
+                        list.Remove(item);
+                    }
+                }
+                else if (inputComand == "Renew")
+                {
+                    string item = input[1];
+                    if (list.Contains(item))
+                    {
+                        list.Remove(item);
+                        list.Add(item);
+                    }
+                }
+                else if (inputComand == "Combine Items")
+                {
+                    string[] items = input[1].Split(":");
+                    string olsdItem = items[0];
+                    string newItem = items[1];
+                    if (list.Contains(olsdItem))
+                    {
+                        int index = list.IndexOf(olsdItem);
+                        list.Insert(index + 1, newItem);
+                    }
+                }
+
+                comand = Console.ReadLine();
+            }
+
+            Console.WriteLine(string.Join(", ", list));
+        }
+    }
+}
