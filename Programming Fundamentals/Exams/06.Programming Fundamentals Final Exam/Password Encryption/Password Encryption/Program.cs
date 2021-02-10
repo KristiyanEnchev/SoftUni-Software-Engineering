9aff16d090c2413cd4d5129791246acc0c805f78
Kristiyan Enchev <kristiqnenchevv@gmail.com>
2021-02-10 20:33:57 +0200

    New Add

---
+++ Programming Fundamentals/Exams/06.Programming Fundamentals Final Exam/Password Encryption/Password Encryption/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Text.RegularExpressions;
+using System.Linq;
+
+namespace Exam_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int count = int.Parse(Console.ReadLine());
+
+            string pattern = @"(.*)([\>]{1})(?<password>(\d{3})\|([a-z]{3})\|([A-Z]{3})\|([^<>]+))([<])\1$";
+
+
+            for (int i = 0; i < count; i++)
+            {
+                string input = Console.ReadLine();
+                Regex regex = new Regex(pattern);
+                Match mach = regex.Match(input);
+
+                if (mach.Success)
+                {
+                    string password = mach.Groups["password"].Value.Replace("|", "");
+
+                    Console.WriteLine($"Password: {password}");
+                }
+                else
+                {
+                    Console.WriteLine("Try another password!");
+                }
+
+            }
+
+        }
+    }
+}
