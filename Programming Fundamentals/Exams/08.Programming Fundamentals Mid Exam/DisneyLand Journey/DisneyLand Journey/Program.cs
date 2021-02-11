730b8275a9afb6a584031baf3c63d21f84238ebb
Kristiyan Enchev <kristiqnenchevv@gmail.com>
2021-02-11 23:04:25 +0200

    New Add

---
+++ Programming Fundamentals/Exams/08.Programming Fundamentals Mid Exam/DisneyLand Journey/DisneyLand Journey/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Mid_ExamPrep
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double cost = double.Parse(Console.ReadLine());
+            int mounthsCount = int.Parse(Console.ReadLine());
+
+            double currentMoney = 0;
+
+            for (int i = 1; i <= mounthsCount; i++)
+            {
+                if (i % 2 != 0 && i != 1)
+                {
+                    currentMoney *= 0.84;
+                }
+                if (i % 4 == 0)
+                {
+                    currentMoney += currentMoney * 0.25;
+                }
+                currentMoney += cost * 0.25;
+            }
+            if (currentMoney >= cost)
+            {
+                Console.WriteLine($"Bravo! You can go to Disneyland and you will have {currentMoney - cost:f2}lv. for souvenirs.");
+            }
+            else
+            {
+                Console.WriteLine($"Sorry. You need {cost - currentMoney:f2}lv. more.");
+            }
+        }
+    }
+}
