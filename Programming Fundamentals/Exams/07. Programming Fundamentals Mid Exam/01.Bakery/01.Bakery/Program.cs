dd4e59cec5aa25bd4083e1b1d976340b4ea74dc8
Kristiyan Enchev <kristiqnenchevv@gmail.com>
2021-02-11 23:03:58 +0200

    New Add

---
+++ Programming Fundamentals/Exams/07. Programming Fundamentals Mid Exam/01.Bakery/01.Bakery/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace _02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int biscuitsPerPerson = int.Parse(Console.ReadLine());
+            int workers = int.Parse(Console.ReadLine());
+            int competition = int.Parse(Console.ReadLine());
+
+            int totalBiscuitsAday = biscuitsPerPerson * workers;
+            double totalEveryThurdDay = Math.Floor(totalBiscuitsAday * 0.75);
+
+            double total = 0;
+
+            for (int i = 1; i <= 30; i++)
+            {
+                if (i % 3 == 0)
+                {
+                    total += totalEveryThurdDay;
+                }
+                else
+                {
+                    total += totalBiscuitsAday;
+                }
+            }
+
+            Console.WriteLine($"You have produced {total} biscuits for the past month.");
+            if (total > competition)
+            {
+                double more = total - competition;
+                double percentage = (more / competition) * 100;
+                Console.WriteLine($"You produce {percentage:f2} percent more biscuits.");
+            }
+            else
+            {
+                double more = competition - total;
+                double percentage = (more / competition) * 100;
+                Console.WriteLine($"You produce {percentage:f2} percent less biscuits.");
+            }
+        }
+    }
+}
