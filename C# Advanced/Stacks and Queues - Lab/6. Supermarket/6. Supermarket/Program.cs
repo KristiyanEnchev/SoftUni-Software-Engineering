dd84a7b300a80f6281261b9da8db8b96bc9c6e2b
Kristiyan Enchev <kristiqnenchevv@gmail.com>
2021-02-12 20:25:04 +0200

    New Add

---
+++ C# Advanced/Stacks and Queues - Lab/6. Supermarket/6. Supermarket/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace TrafficJam
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var queu = new Queue<string>();
+            string input = null;
+
+            while ((input = Console.ReadLine()) != "End")
+            {
+                if (input == "Paid")
+                {
+                    while (queu.Any())
+                    {
+                        Console.WriteLine(queu.Dequeue());
+                    }
+                }
+                else
+                {
+                    queu.Enqueue(input);
+                }
+            }
+
+            Console.WriteLine($"{queu.Count} people remaining.");
+        }
+    }
+}
