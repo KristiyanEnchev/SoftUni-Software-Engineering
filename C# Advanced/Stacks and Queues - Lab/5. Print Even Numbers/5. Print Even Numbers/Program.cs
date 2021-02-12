6341156976f985572c1eb84f1732f99d7a0bb235
Kristiyan Enchev <kristiqnenchevv@gmail.com>
2021-02-12 20:24:45 +0200

    New Add

---
+++ C# Advanced/Stacks and Queues - Lab/5. Print Even Numbers/5. Print Even Numbers/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _7._Hot_Potato
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] children = Console.ReadLine().Split();
+            int n = int.Parse(Console.ReadLine());
+            Queue<string> potatoQueue = new Queue<string>(children);
+            int potatoTosses = 0;
+
+            while (potatoQueue.Count > 1)
+            {
+                potatoTosses++;
+                string kid = potatoQueue.Dequeue();
+                if (potatoTosses == n)
+                {
+                    potatoTosses = 0;
+                    Console.WriteLine("Removed " + kid);
+                }
+                else
+                {
+                    potatoQueue.Enqueue(kid);
+                }
+            }
+
+            Console.WriteLine("Last is " + potatoQueue.Dequeue());
+        }
+    }
+}
