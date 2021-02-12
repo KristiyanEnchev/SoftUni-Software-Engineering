22813b91384218a136b7c86579cb57cd95f717e1
Kristiyan Enchev <kristiqnenchevv@gmail.com>
2021-02-12 20:24:20 +0200

    New Add

---
+++ C# Advanced/Stacks and Queues - Lab/3. Simple Calculator/3. Simple Calculator/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace _3._Simple_Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] sentence = Console.ReadLine().Split();
+
+            Stack<string> stack = new Stack<string>(sentence.Reverse());
+
+            while (stack.Count > 1)
+            {
+                int leftNum = int.Parse(stack.Pop());
+                string operatorSign = stack.Pop(); 
+                int rightNum = int.Parse(stack.Pop());
+
+                if (operatorSign == "+")
+                {
+                    stack.Push((leftNum + rightNum).ToString());
+                }
+                else if (operatorSign == "-")
+                {
+                    stack.Push((leftNum - rightNum).ToString());
+                }
+            }
+
+            Console.WriteLine(stack.Pop());
+        }
+    }
+}
