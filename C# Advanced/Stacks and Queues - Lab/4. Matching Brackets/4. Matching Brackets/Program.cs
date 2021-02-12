4f1442147ac534de14a14c422808c18c8e9e37f5
Kristiyan Enchev <kristiqnenchevv@gmail.com>
2021-02-12 20:24:29 +0200

    New Add

---
+++ C# Advanced/Stacks and Queues - Lab/4. Matching Brackets/4. Matching Brackets/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace _4._Matching_Brackets
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = Console.ReadLine();
+            Stack<int> bracketsIndexes = new Stack<int>();
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                if (input[i] == '(')
+                {
+                    bracketsIndexes.Push(i);
+                }
+
+                if (input[i] == ')')
+                {
+                    int startIndex = bracketsIndexes.Pop();
+                    Console.WriteLine(input.Substring(startIndex, i - startIndex + 1));
+                }
+            }
+        }
+    }
+}
