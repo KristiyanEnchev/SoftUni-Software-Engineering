a6ce20d8e33aa5b64b0b8c5c6d796e7cf3e2b606
Kristiyan Enchev <kristiqnenchevv@gmail.com>
2021-02-13 19:06:38 +0200

    New Add

---
+++ C# Advanced/Stacks and Queues - Exercise/05. Fashion Boutique/05. Fashion Boutique/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _05._Fashion_Boutique
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] allClothesInput = Console.ReadLine().Split(" ").Select(int.Parse).ToArray();
+            Stack<int> allClothes = new Stack<int>(allClothesInput);
+
+            int boxCapacity = int.Parse(Console.ReadLine());
+            int curentBoxCap = boxCapacity;
+            int racksCount = 1;
+
+            while (allClothes.Any())
+            {
+                int clothes = allClothes.Pop();
+
+                curentBoxCap -= clothes;
+
+                if (curentBoxCap < 0)
+                {
+                    racksCount++;
+                    curentBoxCap = boxCapacity;
+                    curentBoxCap -= clothes;
+                }
+            }
+
+            Console.WriteLine(racksCount);
+        }
+    }
+}
