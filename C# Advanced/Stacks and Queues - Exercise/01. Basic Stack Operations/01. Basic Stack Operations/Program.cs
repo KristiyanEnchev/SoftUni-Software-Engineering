3f3b2e5a0f714ad1eb90f43b75925487dd3524ef
Kristiyan Enchev <kristiqnenchevv@gmail.com>
2021-02-13 19:06:00 +0200

    New Add

---
+++ C# Advanced/Stacks and Queues - Exercise/01. Basic Stack Operations/01. Basic Stack Operations/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _01._Basic_Stack_Operations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] data = Console.ReadLine().Split(" ").Select(int.Parse).ToArray();
+
+            int n = data[0];
+            int s = data[1];
+            int x = data[2];
+
+            int[] nubersInput = Console.ReadLine().Split(" ").Select(int.Parse).ToArray();
+            Stack<int> number = new Stack<int>();
+
+            for (int i = 0; i < n; i++)
+            {
+                number.Push(nubersInput[i]);
+            }
+
+            for (int i = 0; i < s; i++)
+            {
+                number.Pop();
+            }
+
+            bool isFound = number.Contains(x);
+
+            if (isFound)
+            {
+                Console.WriteLine("true");
+            }
+            else if (!number.Any())
+            {
+                Console.WriteLine(0);
+            }
+            else
+            {
+                Console.WriteLine(number.Min());
+            }
+        }
+    }
+}
