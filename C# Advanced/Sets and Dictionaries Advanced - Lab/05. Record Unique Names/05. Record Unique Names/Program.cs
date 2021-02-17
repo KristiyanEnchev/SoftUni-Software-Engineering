87951e982629144cd35b9bd87093452c28a69f9a
Kristiyan Enchev <kristiqnenchevv@gmail.com>
2021-02-17 11:29:01 +0200

    New Add

---
+++ C# Advanced/Sets and Dictionaries Advanced - Lab/05. Record Unique Names/05. Record Unique Names/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _05._Record_Unique_Names
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HashSet<string> names = new HashSet<string>();
+
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                names.Add(Console.ReadLine());
+            }
+
+            foreach (var item in names)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
