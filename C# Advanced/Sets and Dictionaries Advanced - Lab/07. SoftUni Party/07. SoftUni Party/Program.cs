5eb9a2152520918a9f5a4b279091e9f07ae53237
Kristiyan Enchev <kristiqnenchevv@gmail.com>
2021-02-17 11:29:18 +0200

    New Add

---
+++ C# Advanced/Sets and Dictionaries Advanced - Lab/07. SoftUni Party/07. SoftUni Party/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _07._SoftUni_Party
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HashSet<string> Vip = new HashSet<string>();
+            HashSet<string> Regular = new HashSet<string>();
+            string number = string.Empty;
+            while ((number = Console.ReadLine()) != "PARTY")
+            {
+                if (char.IsDigit(number[0]))
+                {
+                    Vip.Add(number);
+                }
+                else
+                {
+                    Regular.Add(number);
+                }
+            }
+            string commingPeople = string.Empty;
+            while ((commingPeople = Console.ReadLine()) != "END")
+            {
+                if (Vip.Contains(commingPeople))
+                {
+                    Vip.Remove(commingPeople);
+                    continue;
+                }
+                Regular.Remove(commingPeople);
+            }
+            Console.WriteLine(Vip.Count + Regular.Count);
+
+            foreach (var num in Vip)
+            {
+                Console.WriteLine(num);
+            }
+            foreach (var num in Regular)
+            {
+                Console.WriteLine(num);
+            }
+        }
+    }
+}
