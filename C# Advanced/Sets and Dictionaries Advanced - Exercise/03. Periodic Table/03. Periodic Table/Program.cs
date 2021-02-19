9913a7c75c5f2b59dc71a32b49cbe62432199071
Kristiyan Enchev <kristiqnenchevv@gmail.com>
2021-02-19 18:02:38 +0200

    New Add

---
+++ C# Advanced/Sets and Dictionaries Advanced - Exercise/03. Periodic Table/03. Periodic Table/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace _03._Periodic_Table
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            SortedSet<string> compounds = new SortedSet<string>();
+
+            for (int i = 0; i < n; i++)
+            {
+                string[] input = Console.ReadLine().Split();
+
+                for (int j = 0; j < input.Length; j++)
+                {
+                    compounds.Add(input[j]);
+                }
+            }
+
+            Console.WriteLine(string.Join(" ", compounds));
+        }
+    }
+}
