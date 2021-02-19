63025b4691020664eb90461ed4b0d87f8e856c12
Kristiyan Enchev <kristiqnenchevv@gmail.com>
2021-02-19 18:02:16 +0200

    New Add

---
+++ C# Advanced/Sets and Dictionaries Advanced - Exercise/01. Unique Usernames/01. Unique Usernames/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _01._Unique_Usernames
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            SortedSet<string> hashset = new SortedSet<string>();
+            
+            for (int i = 0; i < n; i++)
+            {
+                hashset.Add(Console.ReadLine());
+            }
+
+            Console.WriteLine(string.Join(" ", hashset));
+        }
+    }
+}
