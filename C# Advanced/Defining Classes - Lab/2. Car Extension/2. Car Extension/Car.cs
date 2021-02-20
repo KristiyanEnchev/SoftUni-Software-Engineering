8921259ddffabfe77301e5db5944be233ed8ec7f
Kristiyan Enchev <kristiqnenchevv@gmail.com>
2021-02-20 18:18:15 +0200

    New Add

---
+++ C# Advanced/Defining Classes - Lab/2. Car Extension/2. Car Extension/Car.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CarManufacturer
+{
+    class Car
+    {
+        public string Make { get; set; }
+        public string Model { get; set; }
+        public int Year { get; set; }
+        public double FuelConsumption { get; set; }
+        public double FuelQuantity { get; set; }
+
+        public void Drive(double distance)
+        {
+            double fuelToConsume = distance * FuelConsumption;
+            if (FuelQuantity - fuelToConsume >= 0)
+            {
+                FuelQuantity -= fuelToConsume;
+            }
+            else
+            {
+                Console.WriteLine($"Not enough fuel to perform this trip!");
+            }
+        }
+
+        public string WhoAmI()
+        {
+            return $"Make: {this.Make}\nModel: {this.Model}\nYear: {this.Year}\nFuel: {this.FuelQuantity:F2}L";
+        }
+    }
+}
