03f2d8985b5e77cb011db3591ff2b34b786412ba
Kristiyan Enchev <kristiqnenchevv@gmail.com>
2021-02-20 18:18:24 +0200

    New Add

---
+++ C# Advanced/Defining Classes - Lab/3. Car Constructors/3. Car Constructors/Car.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CarManufacturer
+{
+    class Car
+    {
+        public Car()
+        {
+            this.Make = "VW";
+            this.Model = "Golf";
+            this.Year = 2025;
+            this.FuelQuantity = 200;
+            this.FuelConsumption = 10;
+        }
+
+        public Car(string make, string model, int year)
+            : this()
+        {
+            Make = make;
+            Model = model;
+            Year = year;
+        }
+
+        public Car(string make, string model, int year, double fuelQuantity, double fuelConsumption)
+            : this(make, model , year)
+        {
+            FuelQuantity = fuelQuantity;
+            FuelConsumption = fuelConsumption;
+        }
+
+        public string Make { get; set; }
+        public string Model { get; set; }
+        public int Year { get; set; }
+        public double FuelConsumption { get; set; }
+        public double FuelQuantity { get; set; }
+
+        public void Drive(double distance)
+        {
+            double fuelToConsume = distance * FuelConsumption;
+            if (FuelQuantity - fuelToConsume >= 0)
+            {
+                FuelQuantity -= fuelToConsume;
+            }
+            else
+            {
+                Console.WriteLine($"Not enough fuel to perform this trip!");
+            }
+        }
+
+        public string WhoAmI()
+        {
+            return $"Make: {this.Make}\nModel: {this.Model}\nYear: {this.Year}\nFuel: {this.FuelQuantity:F2}L";
+        }
+    }
+}
---
+++ C# Advanced/Defining Classes - Lab/3. Car Constructors/3. Car Constructors/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace CarManufacturer
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            Car bmw = new Car("BMW", "X6", 1993, 5003 ,- 50);
+            Car defaultGolf = new Car();
+            Console.WriteLine($"Default golf: " + defaultGolf.WhoAmI());
+
+            Car car = new Car();
+            car.Make = "VW";
+            car.Model = "MK3";
+            car.Year = 1992;
+            car.FuelQuantity = 200;
+            car.FuelConsumption = 200;
+
+            car.Drive(0.5);
+
+            Console.WriteLine(car.WhoAmI());
+        }
+    }
+}
