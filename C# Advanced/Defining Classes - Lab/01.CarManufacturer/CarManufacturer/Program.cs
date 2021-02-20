50be4aa06a240e2fe551a3b9abb3b12d62714b53
Kristiyan Enchev <kristiqnenchevv@gmail.com>
2021-02-20 18:18:07 +0200

    New Add

---
+++ C# Advanced/Defining Classes - Lab/01.CarManufacturer/CarManufacturer/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace CarManufacturer
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            Car car = new Car();
+            car.Make = "VW";
+            car.Model = "MK3";
+            car.Year = 1992;
+
+            Console.WriteLine($"Make: {car.Make} Model: {car.Make} Year: {car.Year}");
+        }
+    }
+}
