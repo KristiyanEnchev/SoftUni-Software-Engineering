3e15340a56be971bafb844c1d8b3fbc67724946b
Kristiyan Enchev <kristiqnenchevv@gmail.com>
2021-02-21 23:18:50 +0200

    New Add

---
+++ C# Advanced/Defining Classes - Exercices/P07.RawData/P07.RawData/Cargo.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RawData
+{
+    public class Cargo
+    {
+
+        private int cargoWeight;
+
+        private string cargoType;
+
+        public Cargo(int cargoWeight, string cargoType)
+        {
+            CargoWeight = cargoWeight;
+            CargoType = cargoType;
+        }
+
+        public int CargoWeight
+        {
+            get { return cargoWeight; }
+            set { cargoWeight = value; }
+        }
+
+        public string CargoType
+        {
+            get { return cargoType; }
+            set { cargoType = value; }
+        }
+    }
+}
---
+++ C# Advanced/Defining Classes - Exercices/P07.RawData/P07.RawData/Tire.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RawData
+{
+    public class Tire
+    {
+        private int year;
+
+        private double pressure;
+
+        public Tire(double pressure, int year)
+        {
+            Year = year;
+            Pressure = pressure;
+        }
+        public int Year
+        {
+            get { return year; }
+            set { year = value; }
+        }
+
+        public double Pressure
+        {
+            get { return pressure; }
+            set { pressure = value; }
+        }
+    }
+}
