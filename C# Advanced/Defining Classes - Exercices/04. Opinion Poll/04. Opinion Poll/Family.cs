7c3b3726ad44b156ca25d7254bf961ee1ebccad8
Kristiyan Enchev <kristiqnenchevv@gmail.com>
2021-02-21 23:18:21 +0200

    New Add

---
+++ C# Advanced/Defining Classes - Exercices/04. Opinion Poll/04. Opinion Poll/Family.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+public class Family
+{
+    public List<Person> Members { get; set; }
+
+
+    public void AddMember(Person person)
+    {
+        this.Members.Add(person);
+    }
+    public Family()
+    {
+        this.Members = new List<Person>();
+    }
+
+    public Person GetOldestMember()
+    {
+        Person person = this.Members.OrderByDescending(p => p.Age).FirstOrDefault();
+        return person;
+    }
+}
---
+++ C# Advanced/Defining Classes - Exercices/04. Opinion Poll/04. Opinion Poll/StartUp.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace DefiningClasses
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            List<Person> people = new List<Person>();
+
+            for (int i = 0; i < n; i++)
+            {
+                string[] personData = Console.ReadLine().Split(" ");
+                string name = personData[0];
+                int age = int.Parse(personData[1]);
+
+                Person currentperson = new Person(name, age);
+                people.Add(currentperson);
+            }
+
+            List<Person> filtered = people.Where(p => p.Age > 30).OrderBy(p => p.Name).ToList();
+
+            foreach (Person person in filtered)
+            {
+                Console.WriteLine($"{person.Name} - {person.Age}");
+            }
+        }
+    }
+}
