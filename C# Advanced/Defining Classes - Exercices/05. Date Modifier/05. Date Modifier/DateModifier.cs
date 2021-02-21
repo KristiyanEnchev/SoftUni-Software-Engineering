9015d14c5b7e5c5c876233ed5013340f56b2cb30
Kristiyan Enchev <kristiqnenchevv@gmail.com>
2021-02-21 23:18:29 +0200

    New Add

---
+++ C# Advanced/Defining Classes - Exercices/05. Date Modifier/05. Date Modifier/DateModifier.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DateModifier
+{
+    public class DateModifier
+    {
+        public static int GetDiffBetweenDateInDays(string dateOneStr, string dateTwoStr)
+        {
+            DateTime dataOne = DateTime.Parse(dateOneStr);
+            DateTime dataTwo = DateTime.Parse(dateTwoStr);
+
+            TimeSpan diff = dataOne - dataTwo;
+
+            return Math.Abs( diff.Days);
+        }
+    }
+}
