337f597e89b431db6ab08eab17c4d9920a2963d5
Kristiyan Enchev <kristiqnenchevv@gmail.com>
2021-02-21 23:17:51 +0200

    New Add

---
+++ C# Advanced/Defining Classes - Exercices/01. Define a Class Person/01. Define a Class Person/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace DefiningClasses
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            Person personeOne = new Person();
+            personeOne.Name = "Peter";
+            personeOne.Age = 20;
+
+            Person personTwo = new Person()
+            {
+                Name = "Kris",
+                Age = 22
+            };
+        }
+    }
+}
