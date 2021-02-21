b647c5f72db60f13311e274aef8f4b90c0007e89
Kristiyan Enchev <kristiqnenchevv@gmail.com>
2021-02-21 23:18:01 +0200

    New Add

---
+++ C# Advanced/Defining Classes - Exercices/02. Creating Constructors/02. Creating Constructors/Person.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DefiningClasses
+{
+    public class Person
+    {
+        private string name;
+        private int age;
+
+        public Person()
+        {
+            this.name = "No name";
+            this.age = 1;
+        }
+
+        public Person(int age) : this()
+        {
+            this.Age = age;
+        }
+
+        public Person(string name, int age)
+        {
+            this.Name = name;
+            this.Age = age;
+        }
+
+        public string Name
+        {
+            get => this.name;
+            set => this.name = value;
+        }
+        public int Age
+        {
+            get => this.age;
+            set => this.age = value;
+        }
+
+    }
+}
---
+++ C# Advanced/Defining Classes - Exercices/02. Creating Constructors/02. Creating Constructors/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace DefiningClasses
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            Person personeOne = new Person();
+            Person personeTwo = new Person(25);
+            Person personeTree = new Person("Peter", 24);
+
+        }
+    }
+}
