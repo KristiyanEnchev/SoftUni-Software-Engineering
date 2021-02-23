0356a79b079908efb79a1ee5f6eaa725a397e692
Kristiyan Enchev <kristiqnenchevv@gmail.com>
2021-02-23 23:03:25 +0200

    New Add

---
+++ C# Advanced/Functional Programming - Exercices/04. Find Evens or Odds/04. Find Evens or Odds/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _04._Find_Evens_or_Odds
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numer = Console.ReadLine()
+                          .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                          .Select(int.Parse)
+                          .ToArray();
+
+            string evenOrOdd = Console.ReadLine();
+
+            Predicate<int> checEven = num => num % 2 == 0;
+            Predicate<int> checOdd = num => num % 2 != 0;
+
+            List<int> num = new List<int>();
+
+            for (int i = numer[0]; i <= numer[1]; i++)
+            {
+                if (evenOrOdd == "even" && checEven(i))
+                {
+                    num.Add(i);
+                }
+                else if (evenOrOdd == "odd" && checOdd(i))
+                {
+                    num.Add(i);
+                }
+            }
+
+            Console.WriteLine(string.Join(" ", num));
+        }
+    }
+}
