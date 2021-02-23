14fe00dd4c65aabeae08faec0b85c0bc5c1275d3
Kristiyan Enchev <kristiqnenchevv@gmail.com>
2021-02-23 23:03:33 +0200

    New Add

---
+++ C# Advanced/Functional Programming - Exercices/05. Applied Arithmetics/05. Applied Arithmetics/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _05._Applied_Arithmetics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = Console.ReadLine()
+                          .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                          .Select(int.Parse)
+                          .ToArray();
+
+            string command = string.Empty;
+
+            Action<int[]> printing = num => Console.WriteLine(string.Join(" ", numbers));
+
+            Func<int[], string, int[]> manipulator = (numbers, command) =>
+            {
+
+                switch (command)
+                {
+                    case "add":
+                        return numbers.Select(x => x + 1).ToArray();
+
+                    case "multiply":
+                        return numbers.Select(x => x * 2).ToArray();
+
+                    case "subtract":
+                        return numbers.Select(x => x - 1).ToArray();
+
+                }
+                return numbers.Select(x => x).ToArray();
+
+            };
+
+            while ((command = Console.ReadLine()) != "end")
+            {
+                if (command == "print")
+                {
+                    printing(numbers);
+                    continue;
+                }
+
+                numbers = manipulator(numbers, command);
+            }
+        }
+    }
+}
