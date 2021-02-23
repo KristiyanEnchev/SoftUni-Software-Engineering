47a0ca296f28b04fce6d2d2c8ee2152f8a54d2f1
Kristiyan Enchev <kristiqnenchevv@gmail.com>
2021-02-23 23:03:09 +0200

    New Add

---
+++ C# Advanced/Functional Programming - Exercices/02. Knights of Honor/02. Knights of Honor/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _02._Knights_of_Honor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> names = Console.ReadLine()
+                .Split(" ")
+                .ToList();
+
+            List<string> newNAmes = names.Select(name => $"Sir {name}").ToList();
+
+            Action<List<string>> printName = n => Console.WriteLine(string.Join(Environment.NewLine, n));
+
+            printName(newNAmes);
+        }
+    }
+}
