9ba80a2311e7fbfd920c42fdadf40af93ab89379
Kristiyan Enchev <kristiqnenchevv@gmail.com>
2021-02-23 23:03:01 +0200

    New Add

---
+++ C# Advanced/Functional Programming - Exercices/01. Action Print/01. Action Print/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _01._Action_Print
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Action<string> print = x => Console.WriteLine($"{string.Join(Environment.NewLine, x)}");
+
+            Console.ReadLine()
+                        .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                        .ToList()
+                        .ForEach(print);
+        }
+    }
+}
