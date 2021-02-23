8f44e5af726cbbd7adc6694ba795017d3942fb33
Kristiyan Enchev <kristiqnenchevv@gmail.com>
2021-02-23 23:03:16 +0200

    New Add

---
+++ C# Advanced/Functional Programming - Exercices/03. Custom Min Function/03. Custom Min Function/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _03._Custom_Min_Function
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Func<List<int>, int> minNumber = numbers => 
+            {
+                int minNum = int.MaxValue;
+
+                foreach (var num in numbers)
+                {
+                    if (num < minNum)
+                    {
+                        minNum = num;
+                    }
+                }
+
+                return minNum;
+            };
+
+            List<int> numbers = Console.ReadLine()
+                .Split(" ")
+                .Select(int.Parse)
+                .ToList();
+
+            int numNumber = minNumber(numbers);
+            Console.WriteLine(numNumber);
+
+        }
+    }
+}
