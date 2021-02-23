3375c432179d782214a3d9d8c79c46e92e901d92
Kristiyan Enchev <kristiqnenchevv@gmail.com>
2021-02-23 23:04:08 +0200

    New Add

---
+++ C# Advanced/Functional Programming - Exercices/09. List Of Predicates/09. List Of Predicates/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _09._List_Of_Predicates
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int end = int.Parse(Console.ReadLine());
+            List<int> divaders = Console.ReadLine().Split(" ").Select(int.Parse).ToList();
+            List<int> numbers = Enumerable.Range(1, end).ToList();
+
+            foreach (var num in numbers)
+            {
+                if (divaders.All(d => num % d == 0))
+                {
+                    Console.Write(num + " ");
+                }
+            }
+
+        }
+    }
+}
