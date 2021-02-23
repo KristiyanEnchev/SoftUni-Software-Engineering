0659780090bf783ec96b28617b7e6f7a65e7f7f2
Kristiyan Enchev <kristiqnenchevv@gmail.com>
2021-02-23 23:03:40 +0200

    New Add

---
+++ C# Advanced/Functional Programming - Exercices/06. Reverse And Exclude/06. Reverse And Exclude/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _06._Reverse_And_Exclude
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] number = Console.ReadLine()
+                                .Split()
+                                .Select(int.Parse)
+                                .Reverse()
+                                .ToArray();
+
+
+            int num = int.Parse(Console.ReadLine());
+
+            List<int> listOfNumber = new List<int>();
+
+            Predicate<int> remove = n => n % num == 0;
+
+            foreach (var n in number)
+            {
+                if (!remove(n))
+                {
+                    Console.Write($"{n} ");
+                }
+            }
+        }
+    }
+}
