eed97cd1e0684ab3d7dce98cced22d767c4ca085
Kristiyan Enchev <kristiqnenchevv@gmail.com>
2021-02-24 23:49:00 +0200

    New Add

---
+++ C# Advanced/Functional Programming - Lab/04. Add VAT/04. Add VAT/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Linq;
+
+namespace _04._Add_VAT
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            decimal[] numbers = Console.ReadLine()
+                .Split(", ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(decimal.Parse)
+                .Select(number => number + (number * 0.2m))
+                .ToArray();
+
+            foreach (var number in numbers)
+            {
+                Console.WriteLine($"{number:f2}");
+            }
+        }
+    }
+}
