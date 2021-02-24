8d061986e5164af484c303bd20c8442b6f3582b9
Kristiyan Enchev <kristiqnenchevv@gmail.com>
2021-02-24 23:48:23 +0200

    New Add

---
+++ C# Advanced/Functional Programming - Lab/01. Sort Even Numbers/01. Sort Even Numbers/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Linq;
+
+namespace _01._Sort_Even_Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = Console.ReadLine()
+                .Split(", ")
+                .Select(int.Parse)
+                .Where(x => x % 2 == 0)
+                .OrderBy(x => x)
+                .ToArray();
+
+            Console.WriteLine(string.Join(", ", array));
+        }
+    }
+}
