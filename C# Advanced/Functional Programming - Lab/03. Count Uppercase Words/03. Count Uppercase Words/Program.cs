957284860098713f869e9b52b7f960cecfa3fa08
Kristiyan Enchev <kristiqnenchevv@gmail.com>
2021-02-24 23:48:41 +0200

    New Add

---
+++ C# Advanced/Functional Programming - Lab/03. Count Uppercase Words/03. Count Uppercase Words/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace _03._Count_Uppercase_Words
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] array = Console.ReadLine()
+                .Split(" ");
+
+            Returnn(array);
+
+        }
+
+        public static void Returnn(string[] arr)
+        {
+            foreach (var item in arr)
+            {
+                if (item.Any(char.IsUpper))
+                {
+                    Console.WriteLine(item);
+                }
+            }
+
+        }
+    }
+}
