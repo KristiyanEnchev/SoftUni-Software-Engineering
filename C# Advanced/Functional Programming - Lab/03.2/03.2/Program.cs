0b06ae4d819c511d57706e029597449a708c4fed
Kristiyan Enchev <kristiqnenchevv@gmail.com>
2021-02-24 23:48:52 +0200

    New Add

---
+++ C# Advanced/Functional Programming - Lab/03.2/03.2/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Linq;
+
+namespace _03._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Func<string, bool> filter = text => Char.IsUpper(text[0]);
+
+            string text = Console.ReadLine();
+            string[] words = text.Split(" ",StringSplitOptions.RemoveEmptyEntries);
+
+            words = words.Where(filter).ToArray();
+
+            foreach (var item in words)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
