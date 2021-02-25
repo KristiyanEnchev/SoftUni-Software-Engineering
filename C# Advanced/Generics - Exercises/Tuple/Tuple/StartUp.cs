3871db44f2b3defe0e742757b71d504de6ee71cc
Kristiyan Enchev <kristiqnenchevv@gmail.com>
2021-02-25 22:39:56 +0200

    New Add

---
+++ C# Advanced/Generics - Exercises/Tuple/Tuple/StartUp.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Tuple
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            string[] firstTupleData = Console.ReadLine().Split(" ");
+            string fullName = $"{firstTupleData[0]} {firstTupleData[1]}";
+
+            MyTuple<string, string> tuple = new MyTuple<string, string>(fullName, firstTupleData[2]);
+
+            string[] secondTupleData = Console.ReadLine().Split(" ");
+
+            MyTuple<string, int> secondTuple = new MyTuple<string, int>(secondTupleData[0], int.Parse(secondTupleData[1]));
+
+            string[] thirdTupleData = Console.ReadLine().Split(" ");
+
+            MyTuple<int, double> thirdTuple = new MyTuple<int, double>(int.Parse(thirdTupleData[0]), double.Parse(thirdTupleData[1]));
+
+            Console.WriteLine(tuple);
+            Console.WriteLine(secondTuple);
+            Console.WriteLine(thirdTuple);
+
+        }
+    }
+}
