bc0023b621cd35a42b0c0be2f8c34fbe3d18b698
Kristiyan Enchev <kristiqnenchevv@gmail.com>
2021-02-25 22:39:37 +0200

    New Add

---
+++ C# Advanced/Generics - Exercises/GenericSwapMethodString/GenericSwapMethodString/StartUp.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+namespace GenericSwapMethodString
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            List<Box<string>> boxes = new List<Box<string>>();
+
+            for (int i = 0; i < n; i++)
+            {
+                Box<string> box = new Box<string>(Console.ReadLine());
+                boxes.Add(box);
+            }
+
+            int[] indexes = Console.ReadLine().Split(" ").Select(int.Parse).ToArray();
+            SwapIndexes(boxes, indexes[0], indexes[1]);
+
+            foreach (var curentBox in boxes)
+            {
+                Console.WriteLine(curentBox);
+            }
+        }
+
+        private static void SwapIndexes<T>(List<Box<T>> boxes, int firtsindex, int secondIndex)
+        {
+            Box<T> temp = boxes[firtsindex];
+            boxes[firtsindex] = boxes[secondIndex];
+            boxes[secondIndex] = temp;
+        }
+    }
+}
