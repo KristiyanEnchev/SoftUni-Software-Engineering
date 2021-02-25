e0cf2ed96451a4c2159d2be032fd97c8f8e62c00
Kristiyan Enchev <kristiqnenchevv@gmail.com>
2021-02-25 22:39:20 +0200

    New Add

---
+++ C# Advanced/Generics - Exercises/GenericCountMethodStrin/GenericCountMethodStrin/Box.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GenericCountMethodString
+{
+    public class Box<T>
+        where T : IComparable
+    {
+        public Box(T value)
+        {
+            this.Value = value;
+        }
+
+        public T Value { get; private set; }
+
+        public override string ToString()
+        {
+            Type valueType = this.Value.GetType();
+            string valueTypeFullName = valueType.FullName;
+
+            return $"{valueTypeFullName}: {this.Value}";
+        }
+    }
+}
---
+++ C# Advanced/Generics - Exercises/GenericCountMethodStrin/GenericCountMethodStrin/StartUp.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace GenericCountMethodString
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            List<Box<string>> boxes = new List<Box<string>>();
+
+            for (int i = 0; i < n; i++)
+            {
+                Box<string> box = new Box<string>(Console.ReadLine());
+                boxes.Add(box);
+            }
+
+            string value = Console.ReadLine();
+            Box<string> comparableBox = new Box<string>(value);
+            int count = GetGreaterThanCount(boxes, comparableBox);
+
+            Console.WriteLine(count);
+
+        }
+
+        private static int GetGreaterThanCount<T>(List<Box<T>> boxes, Box<T> box)
+            where T : IComparable
+        {
+            int count = 0;
+
+            foreach (var item in boxes)
+            {
+                int compare = item.Value.CompareTo(box.Value);
+
+                if (compare > 0)
+                {
+                    count++;
+                }
+            }
+
+            return count;
+        }
+        private static void SwapIndexes<T>(List<Box<T>> boxes, int firtsindex, int secondIndex)
+            where T: IComparable
+        {
+            Box<T> temp = boxes[firtsindex];
+            boxes[firtsindex] = boxes[secondIndex];
+            boxes[secondIndex] = temp;
+        }
+    }
+}
