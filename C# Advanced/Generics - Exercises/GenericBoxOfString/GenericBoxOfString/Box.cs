063b5b7051b2a9c3a1570c9ac983d33dbd202e23
Kristiyan Enchev <kristiqnenchevv@gmail.com>
2021-02-25 22:39:04 +0200

    New Add

---
+++ C# Advanced/Generics - Exercises/GenericBoxOfString/GenericBoxOfString/Box.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GenericBoxOfString
+{
+    public class Box<T>
+    {
+        public Box(T value)
+        {
+            this.Value = value;
+        }
+
+        public T Value { get; private set; }
+
+        public override string ToString()
+        {
+            Type valueType = this.Value.GetType();
+            string valueTypeFullName = valueType.FullName;
+
+            return $"{valueTypeFullName}: {this.Value}";
+        }
+    }
+}
---
+++ C# Advanced/Generics - Exercises/GenericBoxOfString/GenericBoxOfString/StartUp.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace GenericBoxOfString
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                Box<string> box = new Box<string>(Console.ReadLine());
+
+                Console.WriteLine(box);
+            }
+        }
+    }
+}
