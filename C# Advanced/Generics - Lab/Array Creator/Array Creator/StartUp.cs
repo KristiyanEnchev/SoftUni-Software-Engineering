0e9137ae7494f1e70061a541f173c11d32edc728
Kristiyan Enchev <kristiqnenchevv@gmail.com>
2021-02-26 15:39:17 +0200

    New Add

---
+++ C# Advanced/Generics - Lab/Array Creator/Array Creator/StartUp.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace GenericArrayCreator
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            string[] array = GenericArrayCreator.Create(5, "pesho");
+
+            Console.WriteLine(string.Join(",", array));
+        }
+    }
+}
