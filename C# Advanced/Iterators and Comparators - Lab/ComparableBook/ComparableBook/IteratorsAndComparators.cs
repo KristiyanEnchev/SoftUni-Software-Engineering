ec7113a2286839027e91ccc0d776e93f354be10e
Kristiyan Enchev <kristiqnenchevv@gmail.com>
2021-02-28 22:38:16 +0200

    New Add

---
+++ C# Advanced/Iterators and Comparators - Lab/ComparableBook/ComparableBook/IteratorsAndComparators.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+namespace IteratorsAndComparators
+{
+    public class IteratorsAndComparators<T> : IEnumerator<T>
+    {
+        private List<T> books;
+        private int currentIndex = -1;
+
+        public IteratorsAndComparators(List<T> books)
+        {
+            this.books = books;
+        }
+
+        public T Current => books[currentIndex];
+
+        object IEnumerator.Current => Current;
+
+        public void Dispose()
+        {
+
+        }
+
+        public bool MoveNext()
+        {
+            currentIndex ++;
+
+            return currentIndex < books.Count;
+        }
+
+        public void Reset()
+        {
+            currentIndex = -1;
+        }
+    }
+}
