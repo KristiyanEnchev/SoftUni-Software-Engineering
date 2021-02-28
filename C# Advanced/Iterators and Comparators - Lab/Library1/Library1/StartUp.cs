e0e2dd174223a10fc238ff28dffac6f6f8866813
Kristiyan Enchev <kristiqnenchevv@gmail.com>
2021-02-28 22:38:53 +0200

    New Add

---
+++ C# Advanced/Iterators and Comparators - Lab/Library1/Library1/StartUp.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace IteratorsAndComparators
+{
+    class StartUp
+    {
+        static void Main(string[] args)
+        {
+            Book bookOne = new Book("Book One Name", 1991, "KIKO");
+            Book bookTwo = new Book("Book Two Name", 1992, "AZ");
+            Book bookThree = new Book("Book Three Name", 1993, "TOI");
+
+            Library fires = new Library();
+            Library second = new Library(bookOne, bookTwo, bookThree);
+
+            foreach (var book in second)
+            {
+                Console.WriteLine($"{book.Title} - {book.Year} - {book.Authors[0]}");
+            }
+        }
+    }
+}
