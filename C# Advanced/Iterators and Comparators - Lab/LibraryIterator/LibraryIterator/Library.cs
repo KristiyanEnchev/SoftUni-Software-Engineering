c11aea1ca85eaa1f4a1233cc4ead5aefe7cbfbd3
Kristiyan Enchev <kristiqnenchevv@gmail.com>
2021-02-28 22:39:09 +0200

    New Add

---
+++ C# Advanced/Iterators and Comparators - Lab/LibraryIterator/LibraryIterator/Library.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace IteratorsAndComparators
+{
+    public class Library : IEnumerable<Book>
+    {
+        private List<Book> books;
+        public Library(params Book[] books)
+        {
+            this.Books = books.ToList();
+        }
+
+        public List<Book> Books { get; set; }
+
+        public IEnumerator<Book> GetEnumerator()
+        {
+            return new LibraryIterator(this.Books);
+        }
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            //old interface
+            return GetEnumerator();
+        }
+
+        //Enumerator without yield
+        private class LibraryIterator : IEnumerator<Book>
+        {
+            private readonly List<Book> books;
+            private int curIndex;
+
+            public LibraryIterator(List<Book> books)
+            {
+                this.Reset();
+                this.books = new List<Book>(books);
+            }
+
+            public Book Current => this.books[this.curIndex];
+
+            object IEnumerator.Current => this.Current;
+
+            public void Dispose()
+            {                
+            }
+
+            public bool MoveNext()
+            {
+                return ++this.curIndex < this.books.Count;
+            }
+
+            public void Reset()
+            {
+                this.curIndex = -1;
+            }
+        }
+    }
+}
