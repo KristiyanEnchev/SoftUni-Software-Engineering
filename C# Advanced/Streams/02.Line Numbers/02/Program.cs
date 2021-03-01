402d05f06ee855f5887b39f1be763a1339dc937a
Kristiyan Enchev <kristiqnenchevv@gmail.com>
2021-03-01 18:51:45 +0200

    New Add

---
+++ C# Advanced/Streams/02.Line Numbers/02/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string outputPath = Path.Combine("..", "..", "..", "output.txt");
+            string[] lines = File.ReadAllLines("text.txt");
+            List<char> punctuationMarck = new List<char>() { '-', ',', '.', '!', '?', '\'' };
+            List<string> newLines = new List<string>();
+
+            for (int i = 0; i < lines.Length; i++)
+            {
+                string line = lines[i];
+                int letters = 0;
+                int punctuationCount = 0;
+
+                foreach (var item in line)
+                {
+                    if (char.IsLetter(item))
+                    {
+                        letters++;
+                    }
+                    else if (punctuationMarck.Contains(item))
+                    {
+                        punctuationCount++;
+                    }
+                }
+
+                string newLine = $"Line {i+1}: {line} ({letters})({punctuationCount})";
+                newLines.Add(newLine);
+
+            }
+                Console.WriteLine(string.Join(Environment.NewLine, newLines));
+                File.WriteAllLines(outputPath, newLines);
+        }
+    }
+}
