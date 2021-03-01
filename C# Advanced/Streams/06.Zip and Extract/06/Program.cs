c0f9dcf4e09cb4ee3cf0be9713931fbebadef8b7
Kristiyan Enchev <kristiqnenchevv@gmail.com>
2021-03-01 18:52:16 +0200

    New Add

---
+++ C# Advanced/Streams/06.Zip and Extract/06/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.IO.Compression;
+
+namespace _06
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using ZipArchive zipFile = ZipFile.Open("zipfile.zip", ZipArchiveMode.Create);
+            ZipArchiveEntry zipArchiveEntry = zipFile.CreateEntryFromFile("copyMe.png", "copyMeOutput.png");
+        }
+    }
+}
