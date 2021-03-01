8432a86593af6c9edc7d819dd33de70954fd2092
Kristiyan Enchev <kristiqnenchevv@gmail.com>
2021-03-01 18:52:00 +0200

    New Add

---
+++ C# Advanced/Streams/04.Copy Binary File/04/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace _04
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using FileStream readFile = new FileStream("copyMe.png", FileMode.Open);
+            using FileStream writeStream = new FileStream("newImage.png", FileMode.Create);
+
+
+            while (readFile.Position < readFile.Length)
+            {
+                byte[] buffer = new byte[4096];
+                int count = readFile.Read(buffer, 0, buffer.Length);
+
+
+                writeStream.Write(buffer);
+            }
+
+        }
+    }
+}
