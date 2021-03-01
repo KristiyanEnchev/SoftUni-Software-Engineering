02a16b7cd160130670978af2b3fa53d2fa6b6b0d
Kristiyan Enchev <kristiqnenchevv@gmail.com>
2021-03-01 18:51:36 +0200

    New Add

---
+++ C# Advanced/Streams/01.Even Lines/01/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace _01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string fileLIne = "../../../text.txt";
+            //string fileline = Path.Combine("..","..","..", "text.txt");
+            using (StreamReader reader = new StreamReader(fileLIne))
+            {
+                int count = 0;
+                string chars = "-,.!?";
+                while (!reader.EndOfStream)
+                {
+                    string line = reader.ReadLine();
+                    if (count % 2 == 0)
+                    {
+                        foreach (var item in chars)
+                        {
+                            line = line.Replace(item, '@');
+                        }
+                        string[] splittedLine = line.Split(" ", StringSplitOptions.RemoveEmptyEntries).Reverse().ToArray();
+                        Console.WriteLine(string.Join(" ", splittedLine));
+                    }
+                    count++;
+
+                }
+            }
+        }
+    }
+}
