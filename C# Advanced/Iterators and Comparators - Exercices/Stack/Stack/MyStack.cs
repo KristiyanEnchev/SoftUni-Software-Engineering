b0e1269ebde61763fdc5152fed6d3e607e8b6790
Kristiyan Enchev <kristiqnenchevv@gmail.com>
2021-03-03 19:27:48 +0200

    New Add

---
+++ C# Advanced/Iterators and Comparators - Exercices/Stack/Stack/MyStack.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Stack
+{
+    public class MyStack<T> : IEnumerable<T>
+    {
+        private List<T> items;
+        public MyStack()
+        {
+            this.items = new List<T>();
+        }
+
+        public MyStack(List<T> items)
+        {
+            this.items = items;
+        }
+
+        public void Push(T item)
+        {
+            this.items.Add(item);
+        }
+
+        public T Pop()
+        {
+            if (this.items.Count == 0)
+            {
+                throw new InvalidOperationException("No elements");
+            }
+
+            int index = this.items.Count - 1;
+            T item = this.items[index];
+            this.items.RemoveAt(index);
+
+            return item;
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            for (int i = this.items.Count - 1; i >= 0; i--)
+            {
+                yield return this.items[i];
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return this.GetEnumerator();
+        }
+    }
+}
---
+++ C# Advanced/Iterators and Comparators - Exercices/Stack/Stack/StartUp.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace Stack
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            MyStack<int> mystack = new MyStack<int>();
+
+            string comandInput = Console.ReadLine();
+
+            while (comandInput != "END")
+            {
+                string[] comandData = comandInput.Split(new string[] { ", ", " " }, StringSplitOptions.RemoveEmptyEntries);
+                string comand = comandData[0];
+
+                if (comand == "Push")
+                {
+                    for (int i = 1; i < comandData.Length; i++)
+                    {
+                        int item = int.Parse(comandData[i]);
+                        mystack.Push(item);
+                    }
+                }
+                else if (comand == "Pop")
+                {
+                    try
+                    {
+                        mystack.Pop();
+                    }
+                    catch (InvalidOperationException ex)
+                    {
+                        Console.WriteLine(ex.Message);
+                    }
+                }
+
+
+                comandInput = Console.ReadLine();
+            }
+
+            foreach (var item in mystack)
+            {
+                Console.WriteLine(item);
+            }
+
+            Console.WriteLine(string.Join(Environment.NewLine, mystack));
+        }
+    }
+}
