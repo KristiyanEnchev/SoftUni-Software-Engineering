ff71ee4cab28b4fbda3addaa4e59ac9592c06efb
Kristiyan Enchev <kristiqnenchevv@gmail.com>
2021-03-03 19:26:43 +0200

    New Add

---
+++ C# Advanced/Iterators and Comparators - Exercices/EqualityLogic/EqualityLogic/StartUp.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace EqualityLogic
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+
+            SortedSet<Person> people = new SortedSet<Person>();
+            HashSet<Person> people1 = new HashSet<Person>();
+
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                string[] info = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+                string name = info[0];
+                int age = int.Parse(info[1]);
+
+                people.Add(new Person(name, age));
+                people1.Add(new Person(name, age));
+            }
+
+            Console.WriteLine(people.Count);
+            Console.WriteLine(people1.Count);
+        }
+    }
+}
