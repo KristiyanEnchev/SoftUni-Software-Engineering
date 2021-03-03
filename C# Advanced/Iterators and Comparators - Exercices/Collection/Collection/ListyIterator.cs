143f1df668cc411be6695cf579d9f10c2e782bf8
Kristiyan Enchev <kristiqnenchevv@gmail.com>
2021-03-03 19:26:22 +0200

    New Add

---
+++ C# Advanced/Iterators and Comparators - Exercices/Collection/Collection/ListyIterator.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ListyIterator
+{
+    public class ListyIterator<T> : IEnumerable<T>
+    {
+        private List<T> items;
+        private int index;
+
+        public ListyIterator(List<T> items)
+        {
+            this.items = items;
+        }
+
+        public ListyIterator(params T[] items)
+        {
+            this.items = items.ToList();
+        }
+
+        public bool Move()
+        {
+            bool hasNext = this.HasNext();
+
+            if (this.HasNext())
+            {
+                this.index++;
+            }
+
+            return hasNext;
+        }
+
+        public void Print()
+        {
+
+            if (this.index >= this.items.Count)
+            {
+                throw new InvalidOperationException("Invalid Operation!");
+            }
+
+            Console.WriteLine(this.items[this.index]);
+
+        }
+
+        public bool HasNext()
+        {
+            return this.index < this.items.Count - 1;
+        }
+
+        //public List<T> PrintAll()
+        //{
+        //    foreach (var item in items)
+        //    {
+        //        Console.WriteLine(string.Join());
+        //    }
+        //}
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            foreach (var item in this.items)
+            {
+                yield return item;
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return this.GetEnumerator();
+        }
+    }
+}
---
+++ C# Advanced/Iterators and Comparators - Exercices/Collection/Collection/StartUp.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ListyIterator
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            List<string> items = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).Skip(1).ToList();
+            ListyIterator<string> List = new ListyIterator<string>(items);
+
+            string comand = Console.ReadLine();
+            while (comand != "END")
+            {
+                switch (comand)
+                {
+                    case "Move":
+                        Console.WriteLine(List.Move());
+                        break;
+                    case "HasNext":
+                        Console.WriteLine(List.HasNext());
+                        break;
+                    case "Print":
+                        try
+                        {
+                            List.Print();
+
+                        }
+                        catch (InvalidOperationException ex)
+                        {
+                            Console.WriteLine(ex.Message);
+                        }
+                        break;
+                    case "PrintAll":
+                        Console.WriteLine(string.Join(" ", List));
+                        break;
+                }
+
+
+                comand = Console.ReadLine();
+            }
+        }
+    }
+}
