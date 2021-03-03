33eb81538d13ecb2efef962e087e495542b69b65
Kristiyan Enchev <kristiqnenchevv@gmail.com>
2021-03-03 19:26:54 +0200

    New Add

---
+++ C# Advanced/Iterators and Comparators - Exercices/Froggy/Froggy/Lake.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Froggy
+{
+    public class Lake : IEnumerable<int>
+    {
+        private List<int> stones;
+        public Lake(List<int> stones)
+        {
+            this.stones = stones;
+        }
+
+        public IEnumerator<int> GetEnumerator()
+        {
+            for (int i = 0; i < stones.Count; i += 2)
+            {
+                yield return this.stones[i];
+            }
+
+            //int index = this.stones.Count - 1 % 2 != 0 ? this.stones.Count - 1 : this.stones.Count - 2;
+
+            //for (int i = index; i >= 0; i -= 2)
+            //{
+            //    yield return this.stones[i];
+            //}
+
+            for (int i = stones.Count - 1; i >= 0; i--)
+            {
+                if (i % 2 != 0)
+                {
+                    yield return this.stones[i];
+                }
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return this.GetEnumerator();
+        }
+    }
+}
