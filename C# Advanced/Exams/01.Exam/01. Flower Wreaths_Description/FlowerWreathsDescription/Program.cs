ddc31354f8d37188569263810fe66d0cc2547235
Kristiyan Enchev <kristiqnenchevv@gmail.com>
2021-03-04 18:43:54 +0200

    New Add

---
+++ C# Advanced/Exams/01.Exam/01. Flower Wreaths_Description/FlowerWreathsDescription/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace ConsoleApp57
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Stack<int> lilies = new Stack<int>(Console.ReadLine().Split(",").Select(int.Parse).ToArray());
+            Queue<int> roses = new Queue<int>(Console.ReadLine().Split(",").Select(int.Parse).ToArray());
+
+            int wreaths = 0;
+            int flowers = 0;
+
+            while (roses.Any() && lilies.Any())
+            {
+                int lili = lilies.Pop();
+                int rose = roses.Dequeue();
+
+                while (true)
+                {
+                    int sum = lili + rose;
+                    if (sum == 15)
+                    {
+                        wreaths++;
+                        break;
+                    }
+                    else if (sum < 15)
+                    {
+                        flowers += sum;
+                        break;
+                    }
+                    else
+                    {
+                        lili -= 2;
+                    }
+                }
+            }
+
+            wreaths += flowers / 15;
+
+            if (wreaths >= 5)
+            {
+                Console.WriteLine($"You made it, you are going to the competition with {wreaths} wreaths!");
+            }
+            else
+            {
+                Console.WriteLine($"You didn't make it, you need {5 - wreaths} wreaths more!");
+            }
+
+        }
+    }
+}
