270f1d7b79c59a87563f83d083ceb220dae9e45a
Kristiyan Enchev <kristiqnenchevv@gmail.com>
2021-03-05 14:09:53 +0200

    New Add

---
+++ C# Advanced/Exams/02.Exam/02. Selling/02. Selling/Program.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            char[,] matrix = new char[n, n];
+
+            int playerRow = 0;
+            int playerCol = 0;
+
+            for (int row = 0; row < n; row++)
+            {
+                string input = Console.ReadLine();
+
+                for (int col = 0; col < input.Length; col++)
+                {
+                    matrix[row, col] = input[col];
+
+                    if (matrix[row, col] == 'S')
+                    {
+                        playerRow = row;
+                        playerCol = col;
+                    }
+                }
+            }
+
+            int moneyColected = 0;
+
+            while (moneyColected < 50)
+            {
+                string comand = Console.ReadLine();
+
+                matrix[playerRow, playerCol] = '-';
+                playerRow = MoveRow(playerRow, comand);
+                playerCol = MoveCol(playerCol, comand);
+
+                if (!IsPositionValid(playerRow, playerCol, n, n))
+                {
+                    Console.WriteLine("Bad news, you are out of the bakery.");
+                    break;
+                }
+
+                if (char.IsDigit(matrix[playerRow, playerCol]))
+                {
+                    moneyColected += int.Parse(matrix[playerRow, playerCol].ToString());
+                }
+
+                if (matrix[playerRow, playerCol] == 'O')
+                {
+
+                    matrix[playerRow, playerCol] = '-';
+                    int pilarRow = 0;
+                    int pilarCol = 0;
+
+                    for (int row = 0; row < n; row++)
+                    {
+                        for (int col = 0; col < n; col++)
+                        {
+                            if (matrix[row, col] == 'O')
+                            {
+                                pilarRow = row;
+                                pilarCol = col;
+                            }
+                        }
+                    }
+
+                    playerRow = pilarRow;
+                    playerCol = pilarCol;
+                }
+
+                matrix[playerRow, playerCol] = 'S';
+            }
+
+            if (moneyColected >= 50)
+            {
+                Console.WriteLine("Good news! You succeeded in collecting enough money!");
+            }
+
+            Console.WriteLine($"Money: {moneyColected}");
+            PrintMatrix(matrix);
+        }
+
+        public static void PrintMatrix(char[,] matrix)
+        {
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                for (int col = 0; col < matrix.GetLength(1); col++)
+                {
+                    Console.Write(matrix[row, col]);
+                }
+
+                Console.WriteLine();
+            }
+        }
+
+        public static int MoveRow(int row, string movement)
+        {
+            if (movement == "up")
+            {
+                return row - 1;
+            }
+            if (movement == "down")
+            {
+                return row + 1;
+            }
+
+            return row;
+        }
+
+        public static int MoveCol(int col, string movement)
+        {
+            if (movement == "left")
+            {
+                return col - 1;
+            }
+            if (movement == "right")
+            {
+                return col + 1;
+            }
+
+            return col;
+        }
+
+        public static bool IsPositionValid(int row, int col, int rows, int cols)
+        {
+            if (row < 0 || row >= rows)
+            {
+                return false;
+            }
+            if (col < 0 || col >= cols)
+            {
+                return false;
+            }
+
+            return true;
+        }
+    }
+}
