ab4e0cf16ebba8bd3bc3096675659acac9a4b617
Kristiyan Enchev <kristiqnenchevv@gmail.com>
2021-03-08 16:10:42 +0200

    New Add

---
+++ C# Advanced/Exams/05.Exam/Guild/Guild/Guild.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace Guild
+{
+    public class Guild
+    {
+        private List<Player> roster;
+
+        public Guild(string name, int capacity)
+        {
+            this.Name = name;
+            this.Capacity = capacity;
+            roster = new List<Player>();
+        }
+
+        public string Name { get; set; }
+        public int Capacity { get; set; }
+
+        public int Count { get { return roster.Count; } }
+
+
+        public void AddPlayer(Player player)
+        {
+            if (roster.Count < Capacity)
+            {
+                roster.Add(player);
+            }
+        }
+
+        public bool RemovePlayer(string name)
+        {
+            Player player = roster.FirstOrDefault(x => x.Name == name);
+
+            if (player == null)
+            {
+                return false;
+            }
+
+            roster.Remove(player);
+            return true;
+        }
+
+        public void PromotePlayer(string name)
+        {
+            Player player = roster.FirstOrDefault(x => x.Name == name);
+
+            if (player.Rank != "Member")
+            {
+                player.Rank = "Member";
+            }
+        }
+
+        public void DemotePlayer(string name)
+        {
+            Player player = roster.FirstOrDefault(x => x.Name == name);
+
+            if (player.Rank != "Trial")
+            {
+                player.Rank = "Trial";
+            }
+        }
+
+        public Player[] KickPlayersByClass(string clas)
+        {
+            List<Player> players = new List<Player>();
+
+            foreach (var player in roster)
+            {
+                if (player.Class == clas)
+                {
+                    players.Add(player);
+                }
+            }
+
+            foreach (var player in players)
+            {
+                roster.Remove(player);
+            }
+
+            return players.ToArray();
+        }
+
+        public string Report()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            sb.AppendLine($"Players in the guild: {this.Name}");
+
+            foreach (var player in roster)
+            {
+                sb.AppendLine(player.ToString());
+            }
+
+            return sb.ToString().TrimEnd();
+        }
+    }
+}
---
+++ C# Advanced/Exams/05.Exam/Guild/Guild/Player.cs
@@ -0,0 +1,26 @@
+﻿namespace Guild
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+
+    public class Player
+    {
+        public Player(string name, string clas)
+        {
+            this.Name = name;
+            this.Class = clas;
+        }
+
+        public string Name { get; set; }
+        public string Class { get; set; }
+        public string Rank { get; set; } = "Trial";
+
+        public string Description { get; set; } = "n/a";
+
+        public override string ToString()
+        {
+            return $"Player {Name}: {Class}{Environment.NewLine}Rank: { Rank}{Environment.NewLine}Description: {Description}";
+        }
+    }
+}
