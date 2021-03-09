d6ed42d8fd4247e2be84c8c1033d3084a0cbb6fb
Kristiyan Enchev <kristiqnenchevv@gmail.com>
2021-03-09 13:52:38 +0200

    New Add

---
+++ C# OOP/Inheritance - Lab/RandomList/RandomList/StartUp.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace CustomRandomList
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            RandomList list = new RandomList();
+
+            list.Add("Pesho");
+            list.Add("Gogo");
+            list.Add("Lili");
+            list.Add("Mari");
+            list.Add("Misho");
+            list.Add("Grogo");
+
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine(list.RandomString());
+            }
+        }
+    }
+}
