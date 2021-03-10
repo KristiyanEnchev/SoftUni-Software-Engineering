ed258e1021e4a99310e91e5532c75dfbc07e7350
Kristiyan Enchev <kristiqnenchevv@gmail.com>
2021-03-10 13:35:44 +0200

    New Add

---
+++ C# OOP/Inheritance - Lab/StackOfStrings/StackOfStrings/StartUp.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CustomStack
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            StackOfStrings stack = new StackOfStrings();
+
+            stack.AddRange(new List<string>()
+            {
+                "123",
+                "456",
+                "789",
+                "Az sum"
+
+            });
+
+            foreach (var item in stack)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
