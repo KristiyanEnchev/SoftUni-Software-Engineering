3e35b40509dd5af24118bd5385e5612a003337a9
Kristiyan Enchev <kristiqnenchevv@gmail.com>
2021-03-10 13:35:34 +0200

    New Add

---
+++ C# OOP/Inheritance - Lab/SingleInheritance/SingleInheritance/StartUp.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Farm
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            Dog sharo = new Dog();
+
+            sharo.Eat();
+            sharo.Bark();
+        }
+    }
+}
