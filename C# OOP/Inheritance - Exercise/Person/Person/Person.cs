1e3db4c51b1d4e6c47843a397f135357c8ea82d6
Kristiyan Enchev <kristiqnenchevv@gmail.com>
2021-03-11 14:35:18 +0200

    New Add

---
+++ C# OOP/Inheritance - Exercise/Person/Person/Person.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Person
+{
+    public class Person
+    {
+        public Person(string name, int age)
+        {
+            this.Name = name;
+            this.Age = age;
+        }
+
+        public string Name { get; set; }
+        public int Age { get; set; }
+
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.Append(string.Format("Name: {0}, Age: {1}",
+                this.Name,
+                this.Age));
+
+            return sb.ToString();
+        }
+    }
+}
