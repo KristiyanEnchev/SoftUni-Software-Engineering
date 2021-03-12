582e8f73bf7e9f9b75dbc31513bc1bbc8745835e
Kristiyan Enchev <kristiqnenchevv@gmail.com>
2021-03-12 16:30:39 +0200

    New Add

---
+++ C# OOP/Inheritance - Exercise/Restaurant/Restaurant/Coffee.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Restaurant
+{
+    public class Coffee : HotBeverage
+    {
+        public Coffee(string name, double caffeine)
+            : base(name, 3.5m, 50)
+        {
+            this.Caffeine = caffeine;
+        }
+
+        public double Caffeine { get; set; }
+    }
+}
