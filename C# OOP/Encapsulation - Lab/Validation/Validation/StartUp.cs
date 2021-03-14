064c93be2a259f48377b10379791c9da83b14ba6
Kristiyan Enchev <kristiqnenchevv@gmail.com>
2021-03-14 19:53:37 +0200

    New Add

---
+++ C# OOP/Encapsulation - Lab/Validation/Validation/StartUp.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace PersonsInfo
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            int lines = int.Parse(Console.ReadLine());
+            List<Person> persons = new List<Person>();
+            for (int i = 0; i < lines; i++)
+            {
+                try
+                {
+                    string[] cmdArgs = Console.ReadLine().Split();
+                    Person person = new Person(cmdArgs[0],
+                                            cmdArgs[1],
+                                            int.Parse(cmdArgs[2]),
+                                            decimal.Parse(cmdArgs[3]));
+                    persons.Add(person);
+                }
+                catch (Exception ex)
+                {
+
+                    Console.WriteLine(ex.Message);
+                }
+
+            }
+            decimal parcentage = decimal.Parse(Console.ReadLine());
+            persons.ForEach(p => p.IncreaseSalary(parcentage));
+            persons.ForEach(p => Console.WriteLine(p.ToString()));
+        }
+    }
+}
