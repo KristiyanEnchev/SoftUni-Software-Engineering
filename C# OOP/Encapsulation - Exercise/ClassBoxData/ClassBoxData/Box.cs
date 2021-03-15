37ad55f7b5ed5a2d0589df5a0be4b03d90b8bef4
Kristiyan Enchev <kristiqnenchevv@gmail.com>
2021-03-15 14:28:40 +0200

    New Add

---
+++ C# OOP/Encapsulation - Exercise/ClassBoxData/ClassBoxData/Box.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassBoxData
+{
+    public class Box
+    {
+        private double length;
+        private double width;
+        private double height;
+
+        public Box(double length, double width, double height)
+        {
+            this.Length = length;
+            this.Width = width;
+            this.Height = height;
+        }
+
+        public double Length
+        {
+            get
+            {
+                return this.length;
+            }
+            private set
+            {
+                this.ThrowIfInvalidSide(value, nameof(Length));
+
+                length = value;
+            }
+        }
+
+
+        public double Width
+        {
+            get
+            {
+                return this.width;
+            }
+            private set
+            {
+                this.ThrowIfInvalidSide(value, nameof(Width));
+
+                width = value;
+            }
+        }
+        public double Height
+        {
+            get
+            {
+                return this.height;
+            }
+            private set
+            {
+                this.ThrowIfInvalidSide(value, nameof(Height));
+
+                height = value;
+            }
+        }
+
+        public double LiteralSurfaceArea()
+        {
+            double literalValue = 2 * this.Length * this.Height + 2 * this.Width * this.Height;
+            return literalValue;
+        }
+        public double SurfaceArea()
+        {
+            double surfaceArea = 2 * this.Length * this.Width + 2 * this.Length * this.Height + 2 * this.Width * this.Height;
+            return surfaceArea;
+        }
+        public double Volume()
+        {
+            double volume = Length * Width * Height;
+            return volume;
+        }
+
+
+        private void ThrowIfInvalidSide(double value, string side)
+        {
+            if (value <= 0)
+            {
+                throw new ArgumentException($"{side} cannot be zero or negative.");
+            }
+        }
+
+    }
+}
---
+++ C# OOP/Encapsulation - Exercise/ClassBoxData/ClassBoxData/StartUp.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace ClassBoxData
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            double length = double.Parse(Console.ReadLine());
+            double width = double.Parse(Console.ReadLine());
+            double height = double.Parse(Console.ReadLine());
+
+            try
+            {
+                Box box = new Box(length, width, height);
+
+                Console.WriteLine($"Surface Area - {box.SurfaceArea():f2}");
+                Console.WriteLine($"Lateral Surface Area - {box.LiteralSurfaceArea():f2}");
+                Console.WriteLine($"Volume - {box.Volume():f2}");
+            }
+            catch (ArgumentException ex)
+            {
+
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
