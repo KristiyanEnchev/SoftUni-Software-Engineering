a28209fd09fdc04aaa109b8197430953b9f94e9b
Kristiyan Enchev <kristiqnenchevv@gmail.com>
2021-03-16 12:41:42 +0200

    New Add

---
+++ C# OOP/Encapsulation - Exercise/ShoppingSpree/ShoppingSpree/Validator.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ShoppingSpree
+{
+    public static class Validator
+    {
+        public static void ThrowIfStringIsNullOrEmpty(string str, string exeptionMassage)
+        {
+            if (string.IsNullOrWhiteSpace(str))
+            {
+                throw new ArgumentException(exeptionMassage);
+            }
+        }
+
+        public static void ThrowIfNumberIsLessThanZero(decimal number, string exeptionMassage)
+        {
+            if (number < 0)
+            {
+                throw new ArgumentException(exeptionMassage);
+            }
+        }
+    }
+}
