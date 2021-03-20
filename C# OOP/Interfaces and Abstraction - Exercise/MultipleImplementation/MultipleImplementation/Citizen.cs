519231b5af4065cc6918b5686643731c0b55c3b6
Kristiyan Enchev <kristiqnenchevv@gmail.com>
2021-03-20 15:12:09 +0200

    New Add

---
+++ C# OOP/Interfaces and Abstraction - Exercise/MultipleImplementation/MultipleImplementation/Citizen.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PersonInfo
+{
+    public class Citizen : IPerson, IBirthable, IIdentifiable
+    {
+        public Citizen(string name, int age, string id, string birthdate)
+        {
+            this.Name = name;
+            this.Age = age;
+            this.Birthdate = birthdate;
+            this.Id = id;
+        }
+
+        public string Name { get; private set; }
+        public int Age { get; private set; }
+        public string Birthdate { get; private set; }
+        public string Id { get; private set; }
+    }
+}
