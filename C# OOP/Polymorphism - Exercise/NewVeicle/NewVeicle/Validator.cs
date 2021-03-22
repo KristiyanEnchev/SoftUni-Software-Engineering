a358baece0863b10cec49675568959d1827e619d
Kristiyan Enchev <kristiqnenchevv@gmail.com>
2021-03-22 12:14:12 +0200

    New Add

---
+++ C# OOP/Polymorphism - Exercise/NewVeicle/NewVeicle/Validator.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NewVeicle
+{
+    public class Validator
+    {
+        public static void CheckPositiveNumber(double value)
+        {
+            if (value < 0)
+            {
+                Console.WriteLine("Fuel must be a positive number");
+            }
+        }
+    }
+}
