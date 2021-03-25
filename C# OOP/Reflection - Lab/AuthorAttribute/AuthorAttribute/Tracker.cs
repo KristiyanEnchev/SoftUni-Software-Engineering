3a6e713714206c2b3a753f4c22c504e7f30ec6d8
Kristiyan Enchev <kristiqnenchevv@gmail.com>
2021-03-25 13:57:42 +0200

    New Add

---
+++ C# OOP/Reflection - Lab/AuthorAttribute/AuthorAttribute/Tracker.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+
+namespace AuthorProblem
+{
+    public class Tracker
+    {
+        public void PrintMethodsByAuthor()
+        {
+            Type type = typeof(StartUp);
+
+            MethodInfo[] methods = type.GetMethods(BindingFlags.Instance | BindingFlags.Static
+                | BindingFlags.Public | BindingFlags.NonPublic);
+
+            foreach (var method in methods)
+            {
+                if (method.CustomAttributes.Any(x => x.AttributeType == typeof(AuthorAttribute)))
+                {
+                    var attributes = method.GetCustomAttributes(false);
+
+                    foreach (AuthorAttribute att in attributes)
+                    {
+                        Console.WriteLine($"{method.Name} is written by {att.Name}");
+                    }   
+                }
+            }
+        }
+    }
+}
