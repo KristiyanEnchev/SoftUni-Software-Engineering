dc2d5aa810396ee8947b38c053c1ed1bc128a46b
Kristiyan Enchev <kristiqnenchevv@gmail.com>
2021-03-26 19:48:43 +0200

    New Add

---
+++ C# OOP/Reflection - Lab/CreateAttribute1/CreateAttribute/StartUp.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace AuthorProblem
+{
+    [Author("Kiko")]
+    [Author("Ventsi")]
+    public class StartUp
+    {
+        [Author("Shasho")]
+        [Author("Gosho")]
+        static void Main(string[] args)
+        {
+            var tracker = new Tracker();
+            tracker.PrintMethodsByAuthor();
+
+        }
+    }
+}
---
+++ C# OOP/Reflection - Lab/CreateAttribute1/CreateAttribute/Tracker.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Reflection;
+using System.Linq;
+
+namespace AuthorProblem
+{
+    public class Tracker
+    {
+        public void PrintMethodsByAuthor()
+        {
+            Type[] allTypes = Assembly.GetExecutingAssembly().GetTypes();
+
+            foreach (var type in allTypes)
+            {
+
+                MethodInfo[] methoods = type.GetMethods();
+
+                foreach (var method in methoods)
+                {
+                    if (!method.GetCustomAttributes().Any(a => a.GetType() == typeof(AuthorAttribute)))
+                    {
+                        continue;
+                    }
+
+                    Attribute[] atributes = method.GetCustomAttributes().ToArray();
+
+                    foreach (var attr in atributes)
+                    {
+                        if (attr is AuthorAttribute)
+                        {
+                            Console.WriteLine($"{type.Name} is written by {((AuthorAttribute)attr).Name}");
+                        }
+                    }
+                }
+
+                if (!type.GetCustomAttributes().Any(a => a.GetType() == typeof(AuthorAttribute)))
+                {
+                    continue;
+                }
+
+                Attribute[] attributes = type.GetCustomAttributes()
+                    .Where(t => t.GetType() == typeof(AuthorAttribute))
+                    .Select(a => (AuthorAttribute)a)
+                    .ToArray();
+
+                foreach (var attribute in attributes)
+                {
+                    Console.WriteLine($"{type.Name} is written by {((AuthorAttribute)attribute).Name}");
+                }
+            }
+        }
+    }
+}
