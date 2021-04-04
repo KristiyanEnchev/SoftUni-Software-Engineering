e8bbddca9c596a5e3546441f1e7fa945762d14b6
Kristiyan Enchev <kristiqnenchevv@gmail.com>
2021-04-04 13:13:35 +0300

    New Add

---
+++ C# OOP/Mocking - Lab/INStock - Skeleton/INStock/Product.cs
@@ -0,0 +1,75 @@
+﻿using INStock.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Text;
+
+namespace INStock
+{
+    public class Product : IProduct
+    {
+        private string label;
+        private decimal price;
+        private int quantity;
+
+        public Product(string label, decimal price, int quantity)
+        {
+            this.Label = label;
+            this.Price = price;
+            this.Quantity = quantity;
+        }
+
+        public string Label
+        {
+            get
+            {
+                return this.label;
+            }
+            private set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentException("Label cannot be null.");
+                }
+                this.label = value;
+            }
+        }
+
+        public decimal Price
+        {
+            get
+            {
+                return this.price;
+            }
+            private set
+            {
+                if (value <= 0)
+                {
+                    throw new ArgumentException("Price cannot be less or equal to zero.");
+                }
+                this.price = value;
+            }
+        }
+
+        public int Quantity
+        {
+            get
+            {
+                return this.quantity;
+            }
+            private set
+            {
+                if (value < 0)
+                {
+                    throw new ArgumentException("Quantity cannot be less than zero.");
+                }
+                this.quantity = value;
+            }
+        }
+
+        public int CompareTo(IProduct other)
+        {
+            return this.Price.CompareTo(other.Price);
+        }
+    }
+}
---
+++ C# OOP/Mocking - Lab/INStock - Skeleton/INStock/ProductStock.cs
@@ -0,0 +1,127 @@
+﻿using INStock.Contracts;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace INStock
+{
+    public class ProductStock : IProductStock
+    {
+        private List<IProduct> products;
+        private Dictionary<int, IProduct> dict;
+        private int index;
+
+        public ProductStock()
+        {
+            this.products = new List<IProduct>();
+            this.dict = new Dictionary<int, IProduct>();
+            index = -1;
+        }
+        public IProduct this[int index]
+        { 
+            get 
+            {
+                return this.products[index];
+            }
+            set 
+            { 
+                this.products[index] = value; 
+            }
+        }
+
+        public int Count => this.products.Count;
+
+        public void Add(IProduct product)
+        {
+            if (!this.products.Any(p => p.Label == product.Label))
+            {
+                this.products.Add(product);
+                this.dict.Add(++index, product);
+            }
+        }
+
+        public bool Contains(IProduct product)
+        {
+            return this.products.Any(p => p.Label == product.Label);
+        }
+
+        public IProduct Find(int index)
+        {
+            if (index > this.index || index < this.index)
+            {
+                throw new IndexOutOfRangeException();
+            }
+            return this.dict[index];
+        }
+
+        public IEnumerable<IProduct> FindAllByPrice(decimal price)
+        {
+            return this.products.Where(p => p.Price == price);
+        }
+
+        public IEnumerable<IProduct> FindAllByQuantity(int quantity)
+        {
+            return this.products.Where(p => p.Quantity == quantity);
+        }
+
+        public IEnumerable<IProduct> FindAllInRange(decimal lowerEnd, decimal higherEnd)
+        {
+            var greaterThanLower = this.products.Where(p => p.Price >= lowerEnd);
+            var lowerThanGreater = greaterThanLower.Where(p => p.Price <= higherEnd).OrderByDescending(p => p.Price);
+            return lowerThanGreater;
+        }
+
+        public IProduct FindByLabel(string label)
+        {
+            IProduct product = this.products.FirstOrDefault(p => p.Label == label);
+            if (product == null)
+            {
+                throw new ArgumentException();
+            }
+            else
+            {
+                return product;
+            }
+        }
+
+        public IProduct FindMostExpensiveProduct()
+        {
+            decimal maxPrice = decimal.MinValue;
+            IProduct maxProduct = null;
+            foreach (var product in this.products)
+            {
+                if (product.Price > maxPrice)
+                {
+                    maxPrice = product.Price;
+                    maxProduct = product;
+                }
+            }
+            return maxProduct;
+        }
+
+        public IEnumerator<IProduct> GetEnumerator()
+        {
+            return this.GetEnumerator();
+        }
+
+        public bool Remove(IProduct product)
+        {
+            if (!this.products.Any(p => p.Label == product.Label))
+            {
+                this.products.Remove(product);
+                this.dict.Remove(++index, out product);
+
+                return true;
+            }
+
+            return false;
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+}
