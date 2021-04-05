8371ead3446309b6ccbd5e0e5a90cfa00141934a
Kristiyan Enchev <kristiqnenchevv@gmail.com>
2021-04-05 14:17:20 +0300

    New Add

---
+++ C# OOP/SOLID - LAB/P02GraphicEditor/P02GraphicEditor/Circle.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace P02GraphicEditor
+{
+    public class Circle : IShape
+    {
+    }
+}
---
+++ C# OOP/SOLID - LAB/P02GraphicEditor/P02GraphicEditor/Rectangle.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace P02GraphicEditor
+{
+    public class Rectangle : IShape
+    {
+
+    }
+}
