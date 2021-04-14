256da8f720d1a6ac34d09ee5a40ae56d8cf7879a
Kristiyan Enchev <kristiqnenchevv@gmail.com>
2021-04-14 13:42:44 +0300

    New Exam Add

---
+++ C# Advanced/Retake/1/WarmWinter/WarmWinter/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace WarmWinter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Stack<int> hats = new Stack<int>(Console.ReadLine().Split().Select(int.Parse).ToArray());
+            Queue<int> scarfs = new Queue<int>(Console.ReadLine().Split().Select(int.Parse).ToArray());
+            List<int> set = new List<int>();
+
+            while (hats.Any() && scarfs.Any())
+            {
+                int hat = hats.Peek();
+                int scarf = scarfs.Peek();
+
+                if (hat > scarf)
+                {
+                    set.Add(hat + scarf);
+                    hats.Pop();
+                    scarfs.Dequeue();
+                }
+
+                if (scarf > hat)
+                {
+                    hats.Pop();
+                    continue;
+                }
+
+                if (scarf == hat)
+                {
+                    scarfs.Dequeue();
+                    hat += 1;
+                    hats.Pop();
+                    hats.Push(hat);
+                }
+            }
+
+            int theMostExpensive = set.OrderByDescending(x => x).FirstOrDefault();
+
+            Console.WriteLine($"The most expensive set is: {theMostExpensive}");
+
+            Console.WriteLine(string.Join(" ", set));
+        }
+    }
+}
