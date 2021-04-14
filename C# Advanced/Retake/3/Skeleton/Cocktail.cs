01f9737c4e65eab9dfa3588377b4d85097c3bab6
Kristiyan Enchev <kristiqnenchevv@gmail.com>
2021-04-14 13:43:04 +0300

    New Exam Add

---
+++ C# Advanced/Retake/3/Skeleton/Cocktail.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace CocktailParty
+{
+    public class Cocktail
+    {
+        private List<Ingredient> ingredients;
+
+        public Cocktail(string name, int capacity, int maxAlcoholLevel)
+        {
+            ingredients = new List<Ingredient>();
+            this.Name = name;
+            this.Capacity = capacity;
+            this.MaxAlcoholLevel = maxAlcoholLevel;
+        }
+
+        public string Name { get; set; }
+        public int Capacity { get; set; }
+        public int MaxAlcoholLevel { get; set; }
+
+        public int CurrentAlcoholLevel { get { return ingredients.Sum(x => x.Alcohol); } }
+
+        public void Add(Ingredient ingredient)
+        {
+            if (!ingredients.Any(x => x.Name == ingredient.Name))
+            {
+                if (ingredients.Count < Capacity && ingredient.Alcohol + CurrentAlcoholLevel <= MaxAlcoholLevel)
+                {
+                    ingredients.Add(ingredient);
+                }
+            }
+        }
+
+        public bool Remove(string name)
+        {
+            Ingredient ingrediant = ingredients.FirstOrDefault(p => p.Name == name);
+
+            if (ingrediant == null)
+            {
+                return false;
+            }
+
+            ingredients.Remove(ingrediant);
+            return true;
+        }
+
+        public Ingredient FindIngredient(string name)
+        {
+            Ingredient ingredient = ingredients.FirstOrDefault(p => p.Name == name);
+
+            return ingredient;
+        }
+
+        public Ingredient GetMostAlcoholicIngredient()
+        {
+            return ingredients.OrderByDescending(p => p.Alcohol).FirstOrDefault();
+        }
+
+        public string Report()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            sb.AppendLine($"Cocktail: {this.Name} - Current Alcohol Level: {CurrentAlcoholLevel}");
+
+            foreach (var ingrediant in ingredients)
+            {
+                sb.AppendLine(ingrediant.ToString());
+            }
+
+            return sb.ToString().TrimEnd();
+        }
+    }
+}
