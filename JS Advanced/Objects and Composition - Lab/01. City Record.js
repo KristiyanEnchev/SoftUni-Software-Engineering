b03e0940c663bcf48406f56151dc2a0e3f8f8416
Kristiyan Enchev <kristiqnenchevv@gmail.com>
2021-06-30 17:18:16 +0300

    Add more exercises JS

---
+++ JS Advanced/Objects and Composition - Lab/01. City Record.js
@@ -0,0 +1,14 @@
+function solve(name, population, treasury) {
+    const city = {
+        name,
+        population,
+        treasury
+    }
+
+    return city;
+}
+
+console.log(solve('Tortuga',
+7000,
+15000
+));
