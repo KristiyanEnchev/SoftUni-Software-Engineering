ad9f56e563cf14b52eeb8513e5efc71005090aea
Kristiyan Enchev <kristiqnenchevv@gmail.com>
2021-07-18 17:06:55 +0300

    Add more exercises JS

---
+++ JS Advanced/DOM Introduction - Lab/03.Sum Numbers/SumNumbers.js
@@ -0,0 +1,6 @@
+function calc() {
+    let num1 = document.getElementById('num1').value;
+    let num2 = document.getElementById('num2').value;
+    return document.getElementById('sum').value = Number(num1) + Number(num2);
+}
+
