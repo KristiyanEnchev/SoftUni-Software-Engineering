26425df0da69cb63e3de26ab6e135b968e824f60
Kristiyan Enchev <kristiqnenchevv@gmail.com>
2021-07-23 12:14:18 +0300

    New Exercises JS

---
+++ JS Advanced/DOM Manipulation and Events - Lab/02. Add-Delete/app.js
@@ -0,0 +1,21 @@
+function addItem() {
+    const input = document.getElementById('newItemText'); 
+    const liElement = createElement('li', input.value);
+
+    const deleteBtn = createElement('a', '[Delete]');
+    deleteBtn.href = '#';
+    deleteBtn.addEventListener('click', (ev) => {
+        ev.target.parentNode.remove();
+    });
+
+    liElement.appendChild(deleteBtn);
+
+    document.getElementById('items').appendChild(liElement);
+    input.value = '';
+
+    function createElement(type, content) {
+        const element = document.createElement(type);
+        element.textContent = content;
+        return element;
+    }
+}
