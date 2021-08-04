eaefa141a16c3d28c28bb2a1a65bd613091bd994
Kristiyan Enchev <kristiqnenchevv@gmail.com>
2021-08-04 15:09:21 +0300

    New Add JS

---
+++ JS Advanced/Unit Testing and Error Handling - Exercise/05.Notification/app.js
@@ -0,0 +1,12 @@
+function notify(message) {
+
+  const element = document.getElementById('notification');
+  element.style.display = 'block';
+  element.textContent = message;
+
+  element.addEventListener('click', onClickDiv);
+
+  function onClickDiv(event) {
+    event.target.style.display = 'none';
+  }
+}
