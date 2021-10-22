f491455a8df2d98b8182cde1dcc5bbbab6fb07a6
Kristiyan Enchev <kristiqnenchevv@gmail.com>
2021-10-22 14:27:41 +0300

    New JS Add

---
+++ JS Aplication/Client Side Rendering LAB/01.Contacts/cardTemplate.js
@@ -0,0 +1,17 @@
+import { html } from "./node_modules/lit-html/lit-html.js";
+
+export const cardTemplate = (data) => html`
+<div class="contact card">
+    <div>
+        <i class="far fa-user-circle gravatar"></i>
+    </div>
+    <div class="info">
+        <h2>Name: ${data.name}</h2>
+        <button class="detailsBtn">Details</button>
+        <div class="details" id=${data.id}>
+            <p>Phone number: ${data.phoneNumber}</p>
+            <p>Email: ${data.email}</p>
+        </div>
+    </div>
+</div>
+`;
