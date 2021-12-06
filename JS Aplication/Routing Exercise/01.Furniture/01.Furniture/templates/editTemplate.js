f14dd8b06ae32af6ae438390e8bd87c4aaeb463b
Kristiyan Enchev <kristiqnenchevv@gmail.com>
2021-12-06 10:52:49 +0200

    Adding more JS Exercises

---
+++ JS Aplication/Routing Exercise/01.Furniture/01.Furniture/templates/editTemplate.js
@@ -0,0 +1,48 @@
+import { html } from "../node_modules/lit-html/lit-html.js";
+
+
+export const editTemplate = (itemToEdit, submitHandler) => html`
+<div class="row space-top">
+    <div class="col-md-12">
+        <h1>Edit Furniture</h1>
+        <p>Please fill all fields.</p>
+    </div>
+</div>
+<form @submit=${submitHandler}>
+    <div class="row space-top">
+        <div class="col-md-4">
+            <div class="form-group">
+                <label class="form-control-label" for="new-make">Make</label>
+                <input class="form-control" id="new-make" type="text" name="make" .value=${itemToEdit.make}>
+            </div>
+            <div class="form-group has-success">
+                <label class="form-control-label" for="new-model">Model</label>
+                <input class="form-control" id="new-model" type="text" name="model" .value=${itemToEdit.model}>
+            </div>
+            <div class="form-group has-danger">
+                <label class="form-control-label" for="new-year">Year</label>
+                <input class="form-control" id="new-year" type="number" name="year" .value=${itemToEdit.year}>
+            </div>
+            <div class="form-group">
+                <label class="form-control-label" for="new-description">Description</label>
+                <input class="form-control" id="new-description" type="text" name="description" .value=${itemToEdit.description}>
+            </div>
+        </div>
+        <div class="col-md-4">
+            <div class="form-group">
+                <label class="form-control-label" for="new-price">Price</label>
+                <input class="form-control" id="new-price" type="number" name="price" .value=${itemToEdit.price}>
+            </div>
+            <div class="form-group">
+                <label class="form-control-label" for="new-image">Image</label>
+                <input class="form-control" id="new-image" type="text" name="img" .value=${itemToEdit.img}>
+            </div>
+            <div class="form-group">
+                <label class="form-control-label" for="new-material">Material (optional)</label>
+                <input class="form-control" id="new-material" type="text" name="material" .value=${itemToEdit.material}>
+            </div>
+            <input type="submit" class="btn btn-info" value="Edit" />
+        </div>
+    </div>
+</form>
+`;
